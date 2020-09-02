532767d3ac836fe19a97aedcbf471bddf96163d4
oggyrock <steelo.sun@gmail.com>
2020-09-02 16:56:14 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React from 'react';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    setInterval(() => {
+      const date = new Date();
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+      this.setState({ date: date.toLocaleTimeString() });
+    }, 1000);
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {this.state.date}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
