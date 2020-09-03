301298eabf3ac0b63d330c4a13987d40808795d3
Zarva <zarva6596@gmail.com>
2020-09-03 16:12:09 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
 import './App.scss';
+import { Time } from './Time';
 
-class App extends React.Component {
-  state = {
-    date: new Date().toLocaleTimeString(),
-  };
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        date: new Date().toLocaleTimeString(),
-      });
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div className="box">
-        <div className="box__time">
-          {this.state.date}
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="box">
+    <div className="box__time">
+      <Time />
+    </div>
+  </div>
+);
 
 export default App;
