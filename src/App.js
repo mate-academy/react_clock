2ebae79b01f6b00fff94ab7892426d775a5cf558
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-21 14:32:15 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
+import { Clock } from './components/Clock';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
+function App() {
   return (
     <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
+      <Clock />
     </div>
   );
-};
+}
 
 export default App;
