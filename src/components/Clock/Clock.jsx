2048715e94582eb1667af7ddcf5fa87de9ca213b
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-08 14:31:50 +0300

    Solution

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+export class Clock extends React.Component {
+  state = {
+    date: new Date(),
+    name: getRandomInt(),
+    visibility: false,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      visibility: false,
+      name: getRandomInt(),
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ date: new Date() });
+      console.log(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  toggleVisibility = () => {
+    this.setState(state => ({
+      visibility: !state.visibility,
+    }));
+  }
+
+  changeName = () => {
+    this.setState((thisState) => {
+      const randomizedName = getRandomInt();
+
+      console.log(`The Clock was renamed from ${thisState.name} to
+       ${randomizedName}`);
+
+      return {
+        name: randomizedName,
+      };
+    });
+  }
+
+  render() {
+    if (this.state.visibility) {
+      return (
+        <>
+          <h1>
+            Name -
+            {' '}
+            {this.state.name}
+          </h1>
+          <p>
+            Current time:
+            {this.state.date.toLocaleTimeString()}
+          </p>
+          <p>
+            <button onClick={this.toggleVisibility}>Hide clock</button>
+          </p>
+          <p>
+            <button onClick={this.changeName}>Change name</button>
+          </p>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <h1>
+          Name -
+          {' '}
+          {this.state.name}
+        </h1>
+        <p>
+          <button onClick={this.toggleVisibility}>Show clock</button>
+        </p>
+        <p>
+          <button onClick={this.changeName}>Change name</button>
+        </p>
+      </>
+    );
+  }
+}
+
+function getRandomInt() {
+  const min = 0;
+  const max = 999;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
