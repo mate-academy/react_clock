d96dd2a1307225e655bb52131400c4feb8537ed4
andlap24 <andrew.laptev24@gmail.com>
2020-10-24 12:34:07 +0300

    add name-randomizer

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,76 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.scss';
 
 class App extends React.Component {
   state = {
     date: new Date(),
     isClockVisible: 'visible',
+    name: 1,
   }
 
   componentDidMount() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.setState({ date: new Date() });
+      console.log(this.state.date.toLocaleTimeString());
     }, 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  nameRandomizer() {
+    this.setState({ name: Math.floor(Math.random() * 1000) });
+  }
+
   render() {
+    const { date, isClockVisible } = this.state;
+
     return (
       <div className="wrapper">
         <div className="App">
           <div className="App__clock">
-            <h1>React clock</h1>
-            <p style={{ visibility: `${this.state.isClockVisible}` }}>
+          <h1>{`React clock #${this.state.name}`}</h1>
+            <p></p>
+            <p style={{ visibility: `${isClockVisible}` }}>
               Current time:
               {' '}
-              {this.state.date.toLocaleTimeString()}
+              {date.toLocaleTimeString()}
             </p>
           </div>
 
           <div className="App__button button">
             <button
               type="button"
-              className="button__show"
+              className="button__toggler"
               onClick={() => {
-                this.state.isClockVisible = 'visible';
+                isClockVisible === 'hidden'
+                  ? this.setState({ isClockVisible: 'visible'} )
+                  : this.setState({ isClockVisible: 'hidden'} );
               }}
             >
-              show
+              {this.state.isClockVisible === 'visible' ? 'hide' : 'show'}
             </button>
 
             <button
               type="button"
-              className="button__hide"
-              onClick={() => {
-                this.state.isClockVisible = 'hidden';
-              }}
+              className="button__magic"
+              onClick={() => this.nameRandomizer()}
             >
-              hide
+              magic
             </button>
           </div>
         </div>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  isClockVisible: PropTypes.string.isRequired,
+  name: PropTypes.number.isRequired,
 }
 
 export default App;
