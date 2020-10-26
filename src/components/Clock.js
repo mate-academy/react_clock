7e448d2b87e5c95f16ec862b417528fb41365463
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-26 17:01:24 +0200

    solution

---
+++ src/components/Clock.js
@@ -0,0 +1,33 @@
+const { PureComponent } = require('react');
+
+class Clock extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000,
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      this.state.date.toLocaleTimeString()
+    );
+  }
+}
+
+export default Clock;
