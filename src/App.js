239843bf2e6a80b55e0bc288637420f275ee6388
KirillLutsenko <mnrovski68@gmail.com>
2020-10-26 15:29:01 +0200

    React Clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 
 import './App.scss';
+import { Clock } from './clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    name: 1,
+  };
 
+  visibilityClick = () => {
+    this.setState(state => ({
+      isClockVisible: !state.isClockVisible,
+    }));
+  }
+
+  addRandomName = () => {
+    const randomName = Math.round(Math.random() * 1000);
     // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+    console.log(`The Clock was renamed from ${this.state.name} to ${randomName}`);
+    this.setState({ name: randomName });
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__title">React Clock</h1>
+        <Clock
+          name={this.state.name}
+          isVisible={this.state.isClockVisible}
+        />
+
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.visibilityClick}
+        >
+          Hide Clock
+        </button>
         {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.addRandomName}
+        >
+          Change Name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/clock/Clock.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ClockShape } from '../shapes/ClockShape';
+import './Clock.scss';
+
+export class Clock extends React.Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+      // eslint-disable-next-line
+      console.log(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    const { name, isVisible } = this.props;
+    const time = this.state.date.toLocaleTimeString();
+
+    return (
+      <div className="clock">
+        {
+          isVisible
+            ? <p className="clock__face">{`${time}`}</p>
+            : <p className="clock__face">Time to Coffee</p>
+        }
+
+        <p className="clock__name">
+          {`Clock Name : ${name}`}
+        </p>
+      </div>
+    );
+  }
+}
+
+Clock.propTypes = ClockShape;
