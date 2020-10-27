d755c0dfc003d7770064da9453cec4e4df7b501a
Marina <m.gerasymova29@gmail.com>
2020-10-27 12:30:13 +0200

    fix es-lint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Clock } from './components/CLock';
 
 import './App.scss';
 
 const App = () => {
-  setInterval(() => {
-    const date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  const [isClockVisible, setClockVisibility] = useState(true);
+  const [clockName, setClockName] = useState(Math.floor(Math.random() * 100));
 
   return (
     <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
+      <div className="App__time">
+        {
+          isClockVisible ? <Clock name={clockName} /> : ''
+        }
+        <button
+          type="button"
+          className="App__button"
+          onClick={() => setClockVisibility(!isClockVisible)}
+        >
+          {isClockVisible ? 'Hide' : 'Show'}
+        </button>
+
+        <button
+          type="button"
+          className="App__button"
+          onClick={() => {
+            setClockName(Math.floor(Math.random() * 100));
+            // eslint-disable-next-line no-console
+            console.log('The Clock was renamed from oldName to newName');
+          }}
+        >
+          Change name
+        </button>
+      </div>
     </div>
   );
 };
