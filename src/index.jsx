adc0d3de7e5a71ed582ec1e7fc623a32e60b5874
d.yevlakhov <tragenstolz@gmail.com>
2020-12-01 12:38:45 +0200

    Remove react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
