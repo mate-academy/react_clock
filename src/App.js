a0f83a91a0e4b90309b98de1de6257cabedbf435
Valentina Vorfolomeeva <vorfolomeevav@gmail.com>
2021-01-14 15:59:44 +0200

    Added clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from 'react';
+import { Clock } from './Clock';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString(),
+    isClockVisible: true,
+    clockName: 0,
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  componentDidMount() {
+    setInterval(() => {
+      if (!this.state.isClockVisible) {
+        return;
+      }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+      this.setState({ time: new Date().toLocaleTimeString() });
+      // eslint-disable-next-line
+      console.log(this.state.time);
+    }, 1000);
+  }
+
+  showClock = () => {
+    this.setState({ isClockVisible: true });
+  }
+
+  hideClock = () => {
+    this.setState({ isClockVisible: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <Clock
+          time={this.state.time}
+          visibility={this.state.isClockVisible}
+          name={this.state.clockName}
+        />
+
+        <button
+          type="button"
+          onClick={this.showClock}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={this.hideClock}
+        >
+          Hide clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.setState({ clockName: Math.random() })}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
