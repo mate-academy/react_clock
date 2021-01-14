5d6877346c74c4e7abc7a570a30e80c3ca72784c
Sasha Larin <sashalarin200@gmail.com>
2021-01-14 17:53:03 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from 'react';
-
+import { Clock } from './components/Clock/Clock';
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    vueClock: true,
+    data: (new Date()).toLocaleTimeString(),
+    number: 0,
+  };
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  changeTime = () => {
+    this.setState({
+      data: (new Date()).toLocaleTimeString(),
+    });
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  setRandomNumber = () => {
+    this.setState({
+      number: Math.random(),
+    });
+    // eslint-disable-next-line no-console,max-len
+    console.log(this.state.number);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {this.state.data}
+        <p>
+          Current time:
+          {' '}
+          {this.state.vueClock && (
+            <Clock />
+          )}
+        </p>
+
+        <button
+          type="button"
+          onClick={() => this.setState({ vueClock: false })}
+        >
+          Hide Clock
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState({ vueClock: true })}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          onClick={this.changeTime}
+        >
+          change time
+        </button>
+        <button
+          type="button"
+          onClick={this.setRandomNumber}
+        >
+          get console randomNumer
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
