cda8aac5a1b3b8d026e50bce179ad25f80faf9a9
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-16 18:51:32 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: false,
+    clockName: Math.round(Math.random() * 100),
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    const showClock = () => {
+      this.setState({
+        isClockVisible: true,
+      });
+    };
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    const hideClock = () => {
+      this.setState({
+        isClockVisible: false,
+      });
+      clearInterval();
+    };
+
+    const ramdomNumber = () => {
+      this.setState({
+        clockName: Math.round(Math.random() * 100),
+      });
+    };
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {this.state.isClockVisible && <Clock name={this.state.clockName} />}
+        </p>
+
+        <button type="button" onClick={showClock}>
+          Show Clock
+        </button>
+
+        <button type="button" onClick={hideClock}>
+          Hide Clock
+        </button>
+
+        <button type="button" onClick={ramdomNumber}>
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
