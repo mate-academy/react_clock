48be3a16a88472c266ab9c2deeb6246ae4dcb948
ShyianO <ol.shyian@gmacil.com>
2021-01-18 15:55:04 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
 
 import './App.scss';
+import Clock from './components/Clock/Clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  };
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    const { isClockVisible, clockName } = this.state;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {isClockVisible && <Clock name={clockName} />}
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            if (isClockVisible) {
+              this.setState({
+                clockName: Math.round(Math.random() * 1000),
+              });
+            }
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Clock extends React.Component {
+  state = {
+    time: new Date(),
+  };
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({
+        time: new Date(),
+      });
+
+      // eslint-disable-next-line
+      console.log(this.state.time.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentDidUpdate({ name: oldName }) {
+    const { name } = this.props;
+
+    if (oldName !== name) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from ${oldName} to ${name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return (
+      <p>
+        {'Current time: '}
+        {time.toLocaleTimeString()}
+      </p>
+    );
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+};
