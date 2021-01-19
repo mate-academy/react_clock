2b3036ae981874fb3a19dcac4980ce86005f4371
Dima Bond <neverpooh@gmail.com>
2021-01-19 17:39:11 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -4,52 +4,45 @@ import './App.scss';
 
 class App extends React.Component {
   state = {
-    time: new Date().toLocaleTimeString(),
     isClockVisible: false,
     clockName: null,
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ time: new Date().toLocaleTimeString() });
-      // eslint-disable-next-line
-      console.log(this.state.time);
-    }, 1000);
   }
 
   render() {
     return (
       <div className="App">
         <h1>React clock</h1>
-        <Clock
-          time={this.state.time}
-          name={this.state.clockName}
-          isClockVisible={this.state.isClockVisible}
-        />
-        <button
-          type="button"
-          onClick={() => {
-            this.setState({ isClockVisible: true });
-          }}
-        >
-          Show Clock
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            this.setState({ isClockVisible: false });
-          }}
-        >
-          Hide Clock
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            this.setState({ clockName: pickRandomName() });
-          }}
-        >
-          Set random name
-        </button>
+        {this.state.isClockVisible && (
+          <Clock
+            name={this.state.clockName}
+          />
+        )}
+        <div className="App__buttons">
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ isClockVisible: true });
+            }}
+          >
+            Show Clock
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ isClockVisible: false });
+            }}
+          >
+            Hide Clock
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ clockName: pickRandomName() });
+            }}
+          >
+            Set random name
+          </button>
+        </div>
       </div>
     );
   }
--- src/Clock.js
+++ src/Clock.js
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class Clock extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    const interval = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+      // eslint-disable-next-line
+      console.log(this.state.time);
+    }, 1000);
+
+    this.setState({ interval });
+  }
+
   componentDidUpdate(prevProps) {
     if (prevProps.name !== this.props.name) {
       // eslint-disable-next-line
@@ -9,26 +21,21 @@ export class Clock extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
   render() {
     return (
-      <div>
-        {
-          this.props.isClockVisible
-          && (
-            <p>
-              Current time:
-              {' '}
-              {this.props.time}
-            </p>
-          )
-        }
+      <div className="clock">
+        Current time:
+        {' '}
+        {this.state.time}
       </div>
     );
   }
 }
 
 Clock.propTypes = PropTypes.shape({
-  time: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  isClockVisible: PropTypes.bool.isRequired,
 }).isRequired;
