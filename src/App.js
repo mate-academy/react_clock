9d35318159a3c380730edd5b3c2b1369783444dc
Viktor Morhun <victor.morgun16@gmail.com>
2021-01-20 10:26:36 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React from 'react';
+import { Clock } from './Clock/Clock';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    clockName: '',
+    isClockVisiable: true,
+  };
 
+  setRandom = () => {
+    const randomName = Math.random() * 10;
+
+    this.setState({ name: randomName });
     // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+    console.log(`
+    The Clock was renamed from ${this.state.name} to ${randomName}
+  `);
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    const { clockName, isClockVisiable } = this.state;
+
+    return (
+      <div className="wrapper">
+        {isClockVisiable && <Clock name={clockName} />}
+        <div className="container">
+          <button
+            className="button"
+            type="button"
+            onClick={() => {
+              this.setState(
+                {
+                  isClockVisiable: true,
+                },
+              );
+            }}
+          >
+            ShowClock
+          </button>
+
+          <button
+            className="button"
+            type="button"
+            onClick={() => {
+              this.setState(
+                {
+                  isClockVisiable: false,
+                },
+              );
+            }}
+          >
+            Hide Clock
+          </button>
+
+          <button
+            className="button"
+            type="button"
+            onClick={() => this.setRandom()}
+          >
+            Set random name
+          </button>
+        </div>
+
+      </div>
+    );
+  }
+}
 
 export default App;
