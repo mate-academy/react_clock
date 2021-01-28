c390c05cb6e5ec89645f68f8ad6636975bd282bb
Inna Golovina <igigig978@gmail.com>
2021-01-28 15:36:44 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React from 'react';
 
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 1,
+  };
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
+  getRandomName = () => (
+    Math.floor(Math.random() * 100)
   );
-};
+
+  getClockName = () => (
+    this.setState((state) => {
+      const oldName = state.clockName;
+      const newName = this.getRandomName();
+
+      // eslint-disable-next-line no-console
+      console.log(`The Clock was renamed from ${oldName} to ${newName}`);
+
+      return {
+        clockName: newName,
+      };
+    })
+  );
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="app">
+        <div className="app_clock">
+          <h1 className="title">React clock</h1>
+          {isClockVisible && <Clock name={clockName} />}
+
+          <div className="app_buttons">
+            <button
+              type="button"
+              onClick={() => {
+                this.setState(state => ({ isClockVisible: true }));
+              }}
+            >
+              Show Clock
+            </button>
+
+            <button
+              type="button"
+              onClick={() => {
+                this.setState(state => ({ isClockVisible: false }));
+              }}
+            >
+              Hide Clock
+            </button>
+
+            <button
+              type="button"
+              onClick={this.getClockName}
+            >
+              Rename clock
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
