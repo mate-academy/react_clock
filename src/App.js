954dbc8c5c0af6b806a5183381f5b1dfc0a08cac
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-02-08 14:46:21 +0200

    Was add random name and number

--- src/App.js
+++ src/App.js
@@ -1,32 +1,84 @@
 import React, { Component } from 'react';
-// import { Clock } from './components/Clock';
+import { Clock } from './components/Clock';
 
 import './App.scss';
 
 export class App extends Component {
   state = {
-    dataLocalTime: null,
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      const date = new Date();
-
-      // eslint-disable-next-line
-      this.setState({ dataLocalTime: date.toLocaleTimeString()})
-    }, 1000);
+    isClockVisible: false,
+    clockName: '',
+    clockTime: 0,
   }
 
   render() {
     return (
       <div className="App">
         <h1>React clock</h1>
-        <p>
-          Current time:
-          {' '}
-          { this.state.dataLocalTime }
-        </p>
-        {/* <Clock /> */}
+
+        <button
+          type="button"
+          onClick={() => {
+            this.state.isClockVisible
+              ? this.setState({ isClockVisible: false })
+              : this.setState({ isClockVisible: true });
+          }
+          }
+        >
+          {this.state.isClockVisible ? 'Show Clock' : 'Hide Clock'}
+        </button>
+
+        {!this.state.isClockVisible
+          && (
+            <button
+              type="button"
+              onClick={() => {
+                const dataNames = ['Cooking', 'Cleaning', 'Washing', 'Wake Up'];
+
+                this.setState({
+                  // eslint-disable-next-line
+                  clockName: dataNames[Math.floor(Math.random() * dataNames.length)],
+                });
+              }}
+            >
+              Set random name
+            </button>
+          )
+        }
+
+        {!this.state.isClockVisible && this.state.clockName
+          && (
+            <button
+              type="button"
+              onClick={() => {
+                this.setState({
+                  clockTime: Math.floor(Math.random() * 100) + 1,
+                });
+              }}
+            >
+              Set random time
+            </button>
+          )
+        }
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ clockName: null });
+            this.setState({ clockTime: null });
+          }}
+        >
+          Reset
+        </button>
+
+        {!this.state.isClockVisible
+          && (
+            <Clock
+              name={this.state.clockName}
+              time={this.state.clockTime}
+            />
+          )
+        }
+
       </div>
     );
   }
--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Clock extends Component {
   state = {
-    isClockVisible: false,
+    dataLocalTime: null,
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      const date = new Date();
+
+      // eslint-disable-next-line
+      this.setState({ dataLocalTime: date.toLocaleTimeString()});
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   render() {
     return (
       <>
-        <h2>Check</h2>
-        <p>{this.state.isClockVisible}</p>
+        <p>
+          Current time:
+          {' '}
+          { this.state.dataLocalTime }
+        </p>
+        <p>
+          {this.props.name}
+          {' '}
+          {this.props.time}
+        </p>
       </>
     );
   }
 }
+
+Clock.propTypes = {
+  name: PropTypes.oneOfType([
+    // PropTypes.oneOf([null]),
+    PropTypes.string,
+  ]).isRequired,
+
+  time: PropTypes.oneOfType([
+    // PropTypes.oneOf([null]),
+    PropTypes.number,
+  ]).isRequired,
+};
+
+// PropTypes.oneOfType([
+//   PropTypes.string.isRequired,
+//   PropTypes.oneOf([null]).isRequired,
+// ]).isRequired
