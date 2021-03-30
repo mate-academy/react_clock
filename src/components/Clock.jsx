daa52d3fa86f8691dee96a80064c26b6fe786e40
Onishchuk <onishchuk.danil@gmail.com>
2021-03-30 16:02:34 +0300

    solution2

---
+++ src/components/Clock.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Clock = ({name, date}) => (
+  <>
+    <p>
+     Clock #{name}
+    </p>
+    <p>
+      Current time: {date.toLocaleTimeString()}
+    </p>
+  </>
+)
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+  date: PropTypes.object.isRequired,
+}
