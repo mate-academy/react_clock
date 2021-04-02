d0f471bab01b0b5e412c981f31f35255e7870883
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-04-02 16:55:08 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,24 +2,74 @@ import React from 'react';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+export class App extends React.Component {
+  state = {
+    visible: true,
+    name: Math.trunc(Math.random() * 100),
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.name !== this.state.name) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from ${prevState.name} to ${this.state.name}.`);
+    }
+  }
 
-export default App;
+  hide = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  show =() => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  change =() => {
+    this.setState({
+      name: Math.ceil(Math.random() * 100),
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="text">
+          <h1>React clock</h1>
+          {this.state.visible && <Clock name={this.state.name} />}
+          <p>
+            Name:
+            {this.state.name}
+          </p>
+        </div>
+        <div className="buttons">
+          <button
+            className="button"
+            type="button"
+            onClick={this.hide}
+          >
+            Hide
+          </button>
+          <button
+            className="button"
+            type="button"
+            onClick={this.show}
+          >
+            Show
+          </button>
+          <button
+            className="button"
+            type="button"
+            onClick={this.change}
+          >
+            Change name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export class Clock extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  }
+
+  componentDidMount () {
+      this.state.interval = setInterval(() => {
+      console.log(new Date().toLocaleTimeString())
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return(
+      <p>Current time: {time}</p>
+      )
+  }
+}
