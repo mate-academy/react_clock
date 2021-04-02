c6bb3344e8c393a6faece37a8ca41b68e4ac2ee0
Svyat Krit <krit.mgmn@gmail.com>
2021-04-02 11:47:07 +0300

    Added new files

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date() };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(
+      () => this.setState({ time: new Date() }),
+      1000,
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const { time } = this.state;
+
+    // eslint-disable-next-line
+    console.log(time.toLocaleTimeString());
+
+    return (
+      <div className="Clock panel">
+        <div className="panel-heading">
+          React clock
+        </div>
+        <div className="panel-block p-5">
+          <p>
+            Current time:
+            {' '}
+            <strong>{time.toLocaleTimeString()}</strong>
+          </p>
+        </div>
+        <div className="panel-block pl-5">
+          <p class="is-size-7">
+            Name:
+            {' '}
+            {this.props.name}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+};
