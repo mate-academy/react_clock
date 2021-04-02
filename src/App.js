39d0242118118c8b6a6fcfdf67acbda47568932c
Svyat Krit <krit.mgmn@gmail.com>
2021-04-02 11:45:00 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+export class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: Math.random(),
+  }
 
+  componentDidUpdate(prevProps, prevState) {
     // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+    console.log(
+      'The Clock was renamed from '
+      + `${prevState.clockName} to ${this.state.clockName}`,
+    );
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App column is-one-third container">
+        <div className="panel">
+          <div className="panel-block">
+            <button
+              type="button"
+              onClick={
+                () => this.setState({ isClockVisible: true })
+              }
+              className="button is-small is-light is-fullwidth mr-3"
+            >
+              Show Clock
+            </button>
+            <button
+              type="button"
+              onClick={
+                () => this.setState({ isClockVisible: false })
+              }
+              className="button is-small is-light is-fullwidth mr-3"
+            >
+              Hide Clock
+            </button>
+            <button
+              type="button"
+              onClick={
+                () => this.setState({ clockName: Math.random() })
+              }
+              className="button is-small is-light is-fullwidth"
+            >
+              Set random name
+            </button>
+          </div>
+        </div>
+        {this.state.isClockVisible && <Clock name={this.state.clockName} />}
+      </div>
+    );
+  }
+}
 
 export default App;
