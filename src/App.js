2b47ac3075913ff5eb9901b62a3068c0fcf3fe5c
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-06 10:27:47 +0300

    test

--- src/App.js
+++ src/App.js
@@ -11,48 +11,49 @@ export class App extends React.Component {
     hint: true,
   }
 
-  componentDidUpdate(_, oldName) {
-    const message = `The Clock was renamed from ${oldName.clockName}
+  componentDidUpdate(_, prevState) {
+    const message = `The Clock was renamed from ${prevState.clockName}
     to ${this.state.clockName}`;
 
-    if (oldName.clockName === this.state.clockName) {
+    if (prevState.clockName === this.state.clockName) {
       return;
     }
     // eslint-disable-next-line
     console.log(message);
   }
 
+  buttonHidden = () => {
+    this.setState({
+      hiddenClock: true,
+    });
+  };
+
+  mouseEnter = () => {
+    this.setState({
+      hint: false,
+    });
+  };
+
+  mouseLeave = () => {
+    this.setState({
+      hint: true,
+    });
+  };
+
+  buttonShow = () => {
+    this.setState({
+      hiddenClock: false,
+    });
+  };
+
+  setRandomName = () => {
+    this.setState({
+      clockName: Math.random().toString(36).substring(7),
+    });
+  };
+
   render() {
     const { clockName, hiddenClock, hint } = this.state;
-    const buttonHidden = () => {
-      this.setState({
-        hiddenClock: true,
-      });
-    };
-
-    const mouseEnter = () => {
-      this.setState({
-        hint: false,
-      });
-    };
-
-    const mouseLeave = () => {
-      this.setState({
-        hint: true,
-      });
-    };
-
-    const buttonShow = () => {
-      this.setState({
-        hiddenClock: false,
-      });
-    };
-
-    const setRandomName = () => {
-      this.setState({
-        clockName: Math.random().toString(36).substring(7),
-      });
-    };
 
     return (
       <div className="mainBox">
@@ -72,23 +73,23 @@ export class App extends React.Component {
         <button
           type="button"
           className="btn btn-danger margin"
-          onClick={buttonHidden}
+          onClick={this.buttonHidden}
         >
           STOP/HIDE
         </button>
         <button
           type="button"
           className="btn btn-success margin"
-          onClick={buttonShow}
+          onClick={this.buttonShow}
         >
           START/SHOW
         </button>
         <button
           type="button"
           className="btn btn-light margin"
-          onClick={setRandomName}
-          onMouseEnter={mouseEnter}
-          onMouseLeave={mouseLeave}
+          onClick={this.setRandomName}
+          onMouseEnter={this.mouseEnter}
+          onMouseLeave={this.mouseLeave}
         >
           CHAGE NAME
         </button>
