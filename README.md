f63a82370179d3889f87ba2ba44d14d758302fb6
Alex Fedorchenko <mralex.fed@gmail.com>
2021-05-03 19:43:36 +0300

    Solution

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Clock
-- Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_clock/)
+- Replace `mralexfed007` with your Github username in the
+ [DEMO LINK](https://mralexfed007.github.io/react_clock/)
 - Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 
 ## Task
--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-plusplus */
 import React from 'react';
 
 import './App.scss';
+import Clock from './Clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    timerId: null,
+    isClockVisible: true,
+    date: '',
+    clockName: '',
+    randomPack:
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  componentDidMount() {
+    this.start();
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  start = () => {
+    this.setState({ isClockVisible: true });
+    clearInterval(this.state.timerId);
+    this.setState({ timerId: setInterval(() => {
+      const date = new Date();
+
+      this.setState({
+        date: date.toLocaleTimeString(),
+      });
+      // eslint-disable-next-line
+            console.log(date.toLocaleTimeString());
+    }, 1000) });
+  };
+
+  render() {
+    const { isClockVisible, randomPack } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <button
+          type="button"
+          onClick={() => {
+            let randomText = '';
+
+            for (let i = 0; i < 5; i++) {
+              randomText += randomPack.charAt(Math.floor(
+                Math.random() * randomPack.length,
+              ));
+            }
+
+            this.setState({ clockName: randomText });
+          }}
+        >
+          Set random name
+        </button>
+
+        <button
+          type="button"
+          onClick={this.start}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+            clearInterval(this.state.timerId);
+          }}
+        >
+          Hide Clock
+        </button>
+        {isClockVisible && <Clock data={this.state} />}
+
+      </div>
+    );
+  }
+}
 
 export default App;
