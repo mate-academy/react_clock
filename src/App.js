1858374518f9492f426626bb855a70e327766ead
Anton Kostylov <antoniok003@gmail.com>
2021-05-19 12:46:06 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
+import { Clock } from './components/Clock';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    const { isClockVisible, clockName } = this.state;
+    const oldClockName = clockName;
+    const newClockName = Math.floor(Math.random() * 101);
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    return (
+      <>
+        {isClockVisible && <Clock name={this.state.clockName} />}
+        <button
+          type="button"
+          className="App__button"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          className="App__button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide Clock
+        </button>
+        <button
+          type="button"
+          className="App__button"
+          onClick={() => {
+            this.setState({ clockName: newClockName });
+            // eslint-disable-next-line
+            console.log(`The Clock was renamed from ${oldClockName} to ${newClockName}`);
+          }}
+        >
+          Set random name
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
