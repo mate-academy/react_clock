a5a67f8b39ca67db52ee40949f114a0c9a2b8a43
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-20 14:59:01 +0300

    Clock was implemented

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Clock extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    this.indterval = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps) {
+    // eslint-disable-next-line
+    console.log(
+      prevProps.name === this.props.name
+        ? this.state.time
+        : `The clock was renamed from ${prevProps.name} to ${this.props.name}`,
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.indterval);
+  }
+
+  render() {
+    return this.state.time;
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+};
