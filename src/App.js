00af0878611ff9b38afc03e7ef61e89e30d0f09d
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-20 22:53:31 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React from 'react';
-
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    date: '',
+    intervalId: undefined,
+    isClockVisible: true,
+    clockName: 0,
+  }
 
+  componentDidMount() {
+    this.runTimer();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { clockName } = this.state;
+    const oldClockName = prevState.clockName;
+
+    if (oldClockName !== clockName) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed`
+      + ` from ${oldClockName} to ${clockName}.`);
+    }
+  }
+
+  setRandomName = () => {
+    const min = 1;
+    const max = 1000;
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    this.setState({ clockName: num });
+  }
+
+  runTimer = () => {
+    const id = setInterval(() => {
+      this.setState({ date: new Date().toLocaleTimeString() });
+    }, 1000);
+
+    this.setState({ intervalId: id });
+  }
+
+  clearTimer = () => {
+    const { intervalId } = this.state;
+
+    clearInterval(intervalId);
+  }
+
+  showTimer = () => {
+    this.setState({ isClockVisible: true });
+    this.runTimer();
+  }
+
+  hideTimer = () => {
+    this.setState({ isClockVisible: false });
+    this.clearTimer();
+  }
+
+  render() {
+    const { date, isClockVisible } = this.state;
     // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+    console.log(date ? `time is ${date}` : 'timer isn\'t set up yet');
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    return (
+      <div className="App">
+        <div className="info">
+          <h1 className="h1">React clock</h1>
+          <p className={isClockVisible ? ' time show' : 'time hide'}>
+            {`Current time: ${date}`}
+          </p>
+        </div>
+        <div className="control">
+          <button type="button" onClick={this.showTimer}>
+            Show Clock
+          </button>
+          <button type="button" onClick={this.hideTimer}>
+            Hide Clock
+          </button>
+          <button type="button" onClick={this.setRandomName}>
+            Set random name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
