e7377299ffd21493245ea9c4d09138c76981785e
ched88xtc <ched88official@gmail.com>
2021-05-21 11:38:42 +0300

    Added all files

---
+++ src/Clock.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Clock extends React.Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.setState({
+      time: setInterval(() => {
+        this.setState({ date: new Date() });
+        // eslint-disable-next-line
+        console.log(this.state.date.toLocaleTimeString());
+      }, 1000),
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.newName !== this.props.newName) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from`
+      ` ${prevProps.newName} to ${this.props.newName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.state.time);
+  }
+
+  render() {
+    return (
+      <span>{this.state.date.toLocaleTimeString()}</span>
+    );
+  }
+}
+
+Clock.propTypes = {
+  newName: PropTypes.number.isRequired,
+};
