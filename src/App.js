3718a3ef7470b241004c36f64ea79724f57bebc9
Artem Bazhenov <artem.bazhenov.325@mail.com>
2021-05-24 14:07:03 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import Clock from './Clock';
 
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React clock</h1>
-    <p>
-      Current time:
-      {' '}
-      <Clock />
-    </p>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {this.state.isClockVisible && <Clock name={this.state.clockName} />}
+        </p>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ clockName: Math.floor(Math.random() * 100) });
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,72 +1,33 @@
 import React from 'react';
 
 class Clock extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      time: new Date().toLocaleTimeString(),
-      status: true,
-      count: 0,
-    };
-  }
+  state = {
+    time: new Date(),
+  };
 
   componentDidMount() {
     this.inter = () => {
       console.log(new Date().toLocaleTimeString());
-      this.setState({ time: new Date().toLocaleTimeString() });
-      clearInterval(this.randomInterId);
+      this.setState({ time: new Date() });
     };
 
     this.interId = setInterval(this.inter, 1000);
   }
 
-  componentDidUpdate() {
-    this.name = (num) => {
-      if (num !== this.state.count) {
-        console.log(`The Clock was renamed from ${this.state.count} to ${num}`);
-        this.setState({ count: num });
-      }
+  componentDidUpdate(prevProps) {
+    if (prevProps.name !== this.props.name) {
+      console.log(`The Clock was renamed from ${prevProps.name} to ${this.props.name}`)
     }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interId);
   }
 
   render() {
     return (
       <>
-        {this.state.time}
-
-        <br />
-
-        <button
-          type="button"
-          onClick={() => {
-            this.interId = setInterval(this.inter, 1000);
-            this.setState({ status: true });
-          }}
-        >
-          Show Clock
-        </button>
-
-        <button
-          type="button"
-          onClick={() => {
-            clearInterval(this.interId);
-            this.setState({ time: '', status: false });
-          }}
-        >
-          Hide Clock
-        </button>
-
-        <button
-          type="button"
-          onClick={() => {
-            if (this.state.status) {
-              this.name(Math.floor(Math.random() * 100));
-            }
-          }}
-        >
-          Set random name
-        </button>
+        {this.state.time.toLocaleTimeString()}
       </>
     );
   }
