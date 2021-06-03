12942a59b2fa53b82ac4e530f8092e0912d7be11
romandubovskyi <rdubowskij@gmail.com>
2021-06-03 11:07:06 +0300

    improved solution

--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Clock } from './clock';
 
-let interval;
-
 class App extends React.Component {
   state = {
     isClockVisible: false,
-    time: new Date().toLocaleTimeString(),
     clockName: 0,
   }
 
-  componentDidUpdate(_, prevState) {
-    clearInterval(interval);
-    const { isClockVisible, clockName } = this.state;
-
-    if (clockName !== prevState.clockName) {
-      // eslint-disable-next-line
-      console.log(`The Clock was renamed from ${prevState.clockName} to ${clockName}`)
-    }
-
-    if (isClockVisible) {
-      interval = setInterval(() => {
-        const date = new Date().toLocaleTimeString();
-
-        // eslint-disable-next-line
-        console.log(date);
-        this.setState({ time: date });
-      }, 1000);
-    }
-  }
-
   render() {
-    const { isClockVisible, time, clockName } = this.state;
+    const { isClockVisible, clockName } = this.state;
 
     return (
       <div className="App">
@@ -58,10 +35,7 @@ class App extends React.Component {
 
         {isClockVisible
           && (
-          <Clock
-            currentTime={time}
-            name={clockName}
-          />
+          <Clock name={clockName} />
           )}
       </div>
     );
--- src/clock/Clock.jsx
+++ src/clock/Clock.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Clock = ({ currentTime, name }) => (
-  <>
-    <h4>{`Current time: ${currentTime}`}</h4>
-    <h4>{`Current name : ${name} `}</h4>
-  </>
-);
+let interval;
+
+export class Clock extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  }
+
+  componentDidMount() {
+    interval = setInterval(() => {
+      const date = new Date().toLocaleTimeString();
+
+      // eslint-disable-next-line
+      console.log(date);
+      this.setState({ time: date });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { name } = this.props;
+
+    if (name === prevProps.name) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from ${prevProps.name} to ${name}`)
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(interval);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+
+    return (
+      <>
+        <h4>{`Current time: ${time}`}</h4>
+        <h4>{`Current name : ${name} `}</h4>
+      </>
+    );
+  }
+}
 
 Clock.propTypes = {
-  currentTime: PropTypes.string.isRequired,
   name: PropTypes.number.isRequired,
 };
