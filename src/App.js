9cb11c037913a4fb4b1c3db038b7ca0bfbd25ff7
Tolik <tolikdolinskiy@gmail.com>
2021-07-04 21:33:14 +0300

    some message

--- src/App.js
+++ src/App.js
@@ -2,24 +2,70 @@ import React from 'react';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    time: new Date().toLocaleTimeString(),
+    clockName: 0,
+  };
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.isClockVisible
+      && new Date().toLocaleTimeString() !== this.state.time) {
+        this.setState({ time: new Date().toLocaleTimeString() });
+        console.log(this.state.time);
+      }
+    }, 1000);
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  componentDidUpdate(prevProps, prevState) {
+    prevState.clockName !== this.state.clockName
+    && console.log(`
+    The Clock was renamed from ${prevState.clockName} to ${this.state.clockName}
+    `);
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        {this.state.isClockVisible
+        && <h1>React clock</h1>}
+        <p>
+          Current time:
+          {this.state.time}
+        </p>
+        <div>
+          <button
+            type="button"
+            onClick={() => (
+              this.state.isClockVisible
+            || this.setState({
+              isClockVisible: true, time: new Date().toLocaleTimeString(),
+            })
+            )}
+          >
+            Show Clock
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.setState({ isClockVisible: false })}
+          >
+            Hide Clock
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.setState({ clockName: Math.random() })}
+          >
+            Set random name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
