c35893b8e5a99a1e876593d79b5064031d8a2d6f
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-16 13:02:35 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Clock from './components/Clock/Clock';
-import Button from './components/Button/Button';
 import './App.scss';
 
 class App extends React.Component {
@@ -9,42 +8,10 @@ class App extends React.Component {
     isClockVisible: true,
   }
 
-  messege = () => {
-    const oldName = this.state.clockName;
-    const newName = Math.floor(Math.random() * 1000);
-
-    this.setState({
-      clockName: newName,
-    });
-    // eslint-disable-next-line
-    console.log(`The Clock was renamed from ${oldName} to ${newName}`);
-  }
-
-  hide = () => {
-    this.setState({ isClockVisible: true });
-  }
-
-  show = () => {
-    this.setState({ isClockVisible: false });
-  }
-
   render() {
-    const { clockName } = this.state;
-
     return (
       <div className="App">
-        <h1>
-          React clock
-          {' '}
-          {clockName}
-        </h1>
         {this.state.isClockVisible && <Clock {...this.state} />}
-        <Button
-          messege={this.messege}
-          hide={this.hide}
-          show={this.show}
-          {...this.state}
-        />
       </div>
     );
   }
--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../Button/Button';
 
 class Clock extends React.Component {
   state = {
     date: new Date().toLocaleTimeString(),
+    clockName: this.props.clockName,
   }
 
   componentDidMount() {
@@ -21,16 +23,44 @@ class Clock extends React.Component {
     clearInterval(this.timer);
   }
 
-  render() {
+  messege = () => {
+    const oldName = this.state.clockName;
+    const newName = Math.floor(Math.random() * 1000);
 
+    this.setState({
+      clockName: newName,
+    });
+    // eslint-disable-next-line
+    console.log(`The Clock was renamed from ${oldName} to ${newName}`);
+  }
+
+  hide = () => {
+    this.setState({ isClockVisible: true });
+  }
+
+  show = () => {
+    this.setState({ isClockVisible: false });
+  }
+
+  render() {
     return (
       <>
+        <h1>
+          React clock
+          {' '}
+          {this.state.clockName}
+        </h1>
         <p>
           Current time:
           {' '}
           {this.state.date}
         </p>
-
+        <Button
+          messege={this.messege}
+          hide={this.hide}
+          show={this.show}
+          {...this.state}
+        />
       </>
     );
   }
@@ -38,6 +68,7 @@ class Clock extends React.Component {
 
 Clock.propTypes = {
   isClockVisible: PropTypes.bool.isRequired,
+  clockName: PropTypes.string.isRequired,
 };
 
 export default Clock;
