9da4d349bb329baf8e608d9e6875caae8e922604
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-20 11:58:05 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
+import { Clock } from './Components/Clock/Clock';
 
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
-
-export default App;
+export const App = () => (
+  <div className="app">
+    <h1>React clock</h1>
+    <Clock />
+  </div>
+);
