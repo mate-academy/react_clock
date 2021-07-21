badc0b762cf28e556122b2ce7b8095e133c53341
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-07-21 22:56:12 +0300

    solution

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString(),
+    };
+  }
+
+  componentDidMount() {
+    this.myTimer = setInterval(
+      () => (this.doStep()),
+      1000
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.name !== this.props.clockName) {
+      const oldName = prevProps.clockName;
+      const newName = this.props.clockName;
+
+      console.log(`The Clock was renamed from ${oldName} to ${newName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myTimer);
+  }
+
+  doStep() {
+    this.setState({
+      date: new Date().toLocaleTimeString(),
+    });
+    console.log(this.state.date);
+  }
+
+  render() {
+    return (
+      `${this.state.date}`
+    );
+  }
+
+}
