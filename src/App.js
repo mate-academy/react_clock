a4ea447e758d4cfe4bb2be9c36f8fc846d040eef
Artem <temaqwerty15@gmail.com>
2021-07-22 12:54:37 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -10,46 +10,34 @@ class App extends React.Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
+    if (prevState.clockName !== this.state.clockName) {
     // eslint-disable-next-line no-console
-    console.log(`The Clock was renamed from ${prevState.clockName}`
-    + ` to ${this.state.clockName} newName.`);
+      console.log(`The Clock was renamed from ${prevState.clockName}`
+        + ` to ${this.state.clockName} newName.`);
+    }
   }
 
   render() {
     const { clockName, isClockVisible } = this.state;
+    const randomName = () => {
+      this.setState({ clockName: Math.floor(Math.random() * 10000) });
+    };
+
+    const clockVisible = () => (this.setState({ isClockVisible: true }));
+
+    const clockInvisible = () => (this.setState({ isClockVisible: false }));
 
     return (
       <div className="App">
         <h1>React clock</h1>
         { isClockVisible && <Clock name={clockName} /> }
-        <button
-          type="button"
-          onClick={
-            () => {
-              this.setState({ isClockVisible: true });
-            }
-          }
-        >
+        <button type="button" onClick={clockVisible}>
           Show time
         </button>
-        <button
-          type="button"
-          onClick={
-            () => {
-              this.setState({ isClockVisible: false });
-            }
-          }
-        >
+        <button type="button" onClick={clockInvisible}>
           Hide time
         </button>
-        <button
-          type="button"
-          onClick={
-            () => {
-              this.setState({ clockName: Math.floor(Math.random() * 10000) });
-            }
-          }
-        >
+        <button type="button" onClick={randomName}>
           Change Name
         </button>
       </div>
