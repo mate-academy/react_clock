eeb27736c670173325dc7383cc4240bb8117b0a0
Pavlo Starynskyi <starinskiy@gmail.com>
2021-07-22 12:05:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    clockIsVisible: true,
+    clockName: 0,
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    const { clockIsVisible } = this.state;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    return (
+      <>
+        <div className="Container">
+          <h1 className="Title">React clock</h1>
+          <div className="App">
+            <div className="App__button-wrapper">
+              <button
+                type="button"
+                className="App__button"
+                onClick={() => {
+                  this.setState({ clockIsVisible: true });
+                }}
+              >
+                Show Clock
+              </button>
+              <button
+                type="button"
+                className="App__button"
+                onClick={() => {
+                  this.setState({ clockIsVisible: false });
+                }}
+              >
+                Hide Clock
+              </button>
+              <button
+                type="button"
+                className="App__button"
+                onClick={() => {
+                  const randomNumber = Math.ceil(Math.random() * 10);
+
+                  this.setState({ clockName: randomNumber });
+                }}
+              >
+                Set random name
+              </button>
+            </div>
+            { clockIsVisible && (
+            <Clock name={this.state.clockName} />
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
