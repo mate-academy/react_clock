b39081d6058726574997d5bb6ad991c2622e816d
Valeria0304 <lera_voloshina03@ukr.net>
2021-07-22 17:59:05 +0300

    Solution

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Clock extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.name === this.props.name) {
+      // eslint-disable-next-line
+      console.log(this.state.time);
+    } else {
+      // eslint-disable-next-line
+      console.log(
+        `The Clock was renamed from ${prevProps.name} to ${this.props.name}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return this.state.time;
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+};
