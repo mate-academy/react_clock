27f9cd5214ecaeee3370d522b7ced3e16756e4ff
Andrii Balitskyi <10balian10@gmail.com>
2021-07-22 16:39:16 +0200

    Clock implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
 
 import './App.scss';
+import { Clock } from './Clock';
 
-const App = () => {
-  setInterval(() => {
-    const date = new Date();
+class App extends React.Component {
+  state = {
+    isClockVisible: false,
+    clockName: 0,
+  }
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  render() {
+    const { isClockVisible, clockName } = this.state;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <button
+          type="button"
+          onClick={() => this.setState({ isClockVisible: true })}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.setState({ isClockVisible: false })}
+        >
+          Hide Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.setState({
+            clockName: Math.floor(Math.random() * 101),
+          })}
+        >
+          Set random name
+        </button>
+        {isClockVisible && <Clock name={clockName} />}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Clock extends React.Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.TimerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.log(`
+        The Clock was renamed from ${prevProps.name} to ${this.props.name}
+      `);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.TimerId);
+  }
+
+  tick() {
+    // eslint-disable-next-line no-console
+    console.log(this.state.date.toLocaleTimeString());
+
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <p>
+        Current time:
+        {' '}
+        {this.state.date.toLocaleTimeString()}
+      </p>
+    );
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number,
+};
+
+Clock.defaultProps = {
+  name: 0,
+};
