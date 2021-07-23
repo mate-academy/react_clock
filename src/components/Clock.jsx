8eda454cb8e95b9dc818af0a766f41c826876059
Kandiak Oleg <kandio1604@gmail.com>
2021-07-23 15:00:50 +0300

    solution

---
+++ src/components/Clock.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Clock extends Component {
+  state = {
+    date: null,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const date = new Date();
+
+      this.setState({ date: date.toLocaleTimeString() });
+      // eslint-disable-next-line
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.name !== prevProps.name) {
+      // eslint-disable-next-line
+      console.log(
+        `The Clock was renamed from ${prevProps.name} to ${this.props.name}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Current time:
+          {' '}
+          {this.state.date}
+        </p>
+      </div>
+    );
+  }
+}
+
+Clock.propTypes = {
+  name: PropTypes.number.isRequired,
+};
+
+export default Clock;
