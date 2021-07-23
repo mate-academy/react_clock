a8cc6ba7ced65bafb151772412149ee5a668cefb
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-23 13:02:26 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import './App.scss';
 
 class App extends React.Component {
   state = {
-    clockWisible: true,
+    clockVisible: true,
+    // eslint-disable-next-line react/no-unused-state
+    clockName: 'clock',
   }
 
-  wisibility = () => {
-    this.setState(prevState => ({
-      clockWisible: !prevState.clockWisible,
+  visibilityOn = () => {
+    this.setState(() => ({
+      clockVisible: true,
+    }));
+  }
+
+  visibilityOff = () => {
+    this.setState(() => ({
+      clockVisible: false,
+    }));
+  }
+
+  randomNameForClock = () => {
+    const name = Math.ceil(Math.random() * 10);
+
+    this.setState(() => ({
+      // eslint-disable-next-line react/no-unused-state
+      clockName: name,
     }));
   }
 
@@ -18,11 +35,24 @@ class App extends React.Component {
       <>
         <button
           type="button"
-          onClick={this.wisibility}
+          onClick={this.visibilityOn}
         >
-          clock
+          turn on clock
         </button>
-        { this.state.clockWisible && <Clock name={this.state} />}
+
+        <button
+          type="button"
+          onClick={this.visibilityOff}
+        >
+          turn off clock
+        </button>
+        <button
+          type="button"
+          onClick={this.randomNameForClock}
+        >
+          Set random name
+        </button>
+        { this.state.clockVisible && <Clock name={this.state} />}
       </>
     );
   }
--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 export class Clock extends React.Component {
   state = {
     date: undefined,
+    interval: undefined,
   }
 
   componentDidMount() {
     this.state.interval = setInterval(() => {
       this.setState({ date: new Date().toLocaleTimeString() });
+      // eslint-disable-next-line no-console
+      console.log(this.state.date);
     }, 1000);
   }
 
-  componentDidUpdate() {
-    // eslint-disable-next-line no-console
-    console.log(this.state.date);
+  componentDidUpdate(prevState) {
+    if (this.props.name.clockName !== prevState.name.clockName) {
+      // eslint-disable-next-line no-console
+      console.log(`The Clock was renamed from ${prevState.name.clockName}
+      to ${this.props.name.clockName}`);
+    }
   }
 
   componentWillUnmount() {
