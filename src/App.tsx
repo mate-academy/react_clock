3cec0a9209dfda94688b9cf762cf252e0fe51288
lera-yaskevych <redrum.back@gmail.com>
2021-08-31 16:59:41 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
 import React, { Props } from 'react';
 import './App.scss';
+import Clock from './components/Clock';
 
 type State = {
-  time: Date;
   isClockVisible: boolean;
   clockName: number;
 };
 
 class App extends React.Component<Props<State>, State> {
   state = {
-    time: new Date(),
     isClockVisible: false,
     clockName: 0,
   };
 
   componentDidMount() {
-    setInterval(() => {
-      this.state.time.toLocaleTimeString();
-      this.setState({ time: new Date() });
-    }, 1000);
-
     this.setState({ isClockVisible: true });
   }
 
   setRandomName = () => {
-    this.setState(({ clockName }) => {
-      const newName = Math.trunc(Math.random() * 10 + 1);
+    if (this.state.isClockVisible) {
+      this.setState((prevState) => {
+        const newName = Math.trunc(Math.random() * 10 + 1);
 
-      // eslint-disable-next-line no-console
-      console.log(`The Clock was renamed from ${clockName} to ${newName}`);
+        // eslint-disable-next-line no-console
+        console.log(`The Clock was renamed from ${prevState.clockName} to ${newName}`);
 
-      return {
-        clockName: newName,
-      };
-    });
+        return {
+          clockName: newName,
+        };
+      });
+    }
   };
 
   showClock = () => {
@@ -45,47 +41,48 @@ class App extends React.Component<Props<State>, State> {
   };
 
   render() {
-    const { time, isClockVisible, clockName } = this.state;
+    const { isClockVisible, clockName } = this.state;
     const { setRandomName, hideClock, showClock } = this;
 
     return (
       <div className="App">
-        <h1>React clock</h1>
-        <h2>
-          {clockName}
-        </h2>
+        <h1 className="App__title">
+          React clock
+        </h1>
+
         {isClockVisible ? (
-          <p>
-            Current time:
-            {' '}
-            {time.toLocaleTimeString()}
-            {// eslint-disable-next-line no-console
-              console.log(time.toLocaleTimeString())
-            }
-          </p>
+          <Clock name={clockName} />
         ) : (
-          <p>
+          <p className="App__hint">
             Press &quot;Show Clock&quot; button to see the clock
           </p>
         )}
-        <button
-          type="button"
-          onClick={showClock}
-        >
-          Show Clock
-        </button>
-        <button
-          type="button"
-          onClick={hideClock}
-        >
-          Hide Clock
-        </button>
-        <button
-          type="button"
-          onClick={setRandomName}
-        >
-          Set random name
-        </button>
+
+        <div className="App__buttons">
+          <button
+            type="button"
+            className="App__button"
+            onClick={showClock}
+          >
+            Show Clock
+          </button>
+
+          <button
+            type="button"
+            className="App__button"
+            onClick={hideClock}
+          >
+            Hide Clock
+          </button>
+
+          <button
+            type="button"
+            className="App__button"
+            onClick={setRandomName}
+          >
+            Set random name
+          </button>
+        </div>
       </div>
     );
   }
