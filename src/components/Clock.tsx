eeedc721495b782da32a1b5e1296d630ccad6fa4
lera-yaskevych <redrum.back@gmail.com>
2021-08-31 17:00:36 +0300

    done

---
+++ src/components/Clock.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Clock.scss';
+
+interface Props {
+  name: number,
+}
+
+type State = {
+  time: Date;
+};
+
+class Clock extends React.Component<Props, State> {
+  state = {
+    time: new Date(),
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.state.time.toLocaleTimeString();
+      this.setState({ time: new Date() });
+    }, 1000);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+
+    return (
+      <>
+        <div className="Clock">
+          <h2 className="Clock__name">
+            {name}
+          </h2>
+          <p className="Clock__time">
+            Current time:
+            {' '}
+            {time.toLocaleTimeString()}
+            {// eslint-disable-next-line no-console
+              console.log(time.toLocaleTimeString())
+            }
+          </p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Clock;
