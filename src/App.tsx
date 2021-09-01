33bb55baf7baf8b8157461b0ef4d1f8b07666012
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-09-01 18:32:32 +0300

    clock was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App: React.FC = () => {
-  setInterval(() => {
-    const date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: string;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: 'Default Clock name',
+  };
+
+  changeClockVisibility = () => {
+    this.setState((prevState) => ({ isClockVisible: !prevState.isClockVisible }));
+  };
+
+  changeName = () => {
+    this.setState({ clockName: Math.floor(Math.random() * 100000).toString() });
+  };
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">React clock</h1>
+        <p className="App__time">
+          Current time:
+          {' '}
+          {isClockVisible && <Clock name={clockName} />}
+        </p>
+        <div className="App__button-set">
+          <button type="button" className="App__button" onClick={this.changeClockVisibility}>{isClockVisible ? 'Hide clock' : 'Show clock'}</button>
+          <button type="button" className="App__button" onClick={this.changeName}>Set random name</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+type State = {
+  time: string;
+};
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: State = {
+    time: '',
+  };
+
+  timingFunction = setInterval(() => {
+    this.setTime();
+  }, 1000);
+
+  componentDidMount() {
+    return this.timingFunction;
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.name !== prevProps.name) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timingFunction);
+  }
+
+  setTime = () => {
+    const date = new Date();
+    const dateToString = date.toLocaleTimeString();
+
+    this.setState({ time: dateToString });
+    // eslint-disable-next-line
+    console.log(dateToString);
+  };
+
+  render() {
+    const { time } = this.state;
+
+    return time;
+  }
+}
