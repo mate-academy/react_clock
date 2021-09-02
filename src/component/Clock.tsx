cb344f1d5d3bb07654e8e9fe524718141d024d92
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-09-02 15:06:49 +0200

    react clock

---
+++ src/component/Clock.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+type Props = {
+  name: number;
+};
+
+type State = {
+  date: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    date: new Date(),
+  };
+
+  intervalId = setInterval(() => {
+    this.setState({ date: new Date() });
+  }, 1000);
+
+  componentDidMount() {
+    return this.intervalId;
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const oldName = prevProps.name;
+    const newName = this.state.date.toLocaleTimeString();
+
+    return oldName !== this.props.name
+    // eslint-disable-next-line
+    ? console.log(`Clock was renamed from ${oldName} to ${this.props.name}`)
+    // eslint-disable-next-line
+    : console.log(newName);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+    // eslint-disable-next-line
+    console.log(`Clock was stopped`);
+  }
+
+  render() {
+    const { name } = this.props;
+
+    return (
+      <>
+        <p className="App__clockName">
+          Current time №:
+          {name}
+        </p>
+        <p className="App__clockTime">
+          <b>{this.state.date.toLocaleTimeString()}</b>
+        </p>
+      </>
+    );
+  }
+}
