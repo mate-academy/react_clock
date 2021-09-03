18ce74af1b31412b00346076643b64eded729647
kristof22122 <kristf44@gmail.com>
2021-09-03 13:38:44 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  setInterval(() => {
+type Props = {
+
+};
+
+type State = {
+  isClockVisible: boolean,
+  clock: string,
+};
+
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clock: '',
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.isClockVisible) {
+        this.Clock();
+      } else {
+        this.hideClock();
+      }
+    }, 1000);
+  }
+
+  hideClock = () => {
+    this.state.isClockVisible = false;
+    this.setState({ clock: ' ' });
+  };
+
+  showClock = () => {
+    this.state.isClockVisible = true;
+  };
+
+  Clock = () => {
     const date = new Date();
 
     // eslint-disable-next-line
     console.log(date.toLocaleTimeString());
-  }, 1000);
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+    this.setState({ clock: date.toLocaleTimeString() });
+  };
+
+  render() {
+    const { clock } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {clock}
+        </p>
+
+        <button type="button" onClick={this.showClock}>
+          Show clock
+        </button>
+
+        <button type="button" onClick={this.hideClock}>
+          Hide clock
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
