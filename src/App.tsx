48c95c061ca9a69c94f332fc7c9189d1c1343f57
Roman Fedusevych <romanfyks@gmail.com>
2021-09-04 20:39:10 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  setInterval(() => {
-    const date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: string;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: 'React clock',
+  };
+
+  clockRandomNames = () => {
+    const randomNumbers = Math.floor(Math.random() * 1000);
+
+    return `React clock ${randomNumbers}`;
+  };
+
+  showClock = () => {
+    this.setState({ isClockVisible: true });
+  };
+
+  hideClock = () => {
+    this.setState({ isClockVisible: false });
+  };
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App__clock">
+          {isClockVisible && (
+            <Clock name={clockName} />
+          )}
+        </div>
+
+        <div className="App__buttons">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={this.showClock}
+          >
+            Show clock
+          </button>
+
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              this.setState({ clockName: this.clockRandomNames() });
+            }}
+          >
+            Set random name
+          </button>
+
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={this.hideClock}
+          >
+            Hide clock
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
