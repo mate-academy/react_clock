655793aa6b170fa5deb0f07c3d0fb10058d1a32d
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-09-06 13:48:43 +0300

    pilot

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
-const App: React.FC = () => {
-  setInterval(() => {
-    const date = new Date();
+function randomName() {
+  const num = Math.floor(Math.random() * 5 + 5);
+  let randomString = '';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+  for (let i = 0; i <= num; i += 1) {
+    randomString += String.fromCodePoint(Math.floor(26 * Math.random() + 97));
+  }
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+  return randomString;
+}
+
+type State = {
+  isClockVisible: boolean;
+  clockName: string | null;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: null,
+  };
+
+  render() {
+    const date = new Date();
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {isClockVisible && <Clock time={date.toLocaleTimeString()} name={clockName} />}
+        </p>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show Clock
+        </button>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide Clock
+        </button>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => {
+            this.setState({ clockName: randomName() });
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
