08d07530e630c506f48b3f5ec3f202ca2e0bd0f4
Vova-Stoliar <stoliarmoney@gmail.com>
2021-09-06 20:45:27 +0300

    changes were added

--- src/App.tsx
+++ src/App.tsx
@@ -5,68 +5,35 @@ import { Clock } from './Clock/Clock';
 export class App extends React.PureComponent {
   state = {
     isClockVisible: true,
-    time: new Date().toLocaleTimeString(),
     clockName: '0',
   };
 
-  clock = 0;
-
-  randomName = '';
-
-  componentDidMount() {
-    this.setState({ time: new Date().toLocaleTimeString() });
-
-    this.clock = window.setInterval(() => {
-      this.setState({ time: new Date().toLocaleTimeString() });
-      // eslint-disable-next-line no-console
-      console.log(this.state.time);
-    }, 1000);
-
-    this.setState({ isClockVisible: true });
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.clock);
-    this.setState({ isClockVisible: false });
-  }
-
-  setRandomName() {
-    this.randomName = Math.floor(Math.random() * 100).toString();
-    this.setState({ clockName: this.randomName });
-  }
-
   render() {
-    const { time, isClockVisible, clockName } = this.state;
+    const { clockName, isClockVisible } = this.state;
 
     return (
       <>
         <div className="App">
-          <h1>
-            <Clock name={clockName} />
-          </h1>
-          <p>
-            Current time:
-            {' '}
-            {isClockVisible ? time : null}
-          </p>
+          <Clock name={clockName} isClockVisible={isClockVisible} />
+
           <button
             type="button"
             className="App__visible"
-            onClick={() => this.componentDidMount()}
+            onClick={() => this.setState({ isClockVisible: true })}
           >
             Show Clock
           </button>
           <button
             type="button"
             className="App__hidden"
-            onClick={() => this.componentWillUnmount()}
+            onClick={() => this.setState({ isClockVisible: false })}
           >
             Hide Clock
           </button>
           <button
             type="button"
             className="App__random-name"
-            onClick={() => this.setRandomName()}
+            onClick={() => this.setState({ clockName: Math.floor(Math.random() * 100).toString() })}
           >
             Random name
           </button>
--- src/Clock/Clock.tsx
+++ src/Clock/Clock.tsx
@@ -1,35 +1,71 @@
+/* eslint-disable no-console */
 import React from 'react';
 
 type Props = {
   name: string,
+  isClockVisible: boolean,
 };
 
-export class Clock extends React.PureComponent<Props> {
+type State = {
+  clockName: string,
+  time: string,
+};
+
+export class Clock extends React.PureComponent<Props, State> {
   newClockName = '';
 
   clockTypeName = '0';
 
   state = {
     clockName: 'React Clock:',
+    time: new Date().toLocaleTimeString(),
   };
 
-  render() {
-    const clockNamePrev = `The Clock was renamed from ${this.clockTypeName}`;
+  clock = 0;
 
-    const { name } = this.props;
+  componentDidMount() {
+    this.setState({ time: new Date().toLocaleTimeString() });
+
+    this.clock = window.setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+      console.log(this.state.time);
+    }, 1000);
+  }
+
+  componentDidUpdate(prev:Props) {
+    if (!prev.isClockVisible) {
+      this.componentDidMount();
+    }
+  }
+
+  render() {
+    const clockNamePrev = `${this.clockTypeName}`;
+    const { name, isClockVisible } = this.props;
 
     this.clockTypeName = name;
 
-    if (Number(this.clockTypeName)) {
-      // eslint-disable-next-line no-console
-      console.log(`${clockNamePrev} to ${this.clockTypeName}`);
+    if (this.clockTypeName !== clockNamePrev) {
+      console.log(`The Clock was renamed from ${clockNamePrev} to ${this.clockTypeName}`);
+    }
+
+    if (!isClockVisible) {
+      clearInterval(this.clock);
     }
 
     this.newClockName = `${this.state.clockName} ${name}`;
 
     return (
       <>
-        {this.newClockName}
+        {isClockVisible && (
+          <>
+            <h1>
+              {this.newClockName}
+            </h1>
+            <p>
+              {`Current time: ${this.state.time}`}
+            </p>
+          </>
+        )}
       </>
     );
   }
