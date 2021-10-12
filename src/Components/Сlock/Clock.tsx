5b47c20fe0677d14cbd1b46385a99308de57845b
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-10-12 16:10:47 +0300

    clock

---
+++ src/Components/Сlock/Clock.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type Props = {
+  name: number;
+};
+
+type State = {
+  clock: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  private timer?: number;
+
+  state = {
+    clock: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    this.timer = window.setInterval(() => {
+      this.setState({ clock: new Date().toLocaleTimeString() });
+      // eslint-disable-next-line no-console
+      console.log(new Date().toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return (
+      <div>{this.state.clock}</div>
+    );
+  }
+}
