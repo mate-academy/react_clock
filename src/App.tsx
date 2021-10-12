506cdd003ce933c04401082e8cc1fa2198b8ecd6
Igor <i.kornienko1199@gmail.com>
2021-10-12 11:56:22 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -45,24 +45,4 @@ class App extends React.Component<{}, State> {
   }
 }
 
-// const App1: React.FC = () => {
-//   window.setInterval(() => {
-//     const date = new Date();
-//
-//     // eslint-disable-next-line
-//     console.log(date.toLocaleTimeString());
-//   }, 1000);
-//
-//   return (
-//     <div className="App">
-//       <h1>React clock</h1>
-//       <p>
-//         Current time:
-//         {' '}
-//         {/* Print the time here instead of DevTools */}
-//       </p>
-//     </div>
-//   );
-// };
-
 export default App;
