ac1976e871cf8d63cb835010c21d0e3088ba74eb
Dima Anosov <anosov.work@gmail.com>
2021-10-13 11:47:52 +0300

    Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  window.setInterval(() => {
-    const date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: number;
 };
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 11,
+  };
+
+  render() {
+    const {
+      isClockVisible,
+      clockName,
+    } = this.state;
+
+    return (
+      <div className="page">
+        <div className="clook">
+          <div className="clock__info">
+            <h1>React clock</h1>
+            <span>
+              Clook
+              {clockName}
+            </span>
+            <span>
+              Current time:
+            </span>
+            <span>
+              {isClockVisible && <Clock name={clockName} />}
+            </span>
+          </div>
+          <div className="clock__nav">
+            <button
+              type="button"
+              className="clock--visible"
+              onClick={() => this.setState({ isClockVisible: true })}
+            >
+              Show clock
+            </button>
+            <button
+              type="button"
+              className="clock--hidden"
+              onClick={() => this.setState({ isClockVisible: false })}
+            >
+              Hide clock
+            </button>
+            <button
+              type="button"
+              className="clock--name"
+              onClick={() => this.setState({ clockName: Math.round(Math.random() * 100) })}
+            >
+              Randomize Name
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
