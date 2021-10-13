9efd7302440f1a1400baf364d767e1d9b1ec0cea
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-10-14 00:22:55 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+interface State {
+  isClockVisible: boolean,
+  clockName: number,
+}
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+type Props = {};
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  };
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <>
+        <h1>
+          Clock name:
+          {` ${this.state.clockName}`}
+        </h1>
+        <p>
+          Current time:
+          {' '}
+          {isClockVisible && (
+            <Clock name={clockName} />
+          )}
+        </p>
+
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.setState({
+              isClockVisible: true,
+            });
+          }}
+        >
+          Show Clock
+        </button>
+
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.setState({
+              isClockVisible: false,
+            });
+          }}
+        >
+          Hide Clock
+        </button>
+
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.setState({
+              clockName: Math.round(Math.random() * 1000) + 1,
+            });
+          }}
+        >
+          Set random name
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
