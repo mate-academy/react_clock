547b8383b3ed95151b2f5e636c3a22c53762ac54
Anton Forzun <anton_forzun@ukr.net>
2021-10-26 11:23:25 +0300

    react_clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import Clock from './component/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+export default class App extends React.Component {
+  state = {
+    isClockVisible: true,
+  };
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>React Clock</h1>
+        <button type="button" onClick={() => this.setState({ isClockVisible: true })}>
+          Show Clock
+        </button>
+        <button type="button" onClick={() => this.setState({ isClockVisible: false })}>
+          Hide Clock
+        </button>
+        {this.state.isClockVisible && <Clock />}
+      </div>
+    );
+  }
+}
 
-export default App;
+// const App: React.FC = () => {
+//   const timerId: NodeJS.Timer = setInterval(() => {
+//     const date: Date = new Date();
+
+//     // eslint-disable-next-line
+//     console.log(date.toLocaleTimeString());
+//   }, 1000);
