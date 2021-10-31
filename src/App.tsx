85d1f3b3f8951319f56bc781cc9b36387ec96324
Roman Minchenko <minchenko.poma@gmail.com>
2021-10-31 08:50:04 +0200

    fix-listener

--- src/App.tsx
+++ src/App.tsx
@@ -16,30 +16,6 @@ class App extends React.Component<Props, State> {
     randomIndex: 0,
   };
 
-  componentDidMount() {
-    document.querySelector('.watch__display')?.addEventListener('click', () => {
-      this.setState((state) => {
-        return { isClockVisible: !state.isClockVisible };
-      });
-    });
-
-    document.querySelector('.watch__button-style')?.addEventListener('click', () => {
-      function newRandomIndex(i:number): number {
-        const y = Math.floor(Math.random() * 5);
-
-        if (i !== y) {
-          return y;
-        }
-
-        return newRandomIndex(i);
-      }
-
-      this.setState((state) => {
-        return { randomIndex: newRandomIndex(state.randomIndex) };
-      });
-    });
-  }
-
   render() {
     const { isClockVisible, randomIndex } = this.state;
     const watch = RandomStyles[randomIndex];
@@ -50,13 +26,44 @@ class App extends React.Component<Props, State> {
           type="button"
           className={`watch__display display display--${isClockVisible && randomIndex}`}
           style={isClockVisible ? watch.styles : { backgroundColor: 'black' }}
+          onClick={
+            () => {
+              this.setState((state) => {
+                return { isClockVisible: !state.isClockVisible };
+              });
+            }
+          }
         >
           {
             isClockVisible
               && <OpenWatch watch={watch} />
           }
         </button>
-        <button className="watch__button-style" type="button"> </button>
+        <button
+          className="watch__button-style"
+          type="button"
+          onClick={
+            () => {
+              function newRandomIndex(i:number): number {
+                const y = Math.floor(Math.random() * 5);
+
+                if (i !== y) {
+                  return y;
+                }
+
+                return newRandomIndex(i);
+              }
+
+              this.setState((state) => {
+                return { randomIndex: newRandomIndex(state.randomIndex) };
+              });
+            }
+          }
+        >
+          {
+            ' '
+          }
+        </button>
       </div>
     );
   }
