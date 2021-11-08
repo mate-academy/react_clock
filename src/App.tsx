5f18cf3b06935d0a290eb2b5c69f94091139f671
Viktormaz <vitecc91@gmail.com>
2021-11-08 23:13:47 +0200

    one more solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Clock } from './components/Clock';
 class App extends React.Component {
   state = {
     isClockVisible: true,
+    clockName: 0,
   };
 
   timerOn = () => {
@@ -16,11 +17,17 @@ class App extends React.Component {
     this.setState({ isClockVisible: false });
   };
 
+  changeName = () => {
+    this.setState({ clockName: Math.floor(Math.random() * 100) });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React clock</h1>
-        {this.state.isClockVisible && <Clock />}
+        {this.state.isClockVisible && (
+          <Clock name={this.state.clockName} />
+        )}
 
         <button type="button" onClick={this.timerOn}>
           Show Clock
@@ -29,6 +36,12 @@ class App extends React.Component {
         <button type="button" onClick={this.timerOff}>
           Hide Clock
         </button>
+
+        <p>Random name</p>
+        <button type="button" onClick={this.changeName}>
+          Set random name
+        </button>
+        <p>{`clockName: ${this.state.clockName}`}</p>
       </div>
     );
   }
