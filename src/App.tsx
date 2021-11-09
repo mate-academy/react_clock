e6620fa6621664a92f0ae6cfe8d721820ff9e0d3
Volodymyr Taranin <jaffplay@gmail.com>
2021-11-09 13:20:13 +0200

    clock created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
+type Props = {};
+interface State {
+  time: string;
+}
+
+class App extends React.Component<Props, State> {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  timerId: NodeJS.Timer = setInterval(() => {
     const date: Date = new Date();
 
     // eslint-disable-next-line
     console.log(date.toLocaleTimeString());
   }, 1000);
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {time}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
