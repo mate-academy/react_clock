d383babe71a83c663492621df6bbc0d0abfe7eb5
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-09 12:52:22 +0200

    solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface Props {
+  clockName: number;
+}
+
+interface State {
+  currentDate: Date;
+}
+
+export class Clock extends React.Component<Props, State> {
+  timerId?: NodeJS.Timeout | undefined;
+
+  state: State = {
+    currentDate: new Date(),
+  };
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ currentDate: new Date() });
+
+      // eslint-disable-next-line no-console
+      console.log(this.state.currentDate.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.clockName !== prevProps.clockName) {
+      // eslint-disable-next-line no-console
+      console.log(`The Clock was renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { currentDate } = this.state;
+
+    return (
+      <p className="App__clock">
+        Current time:
+        {' '}
+        {currentDate.toLocaleTimeString()}
+      </p>
+    );
+  }
+}
