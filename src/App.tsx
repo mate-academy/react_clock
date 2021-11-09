f0347812308b455eef3507256b8f737167fa19d9
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-09 17:02:06 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,127 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+type Props = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+interface State {
+  isVisible: boolean;
+  clockName: number | string;
+}
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+class App extends React.Component<Props, State> {
+  state: State = {
+    isVisible: true,
+    clockName: 0,
+  };
+
+  visible = () => {
+    this.setState({ isVisible: true });
+  };
+
+  hidden = () => {
+    this.setState({ isVisible: false });
+  };
+
+  random = () => {
+    this.setState({ clockName: uuid() });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className="clock__title">React Clock</h1>
+        <button
+          className="clock__button"
+          type="button"
+          onClick={this.hidden}
+        >
+          Hide
+        </button>
+        <button
+          className="clock__button"
+          type="button"
+          onClick={this.visible}
+        >
+          Visible
+        </button>
+        <button
+          className="clock__button"
+          type="button"
+          onClick={this.random}
+        >
+          Change Name
+        </button>
+        <h2 className="clock__show">
+          Time is:
+          {
+            this.state.isVisible === true
+              ? (<Clock clockName={this.state.clockName} />)
+              : (null)
+          }
+        </h2>
+      </div>
+    );
+  }
+}
+
+//
+//
+// class App extends React.Component<Props, State> {
+//   state: State = {
+//     isVisible: true,
+//     clockName: 0,
+//   };
+//
+//   visible = () => {
+//     this.setState({ isVisible: true });
+//   };
+//
+//   hidden = () => {
+//     this.setState({ isVisible: false });
+//   };
+//
+//   random = () => {
+//     this.setState({ clockName: Math.floor(Math.random() * (100 - 1)) + 1 });
+//   };
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>React Clock</h1>
+//         <button
+//           type="button"
+//           className="button"
+//           onClick={this.visible}
+//         >
+//           Visible
+//         </button>
+//         <button
+//           type="button"
+//           className="button"
+//           onClick={this.hidden}
+//         >
+//           Invisible
+//         </button>
+//         <button
+//           type="button"
+//           className="button"
+//           onClick={this.random}
+//         >
+//           Random Name
+//         </button>
+//         <h2>
+//           Time is:
+//           {
+//             this.state.isVisible === true
+//               ? (<Clock clockName={this.state.clockName} />)
+//               : (null)
+//           }
+//         </h2>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
