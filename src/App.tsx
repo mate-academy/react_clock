9cfeea64d80fad34a0dab2ba6c104f97191051ac
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-24 00:14:22 +0200

    Create swiss watch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
+
+import { Clock } from './components/Clock';
+
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: number;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: Math.floor(Math.random() * 100),
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  renameClock = () => {
+    this.setState({
+      clockName: Math.floor(Math.random() * 100),
+    });
+  };
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="App">
+        {isClockVisible && <Clock clockName={clockName} />}
+
+        <div className="buttons">
+          <button type="button" onClick={this.showClock}>
+            Show Clock
+          </button>
+          <button type="button" onClick={this.hideClock}>
+            Hide Clock
+          </button>
+          <button type="button" onClick={this.renameClock}>
+            Rename Clock
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
