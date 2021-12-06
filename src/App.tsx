f7ab158ef78a21bf3ef5a451a7b335149840f73d
Lera Litfullina <litfullina@icloud.com>
2021-12-06 19:50:24 +0300

    Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockName: number,
 };
+
+class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    clockName: Math.floor(Math.random() * 100),
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  setRandomName = () => {
+    this.setState({
+      clockName: Math.floor(Math.random() * 100),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {this.state.isClockVisible && <Clock name={this.state.clockName} />}
+        <button type="button" disabled={!this.state.isClockVisible} onClick={this.hideClock}>
+          Hide Clock
+        </button>
+        <button type="button" disabled={this.state.isClockVisible} onClick={this.showClock}>
+          Show Clock
+        </button>
+        <button type="button" onClick={this.setRandomName}>
+          Set name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+type State = {
+  currentTime: string
+};
+
+type Props = {
+  name: number
+};
+
+export class Clock extends React.Component<Props, State> {
+  timerId: NodeJS.Timer = setInterval(() => {}, 0);
+
+  state = {
+    currentTime: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ currentTime: new Date().toLocaleTimeString() });
+      console.log(this.state.currentTime);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevName: Props) {
+    const newName = this.props;
+
+    if (prevName !== newName) {
+      console.log(newName);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { currentTime } = this.state;
+
+    return (
+      <>
+        <div className="clock">
+          <p className="clock__time">{currentTime}</p>
+        </div>
+      </>
+    );
+  }
+}
