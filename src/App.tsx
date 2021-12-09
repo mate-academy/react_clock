a948784d37b20c82160009942bf8c10964011cd9
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-09 21:31:00 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
 import React from 'react';
+import { Clock } from './components/Clock';
+
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  nameId: number;
 };
 
-export default App;
+export const names = [
+  'Christmas Clock',
+  'Ho Ho Ho',
+  'Happy New Year',
+  'Merry Christmas',
+  'Christmas is in the Air',
+  'O Christmas Tree',
+  'Good luck',
+  'Christmas is Coming',
+  'Ding dong',
+  'Santa’s Little Helper',
+  'Jingle All the Way',
+];
+
+export class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    nameId: 0,
+  };
+
+  hideClock = () => {
+    this.setState({ isClockVisible: false });
+  };
+
+  showClock = () => {
+    this.setState({ isClockVisible: true });
+  };
+
+  changeName = () => {
+    const randomizer = Math.ceil(Math.random() * 10);
+
+    this.setState({ nameId: randomizer });
+  };
+
+  render() {
+    const { isClockVisible, nameId } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          {
+            nameId
+              ? names[nameId]
+              : 'Christmas Clock'
+          }
+        </h1>
+        {
+          isClockVisible
+          && (
+            <p className="app__clock">
+              <Clock nameId={this.state.nameId} />
+            </p>
+          )
+        }
+        <div className="app__buttons">
+          <button
+            className="app__button"
+            type="button"
+            disabled={!isClockVisible}
+            onClick={this.hideClock}
+          >
+            Hide Clock
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            disabled={isClockVisible}
+            onClick={this.showClock}
+          >
+            Show Clock
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.changeName}
+          >
+            Secret Button
+          </button>
+        </div>
+
+      </div>
+    );
+  }
+}
