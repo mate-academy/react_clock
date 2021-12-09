1a40dd5425b06bdada6ef692ac2489e5a51a623c
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-12-09 16:17:00 +0200

    solution

---
+++ src/clockNames.ts
@@ -0,0 +1,21 @@
+const randomClockNames = [
+  'What are you doing?',
+  'What?!',
+  'Ouch!',
+  'Stop!',
+  'Click!',
+  'Click! Click!',
+  'Click! Click! Click!',
+  'One more time and I\'ll tell your mom that\'s you ate all candies',
+  'What\'s wrong with you?',
+  'Okay...',
+  'Fine. Name the price.',
+  'Am I a joke to you?',
+  'I\'m calling the police!',
+  'Long story short...',
+  'HELP!!!',
+  'Look what you\'ve done!',
+  'Now I need whiskey',
+];
+
+export default randomClockNames;
