7da64faef9466c90ed50ecde063d240d58738026
nadiaH <nadiahrb79@gmailcom>
2021-12-09 19:58:05 +0200

    solution

---
+++ src/Clock.tsx
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+
+type State = {
+  time: string;
+};
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends Component<Props, State> {
+  timerId: NodeJS.Timer = setInterval(() => {}, 0);
+
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+      console.log(this.state.time);
+    }, 1000);
+  }
+
+  componentDidUpdate(oldName: Props) {
+    const newName = this.props;
+
+    if (oldName !== newName) {
+      console.log(`The Clock was renamed from ${oldName.name} to ${newName.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return (
+      <p>{time}</p>
+    );
+  }
+}
