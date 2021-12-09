e400fd0c925aa356781d46038a0ed0337c388a79
lanebx <havchikos@gmail.com>
2021-12-09 14:20:50 +0200

    added Clock block

---
+++ src/components/Clock.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+type Props = {
+  name: number,
+};
+
+type State = {
+  time: string,
+};
+
+export class Clock extends React.Component<Props, State> {
+  timerId?: NodeJS.Timer;
+
+  state: State = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  componentDidMount() {
+    let { name }: Props = this.props;
+
+    this.timerId = setInterval(() => {
+      const newName = this.props.name;
+
+      if (name !== newName) {
+        console.log(`The Clock was renamed from ${name} to ${newName}`);
+        name = newName;
+      }
+
+      console.log(this.state.time);
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log(`the time was hidden at ${this.state.time}`);
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.time}
+      </>
+    );
+  }
+}
