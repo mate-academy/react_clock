25db76784583e9a2a1ff311795176e2ef6b24515
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-12-09 15:44:10 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockNameNumber: number,
 };
+
+class App extends React.Component<{}, State> {
+  // const timerId: NodeJS.Timer = setInterval(() => {
+  //   const date: Date = new Date();
+
+  //   // eslint-disable-next-line
+  //   console.log(date.toLocaleTimeString());
+  // }, 1000);
+
+  state: State = {
+    isClockVisible: true,
+    clockNameNumber: 1,
+  };
+
+  render() {
+    const { isClockVisible, clockNameNumber } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          <strong>
+            {`Clock #${clockNameNumber}`}
+          </strong>
+        </p>
+        {isClockVisible && (
+          <Clock
+            clockName={`Clock #${clockNameNumber}`}
+            isVisible={isClockVisible} />
+        )}
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ isClockVisible: true });
+            }}
+          >
+            Show Clock
+          </button>
+          {' '}
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ isClockVisible: false });
+            }}
+          >
+            Hide Clock
+          </button>
+          {' '}
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ clockNameNumber: Math.floor(Math.random() * 100) });
+            }}
+          >
+            Set random name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
