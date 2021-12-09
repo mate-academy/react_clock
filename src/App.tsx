d7c81b20185d4c47909940b183416fc205bf4b87
lanebx <havchikos@gmail.com>
2021-12-09 14:20:23 +0200

    app block changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockName: number;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: 1,
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  setRandomName = () => {
+    this.setState({
+      clockName: Math.floor(Math.random() * 100),
+    });
+  };
+
+  render() {
+    return (
+      <div className="clock-container">
+        <div className="clock">
+          <h1 className="clock__title">
+            React clock
+          </h1>
+
+          <h3 className="clock__subtitle">
+            Current time:
+          </h3>
+
+          <div className="time">
+            { this.state.isClockVisible && <Clock name={this.state.clockName} /> }
+          </div>
+
+          <button
+            className="button"
+            type="button"
+            onClick={this.showClock}
+          >
+            show clock
+          </button>
+
+          <button
+            className="button"
+            type="button"
+            onClick={this.hideClock}
+          >
+            hide clock
+          </button>
+
+          <button
+            className="button"
+            type="button"
+            onClick={this.setRandomName}
+          >
+            set random name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
