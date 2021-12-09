1a7bacbf4010bc88445da8868c7fc78d3c42c9f3
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-09 16:21:42 +0200

    without styles

---
+++ src/components/Clock.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type State = {
+  currentTime: string | null;
+};
+
+export class Clock extends React.Component<{}, State> {
+  state = {
+    currentTime: null,
+  };
+
+  componentDidMount() {
+    this.setState({ currentTime: new Date().toLocaleTimeString() });
+  }
+
+  componentDidUpdate() {
+    setInterval(() => {
+      this.setState({ currentTime: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  // timerId: NodeJS.Timer = setInterval(() => {
+  //   const date: Date = new Date();
+
+  //   // eslint-disable-next-line
+  //   console.log(date.toLocaleTimeString());
+
+  //   return date.toLocaleTimeString();
+  // }, 1000);
+  render() {
+    return (
+      <>
+        Current time:
+        {' '}
+        {this.state.currentTime}
+      </>
+    );
+  }
+}
