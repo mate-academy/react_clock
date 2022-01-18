523b08152c425835bf5f0a29975a263f7a7f4394
Denys Sierhiievskyi <luckdenny@gmail.comecho>
2022-01-18 17:42:59 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isVisible: boolean;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isVisible: true,
+  };
+
+  hideClock = () => {
+    this.setState({ isVisible: false });
+  };
+
+  showClock = () => {
+    this.setState({ isVisible: true });
+  };
+
+  render(): React.ReactNode {
+    const { isVisible } = this.state;
+
+    return (
+      <>
+        <h1 className="App__title">React clock</h1>
+        <div className="App">
+          <p className="center">
+            {' '}
+            <button
+              className="App__button-on"
+              type="button"
+              onClick={this.showClock}
+            >
+              <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+              </svg>
+              <span>Show Clock</span>
+            </button>
+          </p>
+        </div>
+        <br />
+        <br />
+        <div className="app2">
+          <p className="center">
+            <button
+              className="App__button-off"
+              type="button"
+              onClick={this.hideClock}
+            >
+              <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+              </svg>
+              <span>Hide Clock</span>
+            </button>
+            <div className="clock">
+              {isVisible && <Clock />}
+            </div>
+          </p>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
