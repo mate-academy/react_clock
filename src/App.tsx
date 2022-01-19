2bc383b94222e21d8df67df72e084ace02b75e47
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2022-01-19 17:29:12 +0200

    solution_v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { uniqueNamesGenerator, Config, colors } from 'unique-names-generator';
+
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: string;
 };
+
+const config: Config = {
+  dictionaries: [colors],
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+    clockName: '',
+  };
+
+  showClockHandler = () => {
+    this.setState(prevState => ({
+      isClockVisible: !prevState.isClockVisible,
+    }));
+  };
+
+  randomNameClickHadler = () => {
+    this.setState({
+      clockName: uniqueNamesGenerator(config),
+    });
+  };
+
+  render() {
+    return (
+      <Card className="card">
+        <Card.Body>
+          <Button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => this.showClockHandler()}
+          >
+            {this.state.isClockVisible ? 'Hide Clock' : 'Show Clock'}
+          </Button>
+          <Button
+            className="btn btn-secondary"
+            type="button"
+            onClick={() => this.randomNameClickHadler()}
+          >
+            Set random name
+          </Button>
+
+          {this.state.isClockVisible
+            ? <Clock title={this.state.clockName} />
+            : (
+              <Card.Title
+                className="title"
+              >
+                Click button above if you need to know correct time
+              </Card.Title>
+            )}
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+type Props = {
+  title: string;
+};
+
+type State = {
+  date: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: State = {
+    date: new Date(),
+  };
+
+  timerId?: NodeJS.Timeout;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tic();
+      // eslint-disable-next-line
+      console.log(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.title !== this.props.title) {
+      // eslint-disable-next-line
+      console.log('The Clock was renamed from oldName to newName');
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  tic = () => {
+    this.setState({
+      date: new Date(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Card.Title>
+          {this.props.title
+            ? `${this.props.title} clock`
+            : 'Outstanding Clock)))'}
+        </Card.Title>
+        <Card.Subtitle>
+          <em>
+            Current time:
+          </em>
+          {' '}
+          <strong style={{ color: this.props.title }}>
+            {this.state.date.toLocaleTimeString()}
+          </strong>
+        </Card.Subtitle>
+      </>
+    );
+  }
+}
