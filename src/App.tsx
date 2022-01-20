a37ee6d2f97dad72e55540d2cf6e594d7789d98a
Nikita Polishchuk <theetempleovpsychicyouth@gmail.com>
2022-01-20 13:11:34 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
 };
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isClockVisible: true,
+  };
+
+  ShowButton = () => {
+    this.setState({ isClockVisible: true });
+  };
+
+  HideButton = () => {
+    this.setState({ isClockVisible: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={this.ShowButton}>
+          Show Clock
+        </button>
+
+        <button type="button" onClick={this.HideButton}>
+          Hide Clock
+        </button>
+
+        {this.state.isClockVisible && <Clock />}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type State = {
+  date: Date;
+};
+
+export class Clock extends React.Component<{}, State> {
+  state = {
+    date: new Date(),
+  };
+
+  timerId?: NodeJS.Timeout;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date() });
+
+      // eslint-disable-next-line
+      console.log(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {this.state.date.toLocaleTimeString()}
+        </p>
+      </>
+    );
+  }
+}
