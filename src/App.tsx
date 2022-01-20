675308a72bcbe1c49a556a5830c032cd2af1eeed
Artem <tema.maksimovich@gmail.com>
2022-01-20 12:58:43 +0200

    Task is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import './App.scss';
+import Clock from './Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean
 };
+
+class App extends React.Component {
+  state: State = {
+    isClockVisible: true,
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {this.state.isClockVisible && <Clock />}
+        </p>
+
+        <button
+          type="button"
+          onClick={this.showClock}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          onClick={this.hideClock}
+        >
+          Hide Clock
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Clock.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.scss';
+
+type State = {
+  date: Date,
+};
+
+class Clock extends React.Component {
+  state: State = {
+    date: new Date(),
+  };
+
+  timerId?: NodeJS.Timeout;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+
+      // eslint-disable-next-line
+      console.log(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      this.state.date.toLocaleTimeString()
+    );
+  }
+}
+
+export default Clock;
