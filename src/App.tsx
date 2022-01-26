3393be6ce4b2065ca4b825e090e26eb9534ea549
Inna <inna_temis@ukr.net>
2022-01-26 20:36:46 +0200

    React-Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  number: number,
 };
+
+const getRandom = ():number => {
+  return Math.floor(Math.random() * 100 + 10);
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    number: 2,
+  };
+
+  getShowClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  getHideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  getChangeNumber = () => {
+    this.setState({
+      number: getRandom(),
+    });
+  };
+
+  render() {
+    const { isClockVisible, number } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <div>
+          Current time:
+          { isClockVisible && <Clock name={number} />}
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={this.getShowClock}
+          >
+            ShowClock
+          </button>
+          <button
+            type="button"
+            onClick={this.getHideClock}
+          >
+            HideClock
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={this.getChangeNumber}
+          disabled={!isClockVisible}
+        >
+          Get Random Number
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Clock/Clock.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type ClockState = {
+  time: Date,
+};
+
+type ClockProps = {
+  name: number,
+};
+
+export class Clock extends React.Component<ClockProps, ClockState> {
+  state = {
+    time: new Date(),
+  };
+
+  timerId: NodeJS.Timer = setInterval(() => {}, 1000);
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.setState({ time: new Date() }), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { time } = this.state;
+    const { name } = this.props;
+
+    return (
+      <>
+        <p>
+          {time.toLocaleTimeString()}
+        </p>
+        <p>
+          {`Random number: ${name}`}
+        </p>
+      </>
+    );
+  }
+}
