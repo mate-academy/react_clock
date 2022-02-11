9303345e3546d384e6c4e2889ffab3fb169d78ff
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-02-11 19:04:44 +0200

    main task was made

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-console */
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type ClockProps = {
+  name: string | number;
 };
+
+type ClockStates = {
+  date: string;
+};
+
+class Clock extends React.Component<ClockProps, ClockStates> {
+  state = {
+    date: (new Date()).toLocaleTimeString(),
+  };
+
+  timerId: NodeJS.Timeout;
+
+  constructor(props: any) {
+    super(props);
+    this.timerId = setInterval(() => {
+      this.setState({ date: (new Date()).toLocaleTimeString() });
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidMount() {
+    return this.timerId;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render(): React.ReactNode {
+    return (
+      <span>
+        {this.state.date}
+      </span>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  };
+
+  render(): React.ReactNode {
+    const { isClockVisible } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <p>
+          Current time:
+          {' '}
+          {isClockVisible
+            && <Clock name={this.state.clockName} />}
+        </p>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: true});
+          }}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false});
+          }}
+        >
+          Hide Clock
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false});
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
