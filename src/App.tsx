cf6f1dd1c599648712592b54c01662e522919985
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-02-15 12:01:27 +0200

    Create react-clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type Toggle = {
+  toggle: boolean;
+  clockName: number | null;
 };
+
+class App extends React.Component<{}, Toggle> {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    toggle: true,
+    clockName: null,
+  };
+
+  render(): React.ReactNode {
+    const { toggle, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {clockName && (
+          <h2>{`Clock's name: ${clockName}`}</h2>
+        )}
+        {toggle && (
+          <p>
+            Current time:
+            {' '}
+            <Clock name={clockName} />
+          </p>
+        )}
+
+        <button
+          type="button"
+          onClick={() => this.setState({ toggle: true })}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.setState({ toggle: false })}
+        >
+          Hide Clock
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ clockName: Math.floor(Math.random() * 30) });
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
