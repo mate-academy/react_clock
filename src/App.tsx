63de09a274c2489b165a5e5f36b87887e6fc4a33
AndryKK <korbutiaktso@gmail.com>
2022-02-16 19:19:37 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import './App.scss';
+import Clock from './components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+// eslint-disable-next-line
+class App extends React.Component {
+  state = {
+    hideShow: true,
+    name: ['coll', 'my', 'home', 'bad', 'like', 'rolex', 'cina', 'goods', 'floor', 'hight'],
+    i: 0,
+  };
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          React clock
+          {' '}
+          {this.state.name[this.state.i]}
+        </h1>
+        {this.state.hideShow && <Clock />}
+        <button type="button" onClick={() => this.setState({ hideShow: true })}>Show Clock</button>
+        <button type="button" onClick={() => this.setState({ hideShow: false })}>Hide Clock</button>
+        <button type="button" onClick={() => this.setState({ i: Math.floor(Math.random() * 10) })}>Set random name</button>
+      </div>
+    );
+  }
+}
 
 export default App;
