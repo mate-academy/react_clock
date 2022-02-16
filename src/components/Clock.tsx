7c754b473d8c32a815ab086ff512fbad83ea736f
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-02-16 11:40:46 +0200

    fixes

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
+import { setTimeout } from 'timers';
 
 export class Clock extends React.Component {
   state = {
-    date: new Date().toLocaleTimeString(),
-    timerId: setInterval(() => {}, 0),
+    time: new Date().toLocaleTimeString(),
+    timerId: setTimeout(() => {}, 0),
   };
 
   componentDidMount() {
-    this.state.timerId = setInterval(() => {
-      this.setState({ date: new Date().toLocaleTimeString() });
-      // eslint-disable-next-line no-console
-      console.log(this.state.date);
-    }, 1000);
+    this.setState({
+      timerId: setInterval(() => {
+        const date: Date = new Date();
+
+        // eslint-disable-next-line no-console
+        console.log(date.toLocaleTimeString());
+        this.setState({ time: new Date().toLocaleTimeString() });
+      }, 1000),
+    });
   }
 
   componentWillUnmount() {
-    clearInterval(this.state.timerId);
+    if (this.state.timerId) {
+      clearInterval(this.state.timerId);
+    }
   }
 
   render() {
-    const { date } = this.state;
+    const { time } = this.state;
 
     return (
       <p className="app__timer">
         Current time:
         {' '}
-        {date}
+        {time}
       </p>
     );
   }
