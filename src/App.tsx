5238172d3c5416f6b301752b1406ecc333bf9738
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-02-17 10:58:18 +0200

    fixed2

--- src/App.tsx
+++ src/App.tsx
@@ -44,24 +44,4 @@ export class App extends React.Component {
   }
 }
 
-// const App1: React.FC = () => {
-//   const timerId = setInterval(() => {
-//     const date: Date = new Date();
-
-//     // eslint-disable-next-line
-//     console.log(date.toLocaleTimeString());
-//   }, 1000);
-
-//   return (
-//     <div className="App">
-//       <h1>React clock</h1>
-//       <p>
-//         Current time:
-//         {' '}
-//         {/* Print the time here instead of DevTools */}
-//       </p>
-//     </div>
-//   );
-// };
-
 export default App;
