9dd5184322523be9104d9be44d558454db30bee5
jstmpelowycz <falcon.patron@gmail.com>
2022-03-23 20:42:52 +0200

    Trying to fix `react-bootstrap` devdep issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { App } from './App';
 
 ReactDOM.render(
