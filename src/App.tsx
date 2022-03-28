f4dfce06a539d701da621eadb1628e641cde2488
Max Shlikhta <maxok.vibe@gmail.com>
2022-03-28 19:17:40 +0300

    Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
+import { Clock } from './components/Clock';
+
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: number;
 };
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    clockName: Math.floor(Math.random() * 1000),
+  };
+
+  clockShow = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  clockHide = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  clockRename = () => {
+    this.setState({
+      clockName: Math.floor(Math.random() * 1000),
+    });
+  };
+
+  render() {
+    const { clockName, isClockVisible } = this.state;
+
+    return (
+      <div className="app">
+        <div className="app__frame">
+          <h1 className="app__clock-name">
+            {`Clock name: ${clockName}`}
+          </h1>
+
+          <div className="app__action">
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.clockShow}
+            >
+              Show Clock
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.clockHide}
+            >
+              Hide Clock
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.clockRename}
+            >
+              Rename clock
+            </button>
+          </div>
+          {isClockVisible
+            ? <span className="app__time-digits"><Clock name={clockName} /></span>
+            : <h3 className="app__time-text">&#129045; click &quot;Show Clock&quot;</h3>}
+        </div>
+      </div>
+    );
+  }
+}
