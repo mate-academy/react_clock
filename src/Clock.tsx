abfa937389733e7a42517b933f80ffeac3c8a270
Ivan Major <tarstarcas@gmail.com>
2022-04-06 18:40:28 +0300

    V1

---
+++ src/Clock.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type Props = {
+  name: number,
+};
+
+type State = {
+  date: string,
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    date: new Date().toLocaleTimeString(),
+    timerID: setInterval(() => {}, 0),
+  };
+
+  componentDidMount() {
+    this.state.timerID = setInterval(() => {
+      this.setState({ date: new Date().toLocaleTimeString() });
+
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.name !== prevProps.name) {
+      console.log(`The Clock was renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timerID);
+  }
+
+  render() {
+    return (
+      <span>
+        {this.state.date}
+      </span>
+    );
+  }
+}
