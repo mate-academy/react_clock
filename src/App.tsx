e316691653712e1019ab85faa59e4d6e534ede2d
KristinaTar <kristina.tarashchuk@gmail.com>
2022-04-08 13:22:26 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
 import React from 'react';
 import './App.scss';
+import Clock from './Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+type Props = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  clockName: number;
 };
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  };
+
+  hideHandler = () => {
+    this.setState({ isClockVisible: false });
+  };
+
+  showHandler = () => {
+    this.setState({ isClockVisible: true });
+  };
+
+  nameHandler = () => {
+    this.setState({ clockName: Math.floor((Math.random() * 10) + 1) });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <img className="bgImage" src="https://jabko.ua/image/catalog/products/2020/06/281404/3581%20(1).jpg" alt="applewatch" /> */}
+        <div className="container">
+          <div
+            className="clockButton"
+            onClick={this.showHandler}
+            onKeyDown={this.showHandler}
+            role="button"
+            tabIndex={0}
+          >
+            Show Clock
+          </div>
+          <div
+            className="clockButton"
+            onClick={this.hideHandler}
+            onKeyDown={this.hideHandler}
+            role="button"
+            tabIndex={0}
+          >
+            Hide Clock
+          </div>
+          <div
+            className="clockButton"
+            onClick={this.nameHandler}
+            onKeyDown={this.nameHandler}
+            role="button"
+            tabIndex={0}
+          >
+            Random name
+          </div>
+          { this.state.isClockVisible && <Clock name={this.state.clockName} /> }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
