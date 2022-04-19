58121974e88f657447dfdca45dbde87a6e725cae
Anton Kulchytskyi <antek1809@gmail.com>
2022-04-19 15:03:45 +0200

    solution with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,114 @@
-import React from 'react';
+// import React from 'react';
+// import { Clock } from './Clock';
+// import './App.scss';
+
+// type State = {
+//   isClockVisible: boolean;
+//   clockName: number;
+// };
+
+// export class App extends React.Component<{}, State> {
+//   state = {
+//     isClockVisible: true,
+//     clockName: 0,
+//   };
+
+//   createRandomName = () => {
+//     if (this.state.isClockVisible) {
+//       this.setState({
+//         clockName: Math.round(Math.random() * 1000),
+//       });
+//     }
+//   };
+
+//   render() {
+//     const { isClockVisible, clockName } = this.state;
+
+//     return (
+//       <div className="clock__block">
+//         <h1>React clock</h1>
+
+//         { isClockVisible && <Clock name={clockName} /> }
+
+//         <div className="buttons">
+//           <button
+//             className="show button"
+//             type="button"
+//             onClick={() => this.setState({ isClockVisible: true })}
+//           >
+//             Show
+//           </button>
+//           <button
+//             className="hide button"
+//             type="button"
+//             onClick={() => this.setState({ isClockVisible: false })}
+//           >
+//             Hide
+//           </button>
+//         </div>
+
+//         { isClockVisible && (
+//           <button
+//             className="random button"
+//             type="button"
+//             onClick={this.createRandomName}
+//           >
+//             Set random name
+//           </button>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// ----------------------------- with hooks -----------------//
+
+import React, { useState } from 'react';
 import { Clock } from './Clock';
 import './App.scss';
 
-type State = {
-  isClockVisible: boolean;
-  clockName: number;
-};
+export const App: React.FC = () => {
+  const [clockName, setClockName] = useState(0);
+  const [isClockVisible, setIsClockVisible] = useState(true);
 
-export class App extends React.Component<{}, State> {
-  state = {
-    isClockVisible: true,
-    clockName: 0,
-  };
-
-  createRandomName = () => {
-    if (this.state.isClockVisible) {
-      this.setState({
-        clockName: Math.round(Math.random() * 1000),
-      });
+  const createRandomName = () => {
+    if (isClockVisible) {
+      setClockName(Math.round(Math.random() * 1000));
     }
   };
 
-  render() {
-    const { isClockVisible, clockName } = this.state;
+  return (
+    <div className="clock__block">
+      <h1>React clock</h1>
 
-    return (
-      <div className="clock__block">
-        <h1>React clock</h1>
+      { isClockVisible && <Clock name={clockName} /> }
 
-        { isClockVisible && <Clock name={clockName} /> }
-
-        <div className="buttons">
-          <button
-            className="show button"
-            type="button"
-            onClick={() => this.setState({ isClockVisible: true })}
-          >
-            Show
-          </button>
-          <button
-            className="hide button"
-            type="button"
-            onClick={() => this.setState({ isClockVisible: false })}
-          >
-            Hide
-          </button>
-        </div>
-
-        { isClockVisible && (
-          <button
-            className="random button"
-            type="button"
-            onClick={this.createRandomName}
-          >
-            Set random name
-          </button>
-        )}
+      <div className="buttons">
+        <button
+          className="show button"
+          type="button"
+          onClick={() => setIsClockVisible(true)}
+        >
+          Show
+        </button>
+        <button
+          className="hide button"
+          type="button"
+          onClick={() => setIsClockVisible(false)}
+        >
+          Hide
+        </button>
       </div>
-    );
-  }
-}
+
+      { isClockVisible && (
+        <button
+          className="random button"
+          type="button"
+          onClick={createRandomName}
+        >
+          Set random name
+        </button>
+      )}
+    </div>
+  );
+};
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,55 +1,109 @@
-import React from 'react';
+// import React from 'react';
 
-type State = {
-  time: Date;
-};
+// type State = {
+//   time: Date;
+// };
+
+// type Props = {
+//   name: number;
+// };
+
+// export class Clock extends React.Component<Props, State> {
+//   timerId: NodeJS.Timer = setInterval(() => {}, 0);
+
+//   state = {
+//     time: new Date(),
+//   };
+
+//   componentDidMount() {
+//     this.timerId = setInterval(() => {
+//       this.setState({
+//         time: new Date(),
+//       });
+//       // eslint-disable-next-line
+//       console.log(this.state.time.toLocaleTimeString());
+//     }, 1000);
+//   }
+
+//   componentDidUpdate({ name: oldName }: Props) {
+//     const { name } = this.props;
+
+//     if (oldName !== name) {
+//       // eslint-disable-next-line
+//       console.log(`The Clock was renamed from ${oldName} to ${name}`);
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerId);
+//   }
+
+//   render() {
+//     const { time } = this.state;
+
+//     return (
+//       <>
+//         <p>
+//           {'Current time: '}
+//         </p>
+//         <p>
+//           {time.toLocaleTimeString()}
+//         </p>
+//       </>
+//     );
+//   }
+// }
+
+// -----------with hooks ------ //
+import React, { useEffect, useState, useRef } from 'react';
+import { setInterval } from 'timers';
 
 type Props = {
   name: number;
 };
 
-export class Clock extends React.Component<Props, State> {
-  timerId: NodeJS.Timer = setInterval(() => {}, 0);
+const usePrevious = <T extends unknown>(value: T): T | undefined => {
+  const ref = useRef<T>();
 
-  state = {
-    time: new Date(),
-  };
+  useEffect(() => {
+    ref.current = value;
+  });
 
-  componentDidMount() {
-    this.timerId = setInterval(() => {
-      this.setState({
-        time: new Date(),
-      });
+  return ref.current;
+};
+
+export const Clock: React.FC<Props> = ({ name }) => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      const date = new Date();
+
+      setTime(date);
       // eslint-disable-next-line
-      console.log(this.state.time.toLocaleTimeString());
+      console.log(date.toLocaleTimeString());
     }, 1000);
-  }
 
-  componentDidUpdate({ name: oldName }: Props) {
-    const { name } = this.props;
+    return () => clearInterval(timerId);
+  }, []);
 
+  const oldName = usePrevious(name);
+
+  useEffect(() => {
     if (oldName !== name) {
       // eslint-disable-next-line
       console.log(`The Clock was renamed from ${oldName} to ${name}`);
     }
-  }
+  }, [name]);
 
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
-
-  render() {
-    const { time } = this.state;
-
-    return (
-      <>
-        <p>
-          {'Current time: '}
-        </p>
-        <p>
-          {time.toLocaleTimeString()}
-        </p>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <p>
+        {'Current time: '}
+      </p>
+      <p>
+        {time.toLocaleTimeString()}
+      </p>
+    </>
+  );
+};
