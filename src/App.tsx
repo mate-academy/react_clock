9ded662b30fb92c84bc6df51ad125981dcdf1ab2
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-05-04 16:33:04 +0300

    clock was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+type Props = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  name: number,
+  clockVisible: boolean,
 };
+
+class App extends React.Component<Props, State> {
+  state = {
+    name: 0,
+    clockVisible: true,
+  };
+
+  showClock = () => {
+    this.setState({
+      clockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      clockVisible: false,
+    });
+  };
+
+  generateName = () => {
+    const generateName = Math.ceil(Math.random() * 1000);
+
+    this.setState({
+      name: generateName,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>
+          React clock
+        </h1>
+        <button type="button" onClick={this.hideClock}>
+          Hide Clock
+        </button>
+        <button type="button" onClick={this.showClock}>
+          Show Clock
+        </button>
+        <button type="button" onClick={this.generateName}>
+          Generete name
+        </button>
+
+        {this.state.clockVisible && <Clock name={this.state.name} />}
+      </>
+    );
+  }
+}
 
 export default App;
