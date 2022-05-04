f5409d832d7fbc1d6bec85442f4a675e9abea0c7
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-04 11:01:32 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+type Props = {};
 
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockName: string,
 };
+
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 'React clock',
+  };
+
+  render() {
+    // eslint-disable-next-line max-len
+    const names = ['Clock', 'Another Clock', 'React Clock', 'React', 'Clock Name'];
+
+    return (
+      <div className="App">
+        <h1>{this.state.clockName}</h1>
+
+        {this.state.isClockVisible && (
+          <p data-cy="time">
+            Current time:
+            {' '}
+            <Clock name={this.state.clockName} />
+          </p>
+        )}
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide Clock
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            const randomized = names[Math.floor(Math.random() * names.length)];
+
+            this.setState({ clockName: randomized });
+          }}
+        >
+          Set random name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
