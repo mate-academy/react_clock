c8b621f57378c9db56c47fa8d484df3a21f9af10
Helen Petrovska <paznachka@gmail.com>
2022-05-05 22:24:53 +0300

    clock is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,79 @@
 import React from 'react';
+import { Clock } from './components/Clock';
+
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+type Props = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockName: string,
 };
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 'mood',
+  };
+
+  clockVisible = () => {
+    this.setState({ isClockVisible: true });
+  };
+
+  clockNotVisible = () => {
+    this.setState({ isClockVisible: false });
+  };
+
+  changeName = () => {
+    const arrayOfNames = ['smile', 'sad', 'sick', 'happy', 'peppy', 'relax'];
+
+    this.setState({
+      clockName: arrayOfNames[
+        Math.floor(Math.random() * arrayOfNames.length)],
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className="app__title">React clock</h1>
+
+        <div className="app__buttons">
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.clockVisible}
+          >
+            show
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.clockNotVisible}
+          >
+            hide
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.changeName}
+          >
+            Set random name
+          </button>
+        </div>
+
+        <p className="app__subtitle">
+          Current time:
+          {' '}
+          {this.state.isClockVisible
+            && <Clock name={this.state.clockName} />}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
