44306effcd2c094f7f09e6e4f71939924361780e
Kirill Polozenko <kir.polozenko@icloud.com>
2022-05-05 23:28:47 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,76 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Components/Clock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+type Props = {};
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean,
+  clockName: number,
 };
+
+class App extends React.Component<Props, State> {
+  state = {
+    clockName: Math.floor(Math.random() * 100),
+    isClockVisible: true,
+  };
+
+  showClock = () => {
+    this.setState(
+      { isClockVisible: true },
+    );
+  };
+
+  hideClock = () => {
+    this.setState(
+      { isClockVisible: false },
+    );
+  };
+
+  randomName = () => {
+    this.setState(
+      { clockName: Math.floor(Math.random() * 100) },
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          React clock
+          {' '}
+          {this.state.clockName}
+        </h1>
+        {this.state.isClockVisible
+        && <Clock clockName={this.state.clockName} />}
+        <div className="App__buttons">
+          <button
+            type="button"
+            className="button"
+            onClick={this.showClock}
+            disabled={this.state.isClockVisible}
+          >
+            Show Clock
+          </button>
+          <button
+            type="button"
+            className="button button--hide"
+            onClick={this.hideClock}
+            disabled={!this.state.isClockVisible}
+          >
+            Hide Clock
+          </button>
+          <button
+            className="button button--rename"
+            type="button"
+            onClick={this.randomName}
+          >
+            Rename
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
