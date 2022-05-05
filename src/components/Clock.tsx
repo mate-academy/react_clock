e503c52a7dd8687e84f05188b464cfb2ef54688c
Anton Vakoliuk <antonvakolyuk2001@gmail.com>
2022-05-05 20:50:11 +0200

    Clock was created

---
+++ src/components/Clock.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  timer: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    timer: new Date().toLocaleTimeString(),
+  };
+
+  timerId: NodeJS.Timer = setInterval(() => {
+    const date: Date = new Date();
+
+    // eslint-disable-next-line
+    console.log(date.toLocaleTimeString());
+  }, 1000);
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ timer: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prev: Props) {
+    if (this.props.name !== prev.name) {
+      // eslint-disable-next-line
+      console.log(`The Clock was renamed from ${prev.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { timer } = this.state;
+
+    return (
+      <>
+        {timer}
+      </>
+    );
+  }
+}
