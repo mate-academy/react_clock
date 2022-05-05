a8704306a87ae34ab5bf4393dc3d9a5b880b3d62
SeKaM0 <polozhentsew02@gmail.com>
2022-05-05 17:33:12 +0300

    Finish task

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+type Props = {
+  name: number,
+};
+
+type State = {
+  date: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    date: new Date().toLocaleTimeString(),
+  };
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date().toLocaleTimeString() });
+
+      // eslint-disable-next-line no-console
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.name !== prevProps.name) {
+      // eslint-disable-next-line no-console
+      console.log(`The Clock was renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <p>
+        Current time:
+        {' '}
+        {this.state.date}
+      </p>
+    );
+  }
+}
