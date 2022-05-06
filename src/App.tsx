3bc99d985eb117c28b09671f8647b722206b0034
Alexandr Nikytenko <nika200382@gmail.com>
2022-05-06 17:18:57 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import { Clock } from './Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type State = {
+  clockVisible: boolean,
+  clockId: number,
 };
+
+class App extends React.Component<{}, State> {
+  state = {
+    clockVisible: true,
+    clockId: 0,
+  };
+
+  showClock = () => {
+    this.setState({
+      clockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      clockVisible: false,
+    });
+  };
+
+  generateName = () => {
+    const generateName = Math.ceil(Math.random() * 10);
+
+    this.setState({
+      clockId: generateName,
+    });
+  };
+
+  render() {
+    return (
+      <div className="Clock">
+        <h1 className="Clock__title">
+          React clock
+        </h1>
+        <div className="Clock__button">
+          <button
+            className="Clock__button-hide"
+            type="button"
+            onClick={this.hideClock}
+          >
+            Hide Clock
+          </button>
+          <button
+            className="Clock__button-show"
+            type="button"
+            onClick={this.showClock}
+          >
+            Show Clock
+          </button>
+          <button
+            className="Clock__button-name"
+            type="button"
+            onClick={this.generateName}
+          >
+            Try another name
+          </button>
+        </div>
+
+        {this.state.clockVisible && <Clock clockId={this.state.clockId} />}
+      </div>
+    );
+  }
+}
 
 export default App;
