8e325a7581f544e71a1295e1d6606f6223529d28
Andriy Fesych <andriyhimself@gmail.com>
2022-05-22 13:11:37 +0300

    Solution_0.11_(buttons_for_tests_added)

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,18 @@ class App extends React.Component <Props, State> {
     });
   };
 
+  show = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hide = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
   nameRandomizer = () => {
     this.setState({
       clockName: Math.floor(Math.random() * 999),
@@ -61,20 +73,37 @@ class App extends React.Component <Props, State> {
           <br />
           <button
             type="button"
-            onClick={this.sttChanger}
-          >
-            <h2>
-              Show/Hide Clock component
-            </h2>
-          </button>
-          <button
-            type="button"
             onClick={this.nameRandomizer}
           >
             <h2>
               Set random name for a clock
             </h2>
           </button>
+          <button
+            type="button"
+            onClick={this.sttChanger}
+          >
+            <h2>
+              trigger state button
+            </h2>
+          </button>
+          <br />
+          <button
+            type="button"
+            onClick={this.show}
+          >
+            Show Clock
+          </button>
+          <button
+            type="button"
+            onClick={this.hide}
+          >
+            Hide Clock
+          </button>
+          <span>
+            &#160;🢀 these buttons are just for tests,
+            &#160;better use my &#34;trigger state&#34; button above 😉
+          </span>
           { this.state.isClockVisible && <Clock name={this.state.clockName} />}
           <br />
         </fieldset>
