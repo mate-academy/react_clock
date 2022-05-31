0c0fd6156d56a0870a6c4fb1b91f75693ad70e7d
Maryna <savna023@gmail.com>
2022-05-31 23:29:11 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,70 @@
 import React from 'react';
 import './App.scss';
+import Clock from './Components/Clock/Clock';
+import { names } from './NamesForClock';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
-
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
-
-  // eslint-disable-next-line
-  console.log(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
+type Props = {
+  clockName: string,
+  isClockVisible: boolean,
 };
+
+class App extends React.Component<{}, Props> {
+  state = {
+    clockName: 'React clock',
+    isClockVisible: true,
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  setRandomName = () => {
+    this.setState({
+      clockName: names[Math.floor(Math.random() * names.length)],
+    });
+  };
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="app-name">{clockName}</h1>
+        {isClockVisible && (
+          <Clock name={clockName} />
+        )}
+        <button
+          type="button"
+          className="big-button"
+          onClick={this.showClock}
+        >
+          Show Clock
+        </button>
+        <button
+          type="button"
+          className="big-button"
+          onClick={this.hideClock}
+        >
+          Hide Clock
+        </button>
+        <button
+          type="button"
+          className="big-button"
+          onClick={this.setRandomName}
+        >
+          Set Random Name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Components/Clock/Clock.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Clock.scss';
+
+type Props = {
+  name: string,
+};
+
+type State = {
+  date: string,
+};
+
+class Clock extends React.Component<Props, State> {
+  state = {
+    date: '',
+  };
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({
+        date: new Date().toLocaleTimeString(),
+      });
+      // eslint-disable-next-line
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidUpdate({ name }: Readonly<Props>) {
+    // eslint-disable-next-line
+    console.log(`The Clock was renamed from ${name} to ${this.props.name}`);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { date } = this.state;
+
+    return (
+      <div className="clock">
+        <p className="clock-info">Current time:</p>
+        <strong data-cy="time" className="clock-time">{date}</strong>
+      </div>
+    );
+  }
+}
+
+export default Clock;
