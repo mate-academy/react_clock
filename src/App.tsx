6e2aa676110b57475d9eeeae856f380ad7a61b61
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-31 15:21:21 +0300

    added name randomizer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import './App.scss';
 import { Clock } from './components/Clock';
 
-class App extends React.Component {
+interface State {
+  isClockVisible: boolean,
+  clockName: number,
+}
+
+class App extends React.Component<{}, State> {
   state = {
     isClockVisible: true,
+    clockName: 0,
   };
 
   render() {
     return (
       <div className="App">
         <>
+          <h2>
+            {`Clock name: ${this.state.clockName}`}
+          </h2>
+
           <h1>React clock</h1>
           <p>
             {'Current time: '}
@@ -23,7 +33,8 @@ class App extends React.Component {
                 bg-opacity-50
                 "
             >
-              {this.state.isClockVisible && (<Clock />)}
+              {this.state.isClockVisible
+                && (<Clock name={this.state.clockName} />)}
             </span>
           </p>
         </>
@@ -47,6 +58,19 @@ class App extends React.Component {
           }}
         >
           Hide Clock
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-info"
+          onClick={() => {
+            this.setState({
+              clockName:
+                Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+            });
+          }}
+        >
+          Set random name
         </button>
       </div>
     );
