8046c02846b3305419257f66903e8a63e9834d15
Daria Liubymova <dellachaise@gmail.com>
2022-06-01 14:06:38 +0300

    Solved 'React clock' task

---
+++ src/componets/Clock.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type State = {
+  time: string;
+};
+
+export class Clock extends React.Component <{}, State> {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    // eslint-disable-next-line no-console
+    console.log(this.state.time);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <span>{this.state.time}</span>
+    );
+  }
+}
