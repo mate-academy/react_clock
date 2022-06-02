a81e5af7a78ebe8e0699a69f98a4e88f8de46a8d
foxifio <foxifio00@gmail.com>
2022-06-02 15:28:11 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import { Clock } from './components/Clock';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+interface State {
+  isClockVisible: boolean,
+  clockName: number,
+}
 
-  // eslint-disable-next-line
-  console.log(timerId);
+class App extends React.Component<{}, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  };
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <>
+          <h2>
+            {`Clock name: ${this.state.clockName}`}
+          </h2>
+
+          <h1>React clock</h1>
+          <p>
+            {'Current time: '}
+            <span>
+              {this.state.isClockVisible
+                && (<Clock name={this.state.clockName} />)}
+            </span>
+          </p>
+        </>
+
+        <button
+          type="button"
+          data-cy="time"
+          className="btn btn-success"
+          onClick={() => {
+            this.setState({ isClockVisible: true });
+          }}
+        >
+          Show Clock
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => {
+            this.setState({ isClockVisible: false });
+          }}
+        >
+          Hide Clock
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => {
+            this.setState({
+              clockName:
+                Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+            });
+          }}
+        >
+          Set random Clock name
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
