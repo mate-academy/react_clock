1edf2f5fe45d77990cb8569018e544789fab06c2
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-06-07 15:52:14 +0200

    Task complete

---
+++ src/components/Clock.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type State = {
+  time: string;
+};
+
+type Props = {};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+
+      // eslint-disable-next-line
+      console.log(this.state.time);
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      this.state.time
+    );
+  }
+}
