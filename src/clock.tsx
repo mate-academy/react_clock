a25181d908da449eb99bbc35939020411b989401
Karina Prozner <prozner.karina@gmail.com>
2022-06-07 18:32:17 +0200

    guess it works

---
+++ src/clock.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+type State = {
+  time: string
+};
+
+export class Clock extends React.Component<{}, State> {
+  state = {
+    time: new Date().toLocaleTimeString(),
+  };
+
+  timerId?: NodeJS.Timer;
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <div data-cy="time">
+        {this.state.time}
+      </div>
+    );
+  }
+}
