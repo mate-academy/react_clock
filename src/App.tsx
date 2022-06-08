31d6bac96a8bd19c9314779222085946bc5b5ae3
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-06-08 17:50:20 +0300

    clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,92 @@
 import React from 'react';
+import { Clock } from './components/Clock';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+interface State {
+  isClockVisible: boolean;
+  clockName: string;
+}
 
-  // eslint-disable-next-line
-  console.log(timerId);
+const clockNames: string[] = [
+  'Elementary clock',
+  'Basic clock',
+  'A race against the clock',
+  'Clock is running',
+  'Ukrainian clock UA',
+  'Put the clock back',
+  'Watching the clock O_O',
+  'Rock Around The Clock',
+  'Beat the clock',
+  'The biological clock is ticking',
+];
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+class App extends React.Component<Props, State> {
+  state = {
+    isClockVisible: true,
+    clockName: 'Clock',
+  };
+
+  getClockName = () => {
+    const randomNewName = clockNames[
+      Math.floor(Math.random() * clockNames.length)
+    ];
+
+    this.setState({ clockName: randomNewName });
+  };
+
+  showClock = () => {
+    this.setState({
+      isClockVisible: true,
+    });
+  };
+
+  hideClock = () => {
+    this.setState({
+      isClockVisible: false,
+    });
+  };
+
+  render() {
+    const { clockName, isClockVisible } = this.state;
+
+    return (
+      <div className="app">
+        {isClockVisible && <Clock name={clockName} />}
+
+        <div className="app__buttons">
+          <button
+            className="buttons__item"
+            type="button"
+            onClick={this.showClock}
+            disabled={isClockVisible}
+          >
+            Show Clock
+          </button>
+
+          <button
+            className="buttons__item"
+            type="button"
+            onClick={this.hideClock}
+            disabled={!isClockVisible}
+          >
+            Hide Clock
+          </button>
+
+          <button
+            className="buttons__item"
+            type="button"
+            onClick={this.getClockName}
+            disabled={!isClockVisible}
+          >
+            Random Name
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
