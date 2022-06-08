db38368ccfaee539c5a5c8123b92426e8e69530b
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-06-08 15:25:29 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,73 @@
 import React from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
-  const timerId: NodeJS.Timer = setInterval(() => {
-    const date: Date = new Date();
+import { Countdown } from './Countdown';
 
-    // eslint-disable-next-line
-    console.log(date.toLocaleTimeString());
-  }, 1000);
+interface State {
+  isStarted: boolean,
+  limit: number,
+}
 
-  // eslint-disable-next-line
-  console.log(timerId);
+class App extends React.Component<{}, State> {
+  state = {
+    isStarted: false,
+    limit: 5,
+  };
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-      <p>
-        Current time:
-        {' '}
-        {/* Print the time here instead of DevTools */}
-      </p>
-    </div>
-  );
-};
+  render() {
+    const { isStarted, limit } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              isStarted: true,
+            });
+          }}
+        >
+          Start
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              isStarted: false,
+            });
+          }}
+        >
+          Stop
+        </button>
+
+        <br />
+
+        <button
+          type="button"
+          onClick={() => this.setState({ limit: 10 })}
+        >
+          10
+        </button>
+
+        {isStarted && (
+          <Countdown from={limit} />
+        )}
+      </div>
+    );
+  }
+}
+// const timerId: NodeJS.Timer = setInterval(() => {
+//   const date: Date = new Date();
+
+//   // eslint-disable-next-line
+//   console.log(date.toLocaleTimeString());
+// }, 1000);
+
+// eslint-disable-next-line
+  // console.log(timerId);
+
+// };
 
 export default App;
