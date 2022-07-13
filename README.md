2d8cd8b11c276d043d601567e17a185df1cf8bf9
Illia Bielichenko <ibelich12@gmail.com>
2022-07-13 13:23:51 +0300

    solution was added

--- README.md
+++ README.md
@@ -32,4 +32,4 @@ Create a class component `Clock` updating the time every second using a given ma
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 - Use [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_clock/) and add it to PR description
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://Bielichenko.github.io/react_clock/) and add it to PR description
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import './App.scss';
 
@@ -7,35 +8,67 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-const App: React.FC = () => {
-  const date = new Date();
-  const clockName = getRandomName();
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    // ...
-  }, 1000);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {date.toLocaleTimeString()}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  date: string;
+  hasClock: boolean;
+  clockName: string;
 };
+
+class App extends React.Component<{}, State> {
+  state = {
+    date: new Date().toLocaleTimeString(),
+    hasClock: true,
+    clockName: getRandomName(),
+  };
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({ date: new Date().toLocaleTimeString() });
+    }, 1000);
+
+    window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+
+    document.addEventListener('contextmenu', this.removeClock);
+    document.addEventListener('click', this.addClock);
+  }
+
+  componentDidUpdate(_: {}, prevState : State) {
+    if (prevState.clockName !== this.state.clockName) {
+      console.log(`Renamed from <${prevState.clockName}> to <${this.state.clockName}>`);
+    }
+  }
+
+  removeClock = () => {
+    this.setState({ hasClock: false });
+  };
+
+  addClock = () => {
+    this.setState({ hasClock: true });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        <div className="Clock">
+          <strong className="Clock__name">
+            {this.state.clockName}
+          </strong>
+
+          {' time is '}
+          {this.state.hasClock && (
+            <span className="Clock__time">
+              {this.state.date}
+              {console.log(this.state.date)}
+            </span>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
