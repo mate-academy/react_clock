84a2c1d90c63514d1e6512fdf36edfd3ac33c690
Ihor Kuchin <vedunigor@gmail.com>
2022-07-15 14:20:46 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock/Clock';
 
-function getRandomName(): string {
-  const value = Math.random().toString().slice(2, 6);
-
-  return `Clock-${value}`;
-}
-
-const App: React.FC = () => {
-  const date = new Date();
-  const clockName = getRandomName();
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    // ...
-  }, 1000);
-
-  // this code stops the timer
-  clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="clock">
-        <strong>{clockName}</strong>
-        {' time is '}
-        {date.toLocaleTimeString()}
-      </div>
-    </div>
-  );
+type State = {
+  hasClock: boolean,
+  clockName: number,
 };
+
+class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    hasClock: true,
+    clockName: 0,
+  };
+
+  randomClockName = setInterval(() => this.setState(
+    { clockName: Math.floor(Math.random() * 300 + 1) },
+  ), 3300);
+
+  showClock = () => {
+    this.setState({ hasClock: true });
+  };
+
+  hideClock = (event: React.MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  setRandomName = () => {
+    if (!this.state.hasClock) {
+      return;
+    }
+
+    this.setState({ clockName: Math.round(Math.random() * 1000) });
+  };
+
+  render() {
+    const { clockName, hasClock } = this.state;
+
+    return (
+      <div
+        className="App"
+        onClick={this.showClock}
+        onContextMenu={this.hideClock}
+        onKeyDown={this.showClock}
+        role="button"
+        tabIndex={0}
+      >
+        <div className="App__clock">
+          <h1 className="App__clock-title">
+            {`React clock - ${clockName}`}
+          </h1>
+
+          <div className="clock-container">
+            {hasClock && (
+              <Clock name={clockName} />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Clock.scss';
+
+type Props = {
+  name: number,
+};
+
+type State = {
+  time: Date,
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    time: new Date(),
+  };
+
+  timerId = setInterval(() => {
+    this.setState({ time: new Date() });
+  }, 1000);
+
+  componentDidMount() {
+    return this.timerId;
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const oldName = prevProps.name;
+    const newName = this.props.name;
+
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line
+      console.log(`clockName: ${oldName} changed to clockName: ${newName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="time">
+        {`Time: ${this.state.time.toLocaleTimeString()}`}
+      </div>
+    );
+  }
+}
