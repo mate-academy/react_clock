ebc1e29a86703ea5b3648b2024c93079b2da0595
fledith <barateon023@gmail.com>
2022-07-28 16:02:58 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { Component } from 'react';
+import { Clock } from './component/Clock';
+
 import './App.scss';
 
 function getRandomName(): string {
@@ -7,33 +9,64 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const date = new Date();
-  const clockName = getRandomName();
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    // ...
-  }, 1000);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {date.toLocaleTimeString()}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  hasClock: boolean;
+  clockName: string;
 };
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    hasClock: true,
+    clockName: getRandomName(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = this.setTimer;
+    document.addEventListener('click', this.handlerClock);
+    document.addEventListener('contextmenu', this.handlerClock);
+  }
+
+  componentDidUpdate(_: Readonly<{}>, prevState: Readonly<State>) {
+    const prevName = prevState.clockName;
+    const currentName = this.state.clockName;
+
+    if (prevName !== currentName) {
+      console.log(`Renamed from <${prevName}> to <${currentName}>`); // eslint-disable-line
+    }
+  }
+
+  handlerClock = (event: MouseEvent) => {
+    if (event.type === 'contextmenu') {
+      event.preventDefault();
+      this.setState({ hasClock: false });
+
+      clearInterval(this.setTimer);
+    }
+
+    if (event.type === 'click') {
+      this.setState({ hasClock: true });
+
+      this.setTimer = window.setInterval(this.handlerSetInterval, 3300);
+    }
+  };
+
+  handlerSetInterval = () => {
+    this.setState({ clockName: getRandomName() });
+  };
+
+  setTimer = window.setInterval(this.handlerSetInterval, 3300);
+
+  render() {
+    const { hasClock, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {hasClock && (<Clock clockName={clockName} />)}
+      </div>
+    );
+  }
+}
