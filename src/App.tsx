9ecc455b417181706ddaf4fd4fbd5e9db9596602
Victor Hrygorash <viktor12041991@i.ua>
2022-08-01 13:15:42 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,73 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-function getRandomName(): string {
-  const value = Math.random().toString().slice(2, 6);
-
-  return `Clock-${value}`;
-}
-
-export const App: React.FC = () => {
-  const date = new Date();
-  const clockName = getRandomName();
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    // ...
-  }, 1000);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {date.toLocaleTimeString()}
-        </span>
-      </div>
-    </div>
-  );
+type ClockBasic = {
+  hasClock: boolean;
+  name: number;
 };
+
+export class App extends React.Component<{}, ClockBasic> {
+  state = {
+    hasClock: true,
+    name: 1,
+  };
+
+  randomName = setInterval(() => this.setState(
+    { name: Math.floor(Math.random() * 300 + 1) },
+  ), 3300);
+
+  componentDidMount() {
+    const clear = document.getElementById('clear');
+    const start = document.getElementById('start');
+
+    if (clear) {
+      clear.addEventListener('click', () => this.setState(
+        { hasClock: false },
+      ));
+    }
+
+    if (start) {
+      start.addEventListener('click', () => this.setState(
+        { hasClock: true },
+      ));
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.randomName);
+  }
+
+  render() {
+    const { hasClock } = this.state;
+
+    return (
+      <div className="clock">
+        {hasClock
+          ? <Clock name={this.state.name} />
+          : <div className="clock__info" />}
+
+        <div className="clock__bottom">
+          <button
+            type="button"
+            id="clear"
+            className="clock__button"
+            disabled={!this.state.hasClock}
+          >
+            Clear
+          </button>
+
+          <button
+            type="button"
+            id="start"
+            className="clock__button"
+            disabled={this.state.hasClock}
+          >
+            Start
+          </button>
+        </div>
+      </div>
+
+    );
+  }
+}
