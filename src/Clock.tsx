b353d760aef58a004b1ee4b92d4a89f87d262add
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-08-07 13:04:52 +0300

    task is ready

---
+++ src/Clock.tsx
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string,
+};
+
+type State = {
+  date: Date,
+};
+
+export class Clock extends Component<Props, State> {
+  state: State = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      const date = new Date();
+
+      this.setState({ date });
+      console.log(date);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { clockName } = this.props;
+
+    if (clockName !== prevProps.clockName) {
+      console.log(`Renamed from ${prevProps.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toLocaleTimeString()}
+        </span>
+      </div>
+    );
+  }
+}
