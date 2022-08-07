8be9dd2437e0536f33b6790cda3fe12a2e74b00e
Kate <krasnova2912@gmail.com>
2022-08-07 15:42:13 +0300

    clock component was added

---
+++ src/Clock.tsx
@@ -0,0 +1,57 @@
+import { Component, ReactNode } from 'react';
+
+interface Props {
+  clockName: string,
+}
+
+interface State {
+  date: Date;
+}
+
+export class Clock extends Component<Props, State> {
+  state: Readonly<State> = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+      // eslint-disable-next-line no-console
+      console.log(this.state.date);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { clockName } = this.props;
+
+    if (prevProps.clockName !== clockName) {
+      // eslint-disable-next-line no-console
+      console.log(`Renamed from ${prevProps.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render(): ReactNode {
+    const { clockName } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toLocaleTimeString()}
+        </span>
+      </div>
+    );
+  }
+}
