174240291e4af6b0bec97e887ff82747338d122c
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-09-04 20:28:54 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,16 @@ import { Component } from 'react';
 import './App.scss';
 import { Clock } from './Clock';
 
-type Props = {
-  name: string;
-};
-
 type State = {
-  date: Date;
   hasClock: boolean;
-  clockName: any;
 };
 
-function getRandomName(): string {
-  const value = Math.random().toString().slice(2, 6);
-
-  return `Clock-${value}`;
-}
-
-export class App extends Component<Props, State> {
-  state = {
-    date: new Date(),
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
     hasClock: true,
-    clockName: getRandomName(),
   };
 
-  timerId = 0;
-
   componentDidMount() {
-    this.timerId = window.setInterval(() => {
-      const date = new Date();
-      // eslint-disable-next-line
-      console.info(date);
-      this.setState({ date });
-    }, 1000);
-
-    window.setInterval(() => {
-      this.setState({ clockName: getRandomName() });
-    }, 3300);
-
     document.addEventListener('contextmenu', (event) => {
       event.preventDefault();
       this.setState({ hasClock: false });
@@ -49,44 +22,20 @@ export class App extends Component<Props, State> {
     });
   }
 
-  componentDidUpdate(prevProps: Props) {
-    const { name } = this.props;
-
-    if (name !== prevProps.name) {
-      // eslint-disable-next-line
-      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
-    }
-  }
-
   componentWillUnmount() {
     document.removeEventListener('contextmenu', () => {});
     document.removeEventListener('click', () => {});
-    window.clearInterval(this.timerId);
   }
 
   render() {
     const { hasClock } = this.state;
-    const { date } = this.state;
 
     return (
-      hasClock
-    && (
       <div className="App">
         <h1>React clock</h1>
-
-        <div className="Clock">
-          <strong className="Clock__name">
-            {this.state.clockName}
-          </strong>
-
-          {' time is '}
-
-          <span className="Clock__time">
-            <Clock date={date} />
-          </span>
-        </div>
+        {hasClock && <Clock />}
       </div>
-    )
+
     );
   }
 }
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,18 +1,69 @@
 import { Component } from 'react';
-import './App.scss';
 import './App';
 
-type Props = {
+type State = {
   date: Date;
+  clockName: any;
 };
 
-export class Clock extends Component<Props, {}> {
+function getRandomName(): string {
+  const value = Math.random().toString().slice(2, 6);
+
+  return `Clock-${value}`;
+}
+
+export class Clock extends Component<{}, State> {
+  state = {
+    date: new Date(),
+    clockName: getRandomName(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      const date = new Date();
+      // eslint-disable-next-line
+      console.info(date);
+      this.setState({ date });
+    }, 1000);
+
+    window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+  }
+
+  componentDidUpdate(prevState: State) {
+    const { clockName } = this.state;
+
+    if (clockName !== prevState.clockName) {
+      // eslint-disable-next-line
+      console.debug(`Renamed from ${prevState.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   render() {
-    const { date } = this.props;
+    const { date } = this.state;
     const timeString = date.toLocaleTimeString();
 
     return (
-      <>{timeString}</>
+      <>
+        <div className="Clock">
+          <strong className="Clock__name">
+            {this.state.clockName}
+          </strong>
+
+          {' time is '}
+
+          <span className="Clock__time">
+            {timeString}
+          </span>
+        </div>
+      </>
     );
   }
 }
