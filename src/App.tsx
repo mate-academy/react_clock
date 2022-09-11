dab332a54c7cbb1e4fd5873620a7b4f0fa8dd573
Mark Shapka <mark.shapka1@gmail.com>
2022-09-11 20:00:11 +0300

    Task_react-clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
+import { Clock } from './component/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +8,73 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toLocaleTimeString()}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  hasClock: boolean,
+  clockName: string,
 };
+
+export class App extends Component<{}, State> {
+  state = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+    document.addEventListener('contextmenu', this.hideClock);
+  }
+
+  componentDidUpdate() {
+    if (!this.state.hasClock) {
+      document.removeEventListener('contextmenu', this.hideClock);
+      document.addEventListener('contextmenu', this.contextClick);
+
+      document.addEventListener('click', this.appearClock);
+    } else {
+      document.removeEventListener('click', this.appearClock);
+      document.removeEventListener('contextmenu', this.contextClick);
+
+      document.addEventListener('contextmenu', this.hideClock);
+    }
+  }
+
+  contextClick = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  };
+
+  hideClock = (e: { preventDefault: () => void; }) => {
+    this.setState({ hasClock: false });
+    e.preventDefault();
+  };
+
+  appearClock = () => {
+    this.setState({ hasClock: true });
+  };
+
+  render() {
+    const { hasClock, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {hasClock
+          && <Clock clockName={clockName} />}
+      </div>
+    );
+  }
+
+  // const today = new Date();
+  // let clockName = 'Clock-0';
+
+  // // This code starts a timer
+  // const timerId = window.setInterval(() => {
+  //   clockName = getRandomName();
+  // }, 3300);
+
+  // // this code stops the timer
+  // window.clearInterval(timerId);
+}
