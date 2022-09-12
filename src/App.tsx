f2f7f4afb61d5cac93a81d6ea3c3cdd804081b0f
Oleksii Pustovit <91pustovit91@gmail.com>
2022-09-12 16:05:56 +0300

    Solution of react_clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,60 @@
 import React from 'react';
+import { Clock } from './components/Clock';
 import './App.scss';
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
-
-  return `Clock-${value}`;
-}
-
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toLocaleTimeString()}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  isClockVisible: boolean;
+  name: string;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    isClockVisible: true,
+    name: 'Clock-0',
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+      if (event) {
+        this.setState({ isClockVisible: false });
+      }
+    });
+
+    document.addEventListener('click', (event) => {
+      if (event) {
+        this.setState({ isClockVisible: true });
+      }
+    });
+
+    this.timerId = window.setInterval(() => {
+      this.setState({ name: this.getRandomName() });
+    }, 3300);
+  }
+
+  getRandomName = () => {
+    const random = Date.now().toString().slice(-4);
+
+    return `Clock-${random}`;
+  };
+
+  render() {
+    const { isClockVisible, name } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          React clock
+        </h1>
+        <p className="App__description">
+          Current time:
+        </p>
+        {isClockVisible && (
+          <Clock name={name} />
+        )}
+      </div>
+    );
+  }
+}
