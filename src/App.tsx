0fb09d9925d4fb52877c0eb3dc432b9d00a71858
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-09-12 22:18:06 +0300

    create clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable no-console */
+import { Component } from 'react';
 import './App.scss';
 
 function getRandomName(): string {
@@ -7,33 +8,122 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+export class App extends Component {
+  state: Readonly<{
+    today: Date,
+    clockName?: string,
+    hasClock: boolean,
+  }> = {
+    today: new Date(),
+    clockName: 'Clock-0',
+    hasClock: true,
+  };
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  componentDidMount(): void {
+    let timerId: number | undefined = window?.setInterval(() => {
+      this.setState((prevState) => ({
+        ...prevState,
+        clockName: getRandomName(),
+      }));
+    }, 3300);
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+    let printId: number | undefined = window?.setInterval(() => {
+      console.info(new Date().toLocaleTimeString());
+    }, 1000);
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+    document?.addEventListener('contextmenu', (event: MouseEvent) => {
+      event.preventDefault();
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+      if (this.state.hasClock) {
+        this.setState((prevState) => ({
+          ...prevState,
+          hasClock: false,
+        }));
+        window?.clearInterval(printId);
+        window?.clearInterval(timerId);
+        printId = undefined;
+        timerId = undefined;
+      }
+    });
 
-        {' time is '}
+    document?.addEventListener('click', (event: MouseEvent) => {
+      event.preventDefault();
 
-        <span className="Clock__time">
-          {today.toLocaleTimeString()}
-        </span>
+      if (!this.state.hasClock) {
+        this.setState((prevState) => ({
+          ...prevState,
+          hasClock: true,
+        }));
+        printId = window?.setInterval(() => {
+          console.info(new Date().toLocaleTimeString());
+        }, 1000);
+        timerId = window?.setInterval(() => {
+          this.setState((prevState) => ({
+            ...prevState,
+            clockName: getRandomName(),
+          }));
+        }, 3300);
+      }
+    });
+
+    const removeListeners = () => {
+      document?.removeEventListener('contextmenu', (event: MouseEvent) => {
+        event.preventDefault();
+
+        if (this.state.hasClock) {
+          this.setState((prevState) => ({
+            ...prevState,
+            hasClock: false,
+          }));
+        }
+      });
+
+      document?.removeEventListener('click', (event: MouseEvent) => {
+        event.preventDefault();
+
+        if (!this.state.hasClock) {
+          this.setState((prevState) => ({
+            ...prevState,
+            hasClock: true,
+          }));
+        }
+      });
+    };
+
+    return (
+      removeListeners()
+    );
+  }
+
+  componentDidUpdate(_: unknown, prevState: typeof this.state) {
+    console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
+  }
+
+  render() {
+    const {
+      today,
+      clockName,
+      hasClock,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">
+              {clockName}
+            </strong>
+
+            {' time is '}
+
+            <span className="Clock__time">
+              {today.toLocaleTimeString('us', { hour12: false })}
+            </span>
+          </div>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
