0638a838455a270da04a301efec2a7fdd8874de0
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-09-13 22:14:24 +0300

    created clock-react-app

---
+++ src/Clock.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+type Props = {
+  name: string,
+};
+
+type State = {
+  date: Date,
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    date: new Date(),
+  };
+
+  timesId = 0;
+
+  componentDidMount() {
+    this.timesId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+      console.info(this.state.date.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const oldProps = prevProps.name;
+    const newProps = this.props.name;
+
+    if (oldProps !== newProps) {
+      console.debug(`Renamed from ${oldProps} to ${newProps}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timesId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+  
+        {' time is '}
+  
+        <span className="Clock__time">
+          {date.toLocaleTimeString()}
+        </span>
+      </div>
+    )
+  }
+}
