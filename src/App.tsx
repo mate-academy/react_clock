0a1539bd075030dce9e2c367d9ef346647833ea8
Dima Kobzar <kobzar7dima@gmail.com>
2022-09-13 13:48:03 +0300

    Added destructuring at app render

--- src/App.tsx
+++ src/App.tsx
@@ -46,10 +46,12 @@ export class App extends Component<{}, State> {
   };
 
   render() {
+    const { hasClock, clockName } = this.state;
+
     return (
       <div className="App">
         <h1>React clock</h1>
-        {this.state.hasClock && <Clock clockName={this.state.clockName} />}
+        {hasClock && <Clock clockName={clockName} />}
       </div>
     );
   }
