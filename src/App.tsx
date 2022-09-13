4e90078374295cfa66dbb8d76033d2c4dba54bd4
Andrii Stiebalov <stiebalov@gmail.com>
2022-09-13 14:14:58 +0300

    changed Clock component

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,34 @@ import { Clock } from './components/Clock';
 
 type State = {
   hasClock: boolean;
+  clockName: string;
 };
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
 
 export class App extends React.Component<{}, State> {
   state: Readonly<State> = {
     hasClock: true,
+    clockName: 'Clock-0',
   };
+
+  setClockName = 0;
 
   componentDidMount() {
     document.addEventListener('contextmenu', this.hideClock);
-    document.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.setState({ hasClock: true });
-    });
+    document.addEventListener('click', this.showClock);
+
+    this.setClockName = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.setClockName);
   }
 
   hideClock = (event : MouseEvent) => {
@@ -24,14 +39,19 @@ export class App extends React.Component<{}, State> {
     this.setState({ hasClock: false });
   };
 
+  showClock = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: true });
+  };
+
   render() {
-    const { hasClock } = this.state;
+    const { hasClock, clockName } = this.state;
 
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        { hasClock && <Clock /> }
+        { hasClock && <Clock name={clockName} /> }
       </div>
     );
   }
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,46 +2,36 @@ import React from 'react';
 
 type State = {
   today: Date,
-  clockName: string;
 };
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
+type Prop = {
+  name: string,
+};
 
-  return `Clock-${value}`;
-}
-
-export class Clock extends React.Component<{}, State> {
+export class Clock extends React.Component<Prop, State> {
   state = {
     today: new Date(),
-    clockName: 'Clock-0',
   };
 
   setDate = 0;
-
-  setClockName = 0;
 
   componentDidMount() {
     this.setDate = window.setInterval(() => {
       this.setState({ today: new Date() });
     }, 1000);
-    this.setClockName = window.setInterval(() => {
-      this.setState({ clockName: getRandomName() });
-    }, 3300);
   }
 
   componentWillUnmount() {
     window.clearInterval(this.setDate);
-    window.clearInterval(this.setClockName);
   }
 
   render() {
-    const { today, clockName } = this.state;
+    const { today } = this.state;
 
     return (
       <div className="Clock">
         <strong className="Clock__name">
-          {clockName}
+          {this.props}
         </strong>
 
         {' time is '}
