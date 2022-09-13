25161651f2c092b13c2ab1c32c60e6841cc26e4e
Cyril Prue <kir8mir>
2022-09-13 09:18:01 +0300

    Devided on components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -9,48 +10,31 @@ function getRandomName(): string {
 
 interface State {
   clockName: string;
-  today: Date;
   hasClock: boolean;
 }
 
 export class App extends Component<{}, State> {
   state: Readonly<State> = {
     clockName: 'Clock-0',
-    today: new Date(),
     hasClock: true,
   };
 
   timerId = 0;
 
-  second = 0;
-
   componentDidMount() {
     this.timerId = window.setInterval(this.timerName, 3300);
-    this.second = window.setInterval(this.secondTime, 1000);
     document.addEventListener('contextmenu', this.hide);
     document.addEventListener('click', this.show);
   }
 
-  componentDidUpdate(_not: unknown, prevState: { clockName: string; }) {
-    if (this.state.clockName !== prevState.clockName && this.state.hasClock) {
-      // eslint-disable-next-line no-console
-      console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
-    }
-  }
-
   componentWillUnmount() {
     window.clearInterval(this.timerId);
-    window.clearInterval(this.second);
     document.removeEventListener('contextmenu', this.hide);
     document.removeEventListener('click', this.show);
   }
 
   timerName = () => {
     this.setState({ clockName: getRandomName() });
-  };
-
-  secondTime = () => {
-    this.setState({ today: new Date() });
   };
 
   hide = (event: { preventDefault: () => void; }) => {
@@ -64,10 +48,7 @@ export class App extends Component<{}, State> {
   };
 
   render() {
-    const { clockName, today, hasClock } = this.state;
-
-    // eslint-disable-next-line no-console, @typescript-eslint/no-unused-expressions
-    hasClock && console.info(today.toLocaleTimeString());
+    const { clockName, hasClock } = this.state;
 
     return (
       <div className="App">
@@ -75,21 +56,7 @@ export class App extends Component<{}, State> {
 
         {hasClock
           ? (
-            <div className="Clock">
-              <p>
-                Click Right Mouse Button to
-                <strong> Hide Clock</strong>
-              </p>
-              <strong className="Clock__name">
-                {clockName}
-              </strong>
-
-              {' time is '}
-
-              <span className="Clock__time">
-                {today.toLocaleTimeString()}
-              </span>
-            </div>
+            <Clock clockName={clockName} />
           )
           : (
             <p>
