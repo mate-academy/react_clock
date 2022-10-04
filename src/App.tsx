713a034ea3376fb01e884f0a261489c8ef0f1471
Aleksey Pushkalov <pushkalov86@gmail.com>
2022-10-04 19:37:47 +0300

    solution4

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,3 @@
-// import './App.scss';
-// import { Component } from 'react';
-// import { Clock } from './components/Clock';
-
-// function getRandomName(): string {
-//   const value = Math.random().toString().slice(2, 6);
-
-//   return `Clock-${value}`;
-// }
-
-// type State = {
-//   hasClock: boolean,
-//   clockName: string,
-// };
-
-// export class App extends Component<{}, State> {
-//   state = {
-//     hasClock: true,
-//     clockName: getRandomName(),
-//   };
-
-//   intervalName = 0;
-
-//   componentDidMount() {
-//     window.addEventListener('contextmenu', () => {
-//       this.setState({
-//         hasClock: false,
-//       });
-//     });
-
-//     document.addEventListener('click', () => {
-//       this.setState({
-//         hasClock: true,
-//       });
-//     });
-
-//     this.intervalName = window.setInterval(() => {
-//       this.setState({
-//         clockName: getRandomName(),
-//       });
-//     }, 3300);
-
-//     if (!this.state.hasClock) {
-//       window.clearInterval(this.intervalName);
-//     }
-//   }
-
-//   render() {
-//     const { hasClock, clockName } = this.state;
-
-//     return (
-//       <div className="App">
-//         <h1>React clock</h1>
-//         {hasClock && <Clock clockName={clockName} />}
-//       </div>
-//     );
-//   }
-// }
-
 import React from 'react';
 import './App.scss';
 import { Clock } from './components/Clock';
@@ -100,13 +41,6 @@ export class App extends React.Component<{}, State> {
 
   componentWillUnmount() {
     clearInterval(this.clockNameTimer);
-    this.setState({ hasClock: false });
-  }
-
-  changeClockVisibility() {
-    const { hasClock } = this.state;
-
-    return this.setState({ hasClock: !hasClock });
   }
 
   render() {
