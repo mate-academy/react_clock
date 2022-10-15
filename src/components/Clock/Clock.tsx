62c847b8014a7c0473ad60b3c3411cae4ea68f7b
Dima Romanenko <demigod177712@gmail.com>
2022-10-15 22:51:36 +0300

    clock timer was created

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+
+type Props = {
+  name: string,
+};
+
+type State = {
+  today: Date,
+};
+
+export class Clock extends Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  timerTime = 0;
+
+  componentDidMount() {
+    this.timerTime = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      window.console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerTime);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
