9bb412a5753b28310106855740653b300c44c9fc
Andriy Andrieiev <proph7000@gmail.com>
2022-10-17 20:08:17 +0300

    Fixes after code review

--- src/component/clock.tsx
+++ src/component/clock.tsx
@@ -12,20 +12,6 @@ function getToday(): string {
   return (new Date()).toUTCString().slice(-12, -4);
 }
 
-function extractNumberFromClockName(clockName: string): number | null {
-  let number = '';
-
-  for (let i = 0; i < clockName.length; i += 1) {
-    if ('0123456789'.includes(clockName[i])) {
-      number += clockName[i];
-    } else if (number.length > 0) {
-      break;
-    }
-  }
-
-  return (number.length === 0) ? null : +number;
-}
-
 export class Clock extends Component<Props, State> {
   state: Readonly<State> = {
     today: getToday(),
@@ -42,21 +28,12 @@ export class Clock extends Component<Props, State> {
   }
 
   componentDidUpdate(prevProps: Readonly<Props>): void {
-    const prevNameClockNumber = extractNumberFromClockName(prevProps.name);
-    const newNameClockNumber = extractNumberFromClockName(this.props.name);
-
-    if (prevNameClockNumber !== null && newNameClockNumber !== null) {
-      const timeBetweenCalls = (newNameClockNumber < prevNameClockNumber)
-        ? 10000 - prevNameClockNumber + newNameClockNumber
-        : newNameClockNumber - prevNameClockNumber;
-
-      if (timeBetweenCalls > 0) {
-        // eslint-disable-next-line no-console
-        console.debug(
-          `Renamed from ${prevProps.name}`
-            + ` to ${this.props.name}`,
-        );
-      }
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(
+        `Renamed from ${prevProps.name}`
+          + ` to ${this.props.name}`,
+      );
     }
   }
 
