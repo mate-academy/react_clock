b62bfb98dff81b70c469ef0b51c1480cfd0cd64f
Vitalii Babiienko <v.babiyenko@gmail.com>
2022-10-17 16:51:07 +0300

    solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import { Component } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      console.info(this.state.today.toLocaleTimeString());
+    }, 1000);
+  }
+
+  componentDidUpdate(prevDate: Props) {
+    const { clockName } = this.props;
+
+    if (clockName !== prevDate.clockName) {
+      console.debug(`Renamed from ${prevDate.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { clockName } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+        {' time is '}
+        <span className="Clock__time">
+          {today.toLocaleTimeString()}
+        </span>
+      </div>
+    );
+  }
+}
