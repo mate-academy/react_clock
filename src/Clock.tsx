256661d218520879a75cef83ebb54c93e7b30735
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-17 18:53:46 +0300

    Created clock and app components

---
+++ src/Clock.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+type ClockProps = {
+  clockName: string,
+};
+
+type ClockState = {
+  today: Date,
+};
+
+export class Clock extends React.Component<ClockProps, ClockState> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line
+      console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { clockName: name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
