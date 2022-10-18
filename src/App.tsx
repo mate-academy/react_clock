8aa9316e2377899b604312a2b4c0826597df1ae7
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-10-18 22:32:24 +0300

    creating clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,99 @@
 import React from 'react';
 import './App.scss';
 
-function getRandomName(): string {
+type State = {
+  time: Date;
+  value: string;
+  visibility: boolean;
+};
+
+function ClockName(): string {
   const value = Date.now().toString().slice(-4);
 
-  return `Clock-${value}`;
+  return value;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+export class App extends React.Component<{}, State> {
+  state = {
+    time: new Date(),
+    value: '',
+    visibility: true,
+  };
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  timerID?: NodeJS.Timeout;
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  valueTimerId?: NodeJS.Timeout;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => {
+        this.tick();
+      },
+      1000,
+    );
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+    this.valueTimerId = setInterval(
+      () => this.setState({
+        value: ClockName(),
+      }),
+      3300,
+    );
 
-        {' time is '}
+    document.addEventListener('contextmenu', this.rightMouseButtonHandle);
+    document.addEventListener('click', this.leftMouseButtonHandle);
+  }
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+  componentWillUnmount() {
+    if (this.timerID) {
+      clearInterval(this.timerID);
+    }
+
+    if (this.valueTimerId) {
+      clearInterval(this.valueTimerId);
+    }
+
+    document.removeEventListener('contextmenu', this.rightMouseButtonHandle);
+    document.removeEventListener('click', this.leftMouseButtonHandle);
+  }
+
+  rightMouseButtonHandle = () => {
+    this.setState({ visibility: false });
+  };
+
+  leftMouseButtonHandle = () => {
+    this.setState({ visibility: true });
+  };
+
+  tick() {
+    this.setState({
+      time: new Date(),
+    });
+
+    if (this.state.visibility) {
+      // eslint-disable-next-line
+      console.log(this.state.time.toLocaleTimeString());
+    }
+  }
+
+  render() {
+    const { time, value, visibility } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {visibility && (
+          <>
+            <strong className="Clock">
+              Clock-
+              {value}
+            </strong>
+            <span className="Clock__time">
+              {` time is ${time.toLocaleTimeString()}`}
+            </span>
+          </>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
