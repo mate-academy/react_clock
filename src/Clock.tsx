90ecbcf4557390e1a019088b6d66004a0b74a160
igor-1987 <igor.gayvoronsky@gmail.com>
2022-10-18 09:18:23 +0300

    react-clock was made

---
+++ src/Clock.tsx
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+type State = {
+  timeNow: Date;
+};
+
+export class Clock extends Component<Props, State> {
+  state = {
+    timeNow: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ timeNow: new Date() });
+
+      console.info(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.clockName !== this.props.clockName) {
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { timeNow } = this.state;
+    const { clockName } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {timeNow.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
