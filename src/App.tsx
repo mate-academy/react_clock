e8c6db91dd391b19d9639087f2cab729096a2fc1
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-10-19 18:13:23 +0300

    fix eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +8,57 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  hasClock: boolean,
+  clockName: string,
 };
+
+export class App extends Component<{}, State> {
+  state = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    document.addEventListener('contextmenu', this.hideClock.bind(this, false));
+
+    document.addEventListener('click', this.hideClock.bind(this, true));
+
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+  }
+
+  hideClock(value: boolean, event: MouseEvent) {
+    event.preventDefault();
+    this.setState({ hasClock: value });
+  }
+
+  render() {
+    const { clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {this.state.hasClock && <Clock name={clockName} />}
+      </div>
+    );
+  }
+}
+//
+// export const App: React.FC = () => {
+//
+//
+//
+//   // This code starts a timer
+//
+//
+//   // this code stops the timer
+//   window.clearInterval(timerId);
+//
+//   return (
+//
+//   );
+// };
