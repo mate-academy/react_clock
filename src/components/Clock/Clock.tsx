dc36a8f48bfd104b88d620fed458b71ad5417e84
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2022-10-28 15:33:04 +0300

    fix bug when after click time printed one more time

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -18,6 +18,8 @@ export class Clock extends React.Component<Props, State> {
   componentDidMount() {
     this.Interval = window.setInterval(() => {
       this.setState({ time: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.time.toUTCString().slice(-12, -4));
     }, 1000);
   }
 
@@ -26,9 +28,6 @@ export class Clock extends React.Component<Props, State> {
       // eslint-disable-next-line no-console
       console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
     }
-
-    // eslint-disable-next-line no-console
-    console.info(this.state.time.toUTCString().slice(-12, -4));
   }
 
   componentWillUnmount(): void {
