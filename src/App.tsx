120a3a35e57ee0c13707acbbe4d5d031bbf46f9f
Boris Kerekesha <kerekesha90@ukr.net>
2022-11-01 12:45:18 +0200

    remove eventListeners and interval

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,9 @@ export class App extends Component<{}, State> {
   timerClockName = 0;
 
   componentDidMount() {
-    document.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      this.setState({ hasClock: false });
-    });
+    document.addEventListener('contextmenu', this.handlerContextMenu);
 
-    document.addEventListener('click', () => {
-      this.setState({ hasClock: true });
-    });
+    document.addEventListener('click', this.handlerClick);
 
     if (this.state.hasClock) {
       this.timerClockName = window.setInterval(() => {
@@ -31,10 +26,25 @@ export class App extends Component<{}, State> {
     }
   }
 
+  componentWillUnmount(): void {
+    clearInterval(this.timerClockName);
+    document.removeEventListener('contextmenu', this.handlerContextMenu);
+    document.removeEventListener('click', this.handlerClick);
+  }
+
   getRandomName = (): string => {
     const value = Date.now().toString().slice(-4);
 
     return `Clock-${value}`;
+  };
+
+  handlerContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  handlerClick = () => {
+    this.setState({ hasClock: true });
   };
 
   render() {
