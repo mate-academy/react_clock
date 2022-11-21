7cf2b37332f71e8a5edd71e18941298bcfeff148
Artem66 <melodpi@gmail.com>
2022-11-21 17:35:37 +0000

    removes new lines

--- src/App.tsx
+++ src/App.tsx
@@ -52,11 +52,7 @@ export class App extends Component <{}, State> {
     return (
       <div className="App">
         <h1>React clock</h1>
-        {hasClock && (
-          <Clock
-            name={clockName}
-          />
-        )}
+        {hasClock && (<Clock name={clockName} />)}
       </div>
     );
   }
