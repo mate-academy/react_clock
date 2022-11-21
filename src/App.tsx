616d7925b6b205801011d3cf2db0408b745b1886
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-21 19:06:58 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -47,13 +47,13 @@ export class App extends Component<{}, State> {
   };
 
   render() {
-    const { hasClock } = this.state;
+    const { hasClock, clockName } = this.state;
 
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        {hasClock && <Clock name={this.state.clockName} />}
+        {hasClock && <Clock name={clockName} />}
       </div>
     );
   }
