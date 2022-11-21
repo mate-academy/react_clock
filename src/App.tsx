0c5d110758bb52545b0fd595fe1f09fb4fc81431
Andriy Shymkiv <winamondezik@gmail.com>
2022-11-21 14:05:50 +0200

    clockName fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ type State = {
 export class App extends Component<{}, State> {
   state: Readonly<State> = {
     hasClock: true,
-    clockName: 'Clock-0000',
+    clockName: 'Clock-0',
   };
 
   timerId = 0;
