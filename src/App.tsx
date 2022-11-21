9fe3f05f5feac3e69e942ee4245cb2d7f68a495a
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-21 18:36:14 +0200

    tests passed

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,47 @@ import { Clock } from './components/Clock';
 
 type State = {
   hasClock: boolean;
+  clockName: string;
 };
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
 
 export class App extends Component<{}, State> {
   state = {
     hasClock: true,
+    clockName: 'Clock-0',
   };
 
+  clockNameId = 0;
+
   componentDidMount() {
-    document.addEventListener('contextmenu', (event) => {
-      event.preventDefault(); // not to show the context menu
+    this.clockNameId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
 
-      this.setState({ hasClock: false });
-    });
-
-    document.addEventListener('click', () => {
-      this.setState({ hasClock: true });
-    });
+    document.addEventListener('contextmenu', this.callBackContextMenu);
+    document.addEventListener('click', this.callBackClick);
   }
 
   componentWillUnmount() {
-
+    document.removeEventListener('contextmenu', this.callBackContextMenu);
+    document.removeEventListener('click', this.callBackClick);
+    clearInterval(this.clockNameId);
   }
+
+  callBackContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.setState({ hasClock: false });
+  };
+
+  callBackClick = () => {
+    this.setState({ hasClock: true });
+  };
 
   render() {
     const { hasClock } = this.state;
@@ -34,7 +53,7 @@ export class App extends Component<{}, State> {
       <div className="App">
         <h1>React clock</h1>
 
-        {hasClock && <Clock />}
+        {hasClock && <Clock name={this.state.clockName} />}
       </div>
     );
   }
