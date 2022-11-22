c3f8a5e03e73fab8590c4630da8abfec49cefedc
Danyla Krygin <underworldsan@gmail.com>
2022-11-22 09:45:34 +0200

    Clock completed

---
+++ src/Components/Clock.tsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends Component< Props, State > {
+  state = {
+    today: new Date(),
+  };
+
+  dateTimer = 0;
+
+  componentDidMount() {
+    this.dateTimer = window.setInterval(() => {
+      this.setState({
+        today: new Date(),
+      });
+
+      window.console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.dateTimer);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { name } = this.props;
+
+    const date = today.toUTCString().slice(-12, -4);
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date}
+        </span>
+      </div>
+    );
+  }
+}
