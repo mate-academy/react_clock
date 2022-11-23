cbad1fc430c4e137c1fdfdbd6f13bbad441bcadd
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-23 13:32:38 +0200

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Clock } from './components/Clock/Clock';
+import { Clock } from './components/Clock/Clock.js';
 import './App.scss';
 
 interface State {
