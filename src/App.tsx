07b91d83c854204aa08cea1c3275849351d8851c
Danil Serzhantov <sergo.svd@gmail.com>
2022-11-25 12:25:57 +0200

    v1 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,9 @@ export class App extends React.Component<{}, State> {
       <div className="App">
         <h1>React clock</h1>
 
-        {hasClock && <Clock clockName={clockName} />}
+        {hasClock && (
+          <Clock clockName={clockName} />
+        )}
       </div>
     );
   }
