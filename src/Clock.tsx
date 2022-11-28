1ef993af357551e0d924ef6b38180f67669af1a4
Bohdan Kovalenko <bog7807@gmail.com>
2022-11-28 09:30:47 +0200

    clock 1

---
+++ src/Clock.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    today: new Date(),
+  };
+
+  timeId = 0;
+
+  componentDidMount() {
+    this.timeId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timeId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
