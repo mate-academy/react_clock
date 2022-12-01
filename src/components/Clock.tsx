dd7e3c1190efb4d08e7b8d1c6d9c7ce3d740e452
Kostya Berehoviy <kostyaber3@gmail.com>
2022-12-01 16:54:10 +0200

    Missing files were added

---
+++ src/components/Clock.tsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+
+type Props = {
+  name : string;
+};
+
+type State = {
+  date: Date,
+};
+
+export class Clock extends Component <Props, State> {
+  state = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      // eslint-disable-next-line
+      console.info(this.state.date.toUTCString().slice(-12, -4));
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
