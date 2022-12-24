9a9595d3762276b8b7cec3457cf363f3abb230e8
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2022-12-25 00:46:49 +0200

    fixed console bugs

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,50 +1,50 @@
 import { Component } from 'react';
 
 type Props = {
-  name: string;
+  clockName: string;
 };
 
 type State = {
-  time: string;
+  time: Date;
 };
 
 export class Clock extends Component<Props, State> {
-  state: Readonly<State> = {
-    time: (new Date()).toUTCString().slice(-12, -4),
+  state = {
+    time: new Date(),
   };
 
-  timerId = 0;
+  timerID = 0;
 
   componentDidMount() {
-    this.timerId = window.setInterval(() => {
-      this.setState({ time: (new Date()).toUTCString().slice(-12, -4) });
+    this.timerID = window.setInterval(() => {
+      this.setState({ time: new Date() });
       // eslint-disable-next-line no-console
-      console.info(this.state.time);
+      console.info(this.state.time.toUTCString().slice(-12, -4));
     }, 1000);
   }
 
   componentDidUpdate(prevProps: Readonly<Props>) {
-    if (prevProps.name !== this.props.name) {
+    if (prevProps.clockName !== this.props.clockName) {
       // eslint-disable-next-line no-console
-      console.debug(`Rename from ${prevProps.name} to ${this.props.name}`);
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
     }
   }
 
   componentWillUnmount() {
-    window.clearInterval(this.timerId);
+    window.clearInterval(this.timerID);
   }
 
   render() {
     return (
       <div className="Clock">
         <strong className="Clock__name">
-          {this.props.name}
+          {this.props.clockName}
         </strong>
 
         {' time is '}
 
         <span className="Clock__time">
-          {this.state.time}
+          {this.state.time.toUTCString().slice(-12, -4)}
         </span>
       </div>
     );
