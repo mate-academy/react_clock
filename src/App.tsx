09c4ae090f46c2eb236017b17c626ee131145f32
Anatolii <batstolya@gmail.com>
2023-01-01 11:47:58 +0200

    added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import './App.scss';
 
+import { Card, Icon, Image } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
 import { Clock } from './components/Clock';
 
 type State = {
@@ -54,15 +57,39 @@ export class App extends Component<{}, State> {
   };
 
   render() {
-    const {
-      hasClock,
-      clockName,
-    } = this.state;
+    const { hasClock, clockName } = this.state;
+    const today = new Date();
+    const dateISO = today.toISOString().slice(0, 10);
+    const dateLocaleString = today.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
 
     return (
-      <div className="App">
-        <h1>React clock</h1>
-        {hasClock && <Clock clockName={clockName} />}
+      <div className="container">
+        <Card centered className="App">
+          <Image
+            src="https://cdn-icons-png.flaticon.com/512/4305/4305432.png"
+            wrapped
+            ui={false}
+          />
+          <Card.Content>
+            <Card.Header>React clock</Card.Header>
+            <Card.Meta>
+              <span className="date">react clock</span>
+            </Card.Meta>
+            <Card.Description>
+              {hasClock && <Clock clockName={clockName} />}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name="clock" />
+            <time dateTime={dateISO} data-cy="date">
+              {dateLocaleString}
+            </time>
+          </Card.Content>
+        </Card>
       </div>
     );
   }
