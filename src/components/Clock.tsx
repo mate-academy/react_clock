a4e06130af0968c971375950d4721f7f35dcfa12
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2023-01-03 03:13:09 +0100

    clock created

---
+++ src/components/Clock.tsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import './Clock.scss';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  today: Date,
+};
+
+export class Clock extends Component<Props, State> {
+  state: Readonly<State> = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line no-console
+      console.info((this.state.today).toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <h1 className="Clock__header">React clock</h1>
+        <div className="Clock__name">
+          {name}
+        </div>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -7)}
+        </span>
+        <div className="Seconds">
+          {today.toUTCString().slice(-6, -4)}
+        </div>
+      </div>
+    );
+  }
+}
