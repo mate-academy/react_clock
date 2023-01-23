87492afa8ba10e54e364e3de489fddf879959ccb
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-01-23 23:31:06 +0200

    firsta commit

---
+++ src/components/Clock.tsx
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+
+type State = {
+  today: Date;
+};
+
+type Props = {
+  clockName: string;
+};
+
+export class Clock extends Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      /* eslint-disable */
+      console.info(this.fixDateFormat(this.state.today));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { clockName } = this.props;
+
+    if (clockName !== prevProps.clockName) {
+      /* eslint-disable */
+      console.debug(`Renamed from ${prevProps.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  fixDateFormat = (date: Date) => {
+    return date.toString().slice(-50, -42);
+  };
+
+  render() {
+    const { today } = this.state;
+    const { clockName } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.fixDateFormat(today)}</span>
+      </div>
+    );
+  }
+}
