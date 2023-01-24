7db2bb5489c2d6806a81870e6ae959068ae7878b
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-01-24 21:34:01 +0200

    React clock was implemented

---
+++ src/components/Clock.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+type Props = {
+  name: string,
+};
+
+export class Clock extends React.Component<Props, {}> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { name } = this.props;
+
+    if (name !== prevProps.name) {
+      // console.debug(`Renamed from ${prevProps.name} to ${name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
