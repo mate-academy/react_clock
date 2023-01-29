7214af5641ed40663a1eedd524d2218082965cfa
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-29 21:26:53 +0100

    React_Clock were implemented

---
+++ src/Clock.tsx
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+
+type ClockProps = {
+  name: string;
+};
+
+type ClockState = {
+  today: Date;
+};
+
+export class Clock extends Component<ClockProps, ClockState> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: ClockProps) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(
+        `Renamed from ${prevProps.name} to ${this.props.name}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {this.props.name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
