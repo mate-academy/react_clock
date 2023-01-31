828629250d83bc7efc6b134504a7bfce06d09ae5
Vlad Horovyi <vlad78292@gmail.com>
2023-01-31 16:56:11 +0200

    clock file was added

---
+++ src/Components/Clock.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  currentTime: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    currentTime: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ currentTime: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.currentTime.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const previousName = prevProps.name;
+    const thisName = this.props.name;
+
+    if (previousName !== thisName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${previousName} to ${thisName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { currentTime } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {currentTime.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
