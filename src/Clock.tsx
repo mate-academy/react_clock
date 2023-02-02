29375ef3731ef15027c7bdcb572ee1ab05c0b944
OksanaBaloh <obaloh1108@gmail.com>
2023-02-02 22:11:11 +0200

    Solution

---
+++ src/Clock.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+
+function setDateFormat(date:Date) {
+  return date.toUTCString().slice(-12, -4);
+}
+
+type Props = {
+  name: string,
+};
+
+type State = {
+  date: Date,
+};
+
+export class Clock extends Component<Props, State> {
+  state: Readonly<State> = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(setDateFormat(this.state.date));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps:Props) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {setDateFormat(date)}
+        </span>
+      </div>
+    );
+  }
+}
