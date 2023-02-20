5e2de569f54c7b52b479b485807b5df9b04fa114
Taras Baran <baranprivat22@gmail.com>
2023-02-20 14:27:25 +0200

    added components folder

---
+++ src/components/Clock.tsx
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+export class Clock extends Component<Props, {}> {
+  timerId = 0;
+
+  state = {
+    today: new Date(),
+  };
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+
+      // eslint-disable-next-line no-console
+      console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>) {
+    const { clockName } = this.props;
+
+    if (prevProps.clockName !== clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.clockName} to ${clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {this.props.clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
