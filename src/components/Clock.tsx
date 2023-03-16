ab647cebed42fb2c5d9bfb6c5a6403c686579e90
Sasha Kononenko <nurchiksasha@gmail.com>
2023-03-16 20:50:42 +0200

    react clock

---
+++ src/components/Clock.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface Props {
+  name: string;
+}
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    today: new Date(),
+  };
+
+  timer = 0;
+
+  componentDidMount() {
+    this.timer = window.setInterval(() => {
+      this.setState({ today: new Date() });
+    });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.name !== prevProps.name) {
+      window.console.debug(
+        `Renamed from ${prevProps.name} to ${this.props.name}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
