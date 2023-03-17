e56ff9efa8bdda9f868ed165e017e89ffe39503e
Denys Dubas <den.dubas.44@gmail.com>
2023-03-17 16:02:49 +0200

    Solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+type State = {
+  today: Date;
+};
+
+const getNormTime = (date: Date) => (
+  date.toUTCString().slice(-12, -4)
+);
+
+export class Clock extends Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  clockId = 0;
+
+  componentDidMount() {
+    this.clockId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+
+      window.console.info(
+        getNormTime(this.state.today),
+      );
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    if (this.props.clockName !== prevProps.clockName) {
+      window.console.debug(
+        `Renamed from ${prevProps.clockName} to ${this.props.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount(): void {
+    clearInterval(this.clockId);
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {getNormTime(today)}
+        </span>
+      </div>
+    );
+  }
+}
