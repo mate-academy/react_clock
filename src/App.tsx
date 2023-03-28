d82229334040337b4a714d789129e7bf8882d655
Peti51 <galpeter31@gmail.com>
2023-03-28 20:10:54 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -26,31 +26,44 @@ export class App extends React.Component<Props, State> {
     hasClock: true,
   };
 
+  timerID = 0;
+
   componentDidMount() {
     window.setInterval(() => {
       this.setState({ time: new Date().toLocaleTimeString() });
     }, 1000);
 
-    window.setInterval(() => {
-      if (this.state.hasClock === true) {
-        console.info(this.state.time);
-      }
-    }, 1000);
+    this.timerID = window.setInterval(() => {
+      this.setState(prevState => (
+        { oldName: prevState.newName }
+      ));
 
-    window.setInterval(() => {
       this.setState({ newName: getRandomName() });
-
-      if (this.state.hasClock === true) {
-        console.info(`Renamed from ${this.prevState()} to ${this.state.newName}`);
-      }
     }, 3300);
 
     document.addEventListener('mousedown', this.clockShow);
   }
 
-  prevState = () => {
-    this.state.oldName = this.state.newName;
-  };
+  componentDidUpdate(prevProps: {}, prevState: {}) {
+    if (this.state.hasClock === true
+      && this.state.oldName !== prevState
+      && this.state.newName !== prevState
+      && this.state.oldName !== prevProps
+      && this.state.newName !== prevProps) {
+      console.info(`Renamed from ${this.state.oldName} to ${this.state.newName}`);
+    }
+
+    if (this.state.hasClock === true
+      && this.state.time !== prevState
+      && this.state.time !== prevProps) {
+      console.info(this.state.time);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    document.removeEventListener('mousedown', this.clockShow);
+  }
 
   clockShow = (click: MouseEvent) => {
     click.preventDefault();
