462355a74b450d32ed0bf2986b5a90a88e90b055
diamat1997 <diamat1997@gmail.com>
2023-03-28 14:08:25 +0300

    commit message

---
+++ src/Clock.tsx
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+import './App.scss';
+
+type Props = {
+  name: string,
+};
+
+export class Clock extends Component<Props> {
+  state = {
+    time: new Date(),
+  };
+
+  timerId: number | undefined;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ time: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.time.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { name } = this.props;
+
+    if (prevProps.name !== name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {time.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
