19c98a35785173c0a5a88a5a87b1e42c06c20536
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-15 22:58:09 +0100

    implemented Clock component with updating

---
+++ src/components/Clock.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+interface State {
+  today: string;
+}
+
+interface Props {
+  clockName: string;
+}
+
+const getDate = () => new Date().toUTCString().slice(-12, -4);
+
+export class Clock extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    today: getDate(),
+  };
+
+  timerId: number | null = null;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: getDate() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.today);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    if (this.timerId) {
+      window.clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {this.props.clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.today}
+        </span>
+      </div>
+    );
+  }
+}
