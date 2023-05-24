872d1dc54b31d086150e4141ab0f67dbc1f6749a
Denys Didenko <ddidenko441@gmail.com>
2023-05-24 22:40:22 +0300

    add react clock

---
+++ src/components/Clock.tsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+type State = {
+  currentTime: string;
+};
+
+const getCurrentTime = () => new Date().toUTCString().slice(-12, -4);
+
+export class Clock extends Component<Props, State> {
+  state = {
+    currentTime: getCurrentTime(),
+  };
+
+  interval: number | null = null;
+
+  componentDidMount() {
+    this.interval = window.setInterval(() => {
+      this.setState({ currentTime: getCurrentTime() });
+
+      // eslint-disable-next-line no-console
+      console.info(this.state.currentTime);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { currentTime } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {currentTime}
+        </span>
+      </div>
+    );
+  }
+}
