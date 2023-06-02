e49ef2009fb0aaba526be2db5bcbeb7809d6c327
Iryna Konovalova <iruna191020@gmail.com>
2023-06-02 15:04:47 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
+
+// export const App: React.FC = () => {
+//   const today = new Date();
+//   let clockName = 'Clock-0';
+
+//   // This code starts a timer
+//   const timerId = window.setInterval(() => {
+//     clockName = getRandomName();
+//   }, 3300);
+
+//   // this code stops the timer
+//   window.clearInterval(timerId);
+
+//   return (
+//     <div className="App">
+//       <h1>React clock</h1>
+//       <Clock />
+//     </div>
+//   );
+// };
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +28,68 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  clockName: string;
+  hasClock: boolean,
 };
+
+export class App extends React.Component<{}, State> {
+  state = {
+    clockName: 'Clock-0',
+    hasClock: true,
+  };
+
+  nameTimerId = 0;
+
+  currentName = this.state.clockName;
+
+  oldName = this.state.clockName;
+
+  componentDidMount() {
+    this.nameTimerId = window.setInterval(() => {
+      this.setState({
+        clockName: getRandomName(),
+      });
+    }, 3300);
+
+    document.addEventListener('contextmenu', this.hideClock);
+    document.addEventListener('click', this.showClock);
+  }
+
+  componentDidUpdate(_prevProps: {}, prevState: State) {
+    this.oldName = prevState.clockName;
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.nameTimerId);
+
+    document.removeEventListener('contextmenu', this.hideClock);
+    document.removeEventListener('click', this.showClock);
+  }
+
+  hideClock = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.setState({
+      hasClock: false,
+    });
+  };
+
+  showClock = () => {
+    this.setState({
+      hasClock: true,
+    });
+  };
+
+  render() {
+    const { hasClock, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {hasClock
+          && <Clock name={clockName} />}
+      </div>
+    );
+  }
+}
---
+++ src/Clock.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  today: Date,
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  timeTimerId = 0;
+
+  componentDidMount() {
+    this.timeTimerId = window.setInterval(() => {
+      this.setState({
+        today: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    if (((today !== prevState.today))
+    ) {
+      /* eslint-disable no-console */
+      console.info(today.toUTCString().slice(-12, -4));
+    }
+
+    if ((name !== prevProps.name)
+    ) {
+      console.debug(`Renamed from ${prevProps.name} to ${name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timeTimerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
