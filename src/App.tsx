3d8d3d4d8d7b4cc5c155a74930cb15ca893ba1fd
Iryna Konovalova <iruna191020@gmail.com>
2023-06-03 20:59:54 +0200

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,6 @@ import React from 'react';
 import './App.scss';
 import { Clock } from './Clock';
 
-// export const App: React.FC = () => {
-//   const today = new Date();
-//   let clockName = 'Clock-0';
-
-//   // This code starts a timer
-//   const timerId = window.setInterval(() => {
-//     clockName = getRandomName();
-//   }, 3300);
-
-//   // this code stops the timer
-//   window.clearInterval(timerId);
-
-//   return (
-//     <div className="App">
-//       <h1>React clock</h1>
-//       <Clock />
-//     </div>
-//   );
-// };
-
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
 
