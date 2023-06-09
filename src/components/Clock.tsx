3120c9ccb56cca730e17ff529b75a80a7431c21d
Veronika Pastushkova <d9169393507@gmai.com>
2023-06-09 13:58:17 +0300

    add task solution

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -48,7 +48,7 @@ export class Clock extends React.Component <Props, State> {
           {name}
         </strong>
 
-        {' time is '}
+        <span>{' time is '}</span>
 
         <span className="Clock__time">
           {timeString}
