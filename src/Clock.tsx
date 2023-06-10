45a332bd20d25f3cfe1c2511fa0df53bbf2fcbc1
Yulia Shvets <juls.shvets@gmail.com>
2023-06-10 09:30:25 +0300

    Solution

---
+++ src/Clock.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface Props {
+  clockName : string,
+}
+
+interface State {
+  date: Date,
+}
+
+export class Clock extends React.Component <Props, State> {
+  state = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+
+      window.console.info(this.state.date.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    if (prevProps.clockName !== this.props.clockName) {
+      window.console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
