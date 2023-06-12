60538a9a73d8a3f7ef892dd8cb2e9bfcff0ef5b5
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2023-06-12 07:20:18 +0300

    add task solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+
+type Props = {
+  clockName: string,
+};
+
+type State = {
+  currentTime: string
+};
+
+const getCurrentTime = () => new Date().toUTCString().slice(-12, -4);
+
+export class Clock extends Component<Props, State> {
+  state = {
+    currentTime: getCurrentTime(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ currentTime: getCurrentTime() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.currentTime);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.clockName === this.props.clockName) {
+      return;
+    }
+
+    // eslint-disable-next-line no-console
+    console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { currentTime } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {currentTime}
+        </span>
+      </div>
+    );
+  }
+}
