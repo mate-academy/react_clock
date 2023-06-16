4ec06d0b08a6dc8a3a7ae261db11aa0d81f109fe
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-06-16 17:40:33 +0300

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,6 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-// export const App: React.FC = () => {
-//   const today = new Date();
-//   let clockName = 'Clock-0';
-
-//   // This code starts a timer
-//   const timerId = window.setInterval(() => {
-//     clockName = getRandomName();
-//   }, 3300);
-
-//   // this code stops the timer
-//   window.clearInterval(timerId);
-
-//   return (
-//     <div className="App">
-//       <h1>React clock</h1>
-
-//       <div className="Clock">
-//         <strong className="Clock__name">
-//           {clockName}
-//         </strong>
-
-//         {' time is '}
-
-//         <span className="Clock__time">
-//           {today.toUTCString().slice(-12, -4)}
-//         </span>
-//       </div>
-//     </div>
-//   );
-// };
-
 type State = {
   hasClock: boolean;
   clockName: string;
