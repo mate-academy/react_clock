0c1fae7db221355110fca9e9ef586ffbfece316e
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-22 15:38:40 +0300

    code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +8,52 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+type State = {
+  clockName: string,
+  clockTrue: boolean,
+}
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+export class App extends React.Component<{}, State> {
+  state = {
+    clockName: 'Clock-0',
+    clockTrue: true,
+  };
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  timerId = 0;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+    document.addEventListener('contextmenu', this.rightClick);
+    document.addEventListener('click', this.leftClick);
+  }
 
-        {' time is '}
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+  rightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ clockTrue: false });
+  };
+
+  leftClick = () => {
+    this.setState({ clockTrue: true });
+  };
+
+  render() {
+    const {
+      clockName,
+      clockTrue,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+        {clockTrue && <Clock name={clockName} />}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
