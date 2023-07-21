3ce943dbf19adf2fb6c1e6451eef67243bacfdd5
Eleonora Demikh <deademikh@gmail.com>
2023-07-21 18:01:49 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
+
+type State = {
+  hasVisibleClock: boolean;
+  clockName: string,
+};
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,33 +13,69 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+export class App extends React.Component {
+  state: State = {
+    hasVisibleClock: true,
+    clockName: 'Clock-0',
+  };
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  timerId = 0;
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({
+        clockName: getRandomName(),
+      });
+    }, 3300);
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+    document.addEventListener('contextmenu', this.hideClock);
+    document.addEventListener('click', this.unhideClock);
+  }
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
 
-        {' time is '}
+    document.removeEventListener('contextmenu', this.hideClock);
+    document.removeEventListener('click', this.unhideClock);
+  }
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+  unhideClock = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.setState({
+      hasVisibleClock: true,
+    });
+  };
+
+  hideClock = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.setState({
+      hasVisibleClock: false,
+    });
+  };
+
+  render() {
+    const { hasVisibleClock, clockName } = this.state;
+    // const today = new Date();
+    // let clockName = 'Clock-0';
+
+    // // This code starts a timer
+    // const timerId = window.setInterval(() => {
+    //   clockName = getRandomName();
+    // }, 3300);
+
+    // // this code stops the timer
+    // window.clearInterval(timerId);
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {hasVisibleClock && (
+          <Clock name={clockName} />
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
---
+++ src/components/Clock.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+type State = {
+  today: Date;
+};
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: State = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState(currentTime => ({
+        ...currentTime,
+        today: new Date(),
+      }));
+      // eslint-disable-next-line
+      console.info(this.state.today.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
