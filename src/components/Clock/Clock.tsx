ca994c69ecc711d5b342caeb7079830018707cbc
domkab <dominykas.bobinas@gmail.com>
2023-08-08 17:34:14 +0300

    1.0.1 added missing features

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable no-console */
 import React from 'react';
 
 interface State {
   currentTime: string,
   clockName: string,
+  isVisible: boolean,
 }
 
 export class Clock extends React.Component<{}, State> {
   state: State = {
     clockName: 'Clock-0',
     currentTime: new Date().toUTCString().slice(-12, -4),
+    isVisible: true,
   };
 
   timerID: NodeJS.Timeout | null = null;
@@ -23,6 +26,14 @@ export class Clock extends React.Component<{}, State> {
     this.nameTimerID = setInterval(
       () => this.updateName(), 3300,
     );
+
+    document.addEventListener('contextmenu', this.handleRightClick);
+  }
+
+  componentDidUpdate(_prevProps: {}, prevState: State) {
+    if (prevState.clockName !== this.state.clockName) {
+      console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
+    }
   }
 
   componentWillUnmount() {
@@ -33,6 +44,8 @@ export class Clock extends React.Component<{}, State> {
     if (this.nameTimerID) {
       clearInterval(this.nameTimerID);
     }
+
+    document.removeEventListener('contextmenu', this.handleRightClick);
   }
 
   getRandomName = (): string => {
@@ -41,38 +54,52 @@ export class Clock extends React.Component<{}, State> {
     return `Clock ${value}`;
   };
 
+  handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.setState(prevState => ({ isVisible: !prevState.isVisible }));
+  };
+
   updateTime() {
+    const newTime = new Date().toUTCString().slice(-12, -4);
+
     this.setState(
       {
-        currentTime: new Date().toUTCString().slice(-12, -4),
+        currentTime: newTime,
+      },
+      () => {
+        if (this.state.isVisible) {
+          console.info(this.state.currentTime);
+        }
       },
     );
   }
 
   updateName() {
-    this.setState(
-      {
-        clockName: this.getRandomName(),
-      },
-    );
+    this.setState({
+      clockName: this.getRandomName(),
+    });
   }
 
   render() {
+    const { isVisible, clockName, currentTime } = this.state;
+
     return (
       <div className="App">
         <h1>React clock</h1>
+        {isVisible && (
+          <div className="Clock">
+            <strong className="Clock__name">
+              {clockName}
+            </strong>
 
-        <div className="Clock">
-          <strong className="Clock__name">
-            {this.state.clockName}
-          </strong>
+            {' time is '}
 
-          {' time is '}
-
-          <span className="Clock__time">
-            {this.state.currentTime}
-          </span>
-        </div>
+            <span className="Clock__time">
+              {currentTime}
+            </span>
+          </div>
+        )}
       </div>
     );
   }
