042b945357c2a0e051233c9e3670d454a896e507
Dolnys <thedolnys@gmail.com>
2023-08-28 10:32:46 +0200

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,67 @@
-import React from 'react';
-import './App.scss';
-import { Clock } from './Clock';
+  import React from 'react';
+  import './App.scss';
+  import { Clock } from './Clock';
 
-export function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
+  export function getRandomName(): string {
+    const value = Date.now().toString().slice(-4);
 
-  return `Clock-${value}`;
-}
+    return `Clock-${value}`;
+  }
 
-type State = {
-  clockName: string;
-  hasClock: boolean;
-};
-
-export class App extends React.Component<State> {
-  state = {
-    clockName: 'Clock-0',
-    hasClock: true,
+  type State = {
+    clockName: string;
+    hasClock: boolean;
   };
 
-  timerId = 0;
+  export class App extends React.Component<State> {
+    state = {
+      clockName: 'Clock-0',
+      hasClock: true,
+    };
 
-  componentDidMount() {
-    this.timerId = window.setInterval(() => {
-      this.setState({
-        clockName: getRandomName(),
+    timerId = 0;
+
+    componentDidMount() {
+      this.timerId = window.setInterval(() => {
+        this.setState({
+          clockName: getRandomName(),
+        });
+      }, 3300);
+
+      document.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        this.setState({ hasClock: false });
       });
-    }, 3300);
 
-    document.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      this.setState({ hasClock: false });
-    });
+      document.addEventListener('click', () => {
+        this.setState({ hasClock: true });
+      });
+    }
 
-    document.addEventListener('click', () => {
-      this.setState({ hasClock: true });
-    });
+    componentWillUnmount() {
+      window.clearInterval(this.timerId);
+      
+      document.removeEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        this.setState({ hasClock: false });
+      });
+
+      document.removeEventListener('click', () => {
+        this.setState({ hasClock: true });
+      });
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <h1>React clock</h1>
+          {this.state.hasClock && (
+            <Clock
+              hasClock={this.state.hasClock}
+              clockName={this.state.clockName}
+            />
+          )}
+        </div>
+      );
+    }
   }
-
-  componentWillUnmount() {
-    window.clearInterval(this.timerId);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>React clock</h1>
-        {this.state.hasClock && (
-          <Clock
-            hasClock={this.state.hasClock}
-            clockName={this.state.clockName}
-          />
-        )}
-      </div>
-    );
-  }
-}
