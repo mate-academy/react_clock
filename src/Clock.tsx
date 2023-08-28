13475328a99f0548eaea31e7c98e995ca063d188
Kamil <majcherczak.kamil@protonmail.com>
2023-08-28 12:28:35 +0200

    add task solution

---
+++ src/Clock.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+type State = {
+  today: Date;
+};
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>) {
+    if (prevProps.name !== this.props.name) {
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+
+    console.info(`${this.state.today.toUTCString().slice(-12, -4)}`);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
