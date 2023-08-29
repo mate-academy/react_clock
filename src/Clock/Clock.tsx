c870d9ba596b0bf5d1fceb95e8dde9127be869df
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-08-29 13:11:16 +0200

    task solution

---
+++ src/Clock/Clock.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+type ClockProp = {
+  name: string;
+};
+
+type State = {
+  date: Date,
+};
+
+export class Clock extends React.Component<ClockProp, State> {
+  state: Readonly<State> = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<ClockProp>,
+    prevState: Readonly<State>,
+  ) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+
+    if (prevState.date !== this.state.date) {
+      // eslint-disable-next-line no-console
+      console.info(this.state.date.toUTCString().slice(-12, -4));
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { date } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
