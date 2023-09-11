dee6c767aced1377cfd10ad64e6a55e4c21d0b63
Yuliia Antonenko <shinjisan311@gmail.com>
2023-09-11 18:45:20 +0300

    Task solution - created Clock component

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,83 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  date: Date,
+  clockName: string,
+  showClock: boolean,
 };
+
+export class App extends React.Component<State> {
+  clockName = 'Clock-0';
+
+  state: State = {
+    date: new Date(),
+    clockName: this.clockName,
+    showClock: true,
+  };
+
+  timerId = 0;
+
+  interval = 0;
+
+  componentDidMount() {
+    this.interval = window.setInterval(() => {
+      this.setState({ date: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.date.toUTCString().slice(-12, -4));
+    }, 1000);
+
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+
+    document.addEventListener('contextmenu', (event: MouseEvent) => {
+      event.preventDefault();
+      this.setState({ showClock: false });
+    });
+
+    document.addEventListener('click', () => {
+      this.setState({ showClock: true });
+    });
+  }
+
+  componentDidUpdate(_: Readonly<State>, prevState: Readonly<State>) {
+    const newName = this.state.clockName;
+    const oldName = prevState.clockName;
+
+    if (oldName !== newName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${oldName} to ${newName}`);
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+    window.clearInterval(this.timerId);
+    document.removeEventListener('contextmenu', () => { });
+  }
+
+  render() {
+    const { date, clockName } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.showClock && (
+          <div className="Clock">
+            <strong className="Clock__name">
+              {clockName}
+            </strong>
+
+            {' time is '}
+
+            <span className="Clock__time">
+              {date.toUTCString().slice(-12, -4)}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
