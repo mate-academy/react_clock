42297096c435e678a7afd02459cc08940f295736
vanom <envinenvin@gmail.com>
2023-09-12 18:50:24 +0300

    implemented initial solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+function formatedTime(time: Date) {
+  return time.toUTCString().slice(-12, -4);
+}
+
+type Props = {
+  name: string,
+};
+
+type State = {
+  today: Date,
+};
+
+export class Clock extends React.PureComponent<Props, State> {
+  state: State = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(formatedTime(this.state.today));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {formatedTime(today)}
+        </span>
+      </div>
+    );
+  }
+}
