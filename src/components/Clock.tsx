c022181213fb0093bfaae5666f791e23b0927f56
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-09-12 17:56:15 +0300

    created app class component & clock class component

---
+++ src/components/Clock.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+type Props = {
+  clockName: string,
+};
+
+type State = {
+  today: string,
+};
+
+export class Clock extends React.Component<Props, State> {
+  timerId = 0;
+
+  state: State = {
+    today: new Date().toUTCString().slice(-12, -4),
+  };
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({ today: new Date().toUTCString().slice(-12, -4) });
+
+      // eslint-disable-next-line no-console
+      console.info(this.state.today);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const { today } = this.state;
+    const { clockName } = this.props;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today}
+        </span>
+      </div>
+    );
+  }
+}
