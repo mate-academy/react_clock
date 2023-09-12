83d270af0a67ae25f8e6c68cb6a910f056f9741c
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-09-12 12:24:24 +0300

    console bug was fixed

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
 
-// function to get local time in case we need to show current local time
-// (can't pass test with this function)
-
-// function getLocalTime() { // uncomment this function for local time
-//   const localTime = new Date();
-
-//   localTime.setHours(localTime.getHours() + 3);
-
-//   return localTime;
-// }
-
 type Props = {
   clockName: string,
 };
@@ -27,26 +16,17 @@ export class Clock extends React.Component<Props, State> {
   timerId = 0;
 
   componentDidMount(): void {
-    // this.setState({ time: getLocalTime() }); // uncomment this line and corresponding function for local time
+    this.timerId = window.setInterval(() => {
+      this.setState({ time: new Date() });
 
-    this.timerId = window.setInterval(() => (
-      this.updateTime()), 1000);
-  }
-
-  componentDidUpdate(): void {
-    // eslint-disable-next-line no-console
-    console.info(this.state.time.toUTCString().slice(-12, -4));
+      // eslint-disable-next-line no-console
+      console.info(this.state.time.toUTCString().slice(-12, -4));
+    }, 1000);
   }
 
   componentWillUnmount(): void {
     window.clearInterval(this.timerId);
   }
-
-  updateTime = () => {
-    // this.setState({ time: getLocalTime() }); // uncomment this line and corresponding function for local time
-
-    this.setState({ time: new Date() }); // comment this line for local time
-  };
 
   render(): React.ReactNode {
     return (
