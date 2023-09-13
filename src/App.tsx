106d56eb2e81d5ad11c5de7782684648efbc3bdf
neovendano <kolesnyk.vik@gmail.com>
2023-09-13 14:22:42 +0300

    add Clock component with {name} props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock/Clock';
 
-function getRandomName(): string {
+/* function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
 
   return `Clock-${value}`;
@@ -36,4 +37,69 @@ export const App: React.FC = () => {
       </div>
     </div>
   );
-};
+
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
+}; */
+
+interface AppState {
+  hasClock: boolean;
+  clockName: string;
+}
+
+export class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
+
+  nameUpdateInterval: number | undefined;
+
+  componentDidMount() {
+    document.addEventListener('contextmenu', this.handleRightClick);
+    document.addEventListener('click', this.handleLeftClick);
+
+    this.nameUpdateInterval = window.setInterval(this.updateClockName, 3300);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('contextmenu', this.handleRightClick);
+    document.removeEventListener('click', this.handleLeftClick);
+
+    if (this.nameUpdateInterval !== undefined) {
+      window.clearInterval(this.nameUpdateInterval);
+    }
+  }
+
+  static getRandomName() {
+    const randomNumber = Math.floor(Math.random() * 10000);
+
+    return `Clock-${randomNumber}`;
+  }
+
+  updateClockName = () => {
+    this.setState({
+      clockName: App.getRandomName(),
+    });
+  };
+
+  handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  handleLeftClick = () => {
+    this.setState({ hasClock: true });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.hasClock && <Clock name={this.state.clockName} />}
+      </div>
+    );
+  }
+}
---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+interface Props {
+  name: string;
+}
+
+interface State {
+  currentTime: string;
+}
+
+export class Clock extends React.Component<Props, State> {
+  timerID: number | undefined;
+
+  state = {
+    currentTime: Clock.getCurrentTime(),
+  };
+
+  static getDerivedStateFromProps(
+    nextProps: Props,
+    prevState: State,
+  ) {
+    if (nextProps.name !== prevState.currentTime.split(' ')[0]) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevState.currentTime.split(' ')[0]} to ${nextProps.name}`);
+
+      return {
+        time: `${nextProps.name} ${Clock.getCurrentTime()}`,
+      };
+    }
+
+    return null;
+  }
+
+  componentDidMount() {
+    this.timerID = window.setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerID !== undefined) {
+      window.clearInterval(this.timerID);
+    }
+  }
+
+  static getCurrentTime(): string {
+    return new Date().toUTCString().slice(-12, -4);
+  }
+
+  tick() {
+    this.setState({
+      currentTime: Clock.getCurrentTime(),
+    });
+    // eslint-disable-next-line no-console
+    console.info(this.state.currentTime);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { currentTime } = this.state;
+
+    return (
+      <div>
+        <h1>React clock</h1>
+        <div className="Clock">
+          <strong className="Clock__name">{name}</strong>
+          {' time is '}
+          <span className="Clock__time">{currentTime}</span>
+        </div>
+      </div>
+    );
+  }
+}
