9d1d91a3acab1b55189ffe6c3a506d4381166fd4
Daryna Pidlutska <daryna.pidlutska@gmail.com>
2023-09-13 15:48:23 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,64 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
-function getRandomName(): string {
+const CLOCK_NAME_CHANGE_INTERVAL = 3300;
+
+type State = {
+  clockName: string;
+  hasClock: boolean;
+};
+
+const getRandomName = () => {
   const value = Date.now().toString().slice(-4);
 
   return `Clock-${value}`;
-}
-
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
 };
+
+export class App extends React.PureComponent<{}, State> {
+  state: State = {
+    clockName: 'Clock-0',
+    hasClock: true,
+  };
+
+  clockNameChangeTimerId = 0;
+
+  componentDidMount(): void {
+    this.clockNameChangeTimerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, CLOCK_NAME_CHANGE_INTERVAL);
+
+    document.addEventListener('contextmenu', this.disableClockVisibility);
+    document.addEventListener('click', this.ableClockVisibility);
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.clockNameChangeTimerId);
+    document.removeEventListener('contextmenu', this.disableClockVisibility);
+    document.removeEventListener('click', this.ableClockVisibility);
+  }
+
+  ableClockVisibility = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: true });
+  };
+
+  disableClockVisibility = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  render() {
+    const { clockName, hasClock } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        { hasClock && (
+          <Clock name={clockName} />
+        )}
+      </div>
+    );
+  }
+}
