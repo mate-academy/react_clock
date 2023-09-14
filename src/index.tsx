d28522cba325ab93b4fc3258ac34e9226ed37de3
Maksym Shevelin <shevmax01@gmail.com>
2023-09-14 22:03:25 +0300

    done with Class Component and Functional Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom';
+// import { AppFC } from './AppFC';
 import { App } from './App';
 
 ReactDOM.render(
-  <App />,
+  <>
+    <App />
+    {/* <AppFC /> */}
+  </>,
+
   document.getElementById('root'),
 );
