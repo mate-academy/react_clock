e90973278b25513b169449c90a1cc7d3adbad87f
Denis Chakhalian <my.chahalyan@gmail.com>
2023-09-18 13:24:23 +0300

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -33,9 +33,9 @@ export class App extends React.Component<Props, State> {
   }
 
   componentDidUpdate(_prevProps: Readonly<Props>, prevState: Readonly<State>) {
-    const { clockName } = this.state;
+    const { clockName, hasClock } = this.state;
 
-    if (prevState.clockName !== clockName && clockName) {
+    if (prevState.clockName !== clockName && hasClock) {
       // eslint-disable-next-line no-console
       console.debug(`Renamed from ${prevState.clockName} to ${clockName}`);
     }
