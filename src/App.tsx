408ae07c28d2007cd02cada12b21d11672572ca5
Denys <kolbasin1990@gmail.com>
2023-10-15 12:13:13 +0300

    react_clock_v-04

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import './App.scss';
 import { Clock } from './Class';
 
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+  return `Clock-${value}`;
+}
+
+type State = {
+  hasClock: boolean;
+  clockName: string;
+};
+
 export class App extends React.Component {
-  state = {
+  state: State = {
     hasClock: true,
     clockName: 'Clock-0',
   };
 
+  timerId = 0;
+
   componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
     document.addEventListener('contextmenu', this.handleDocumentRightClick);
     document.addEventListener('click', this.handleDocumentLeftClick);
   }
 
   componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
     document.removeEventListener('contextmenu', this.handleDocumentRightClick);
     document.removeEventListener('click', this.handleDocumentLeftClick);
   }
--- src/Class.tsx
+++ src/Class.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
-
-  return `Clock-${value}`;
-}
-
 type Props = {
   name: string;
 };
 
 type State = {
-  clockName: string;
   today: string;
 };
 
 export class Clock extends React.PureComponent<Props, State> {
   state: State = {
-    clockName: 'Clock-0',
     today: new Date().toUTCString().slice(-12, -4),
   };
 
-  timerId1 = 0;
-
-  timerId2 = 0;
+  timerId = 0;
 
   componentDidMount(): void {
-    this.timerId1 = window.setInterval(() => {
-      this.setState({ clockName: getRandomName() });
-    }, 3300);
-
-    this.timerId2 = window.setInterval(() => {
+    this.timerId = window.setInterval(() => {
       this.setState({ today: new Date().toUTCString().slice(-12, -4) });
       // eslint-disable-next-line no-console
       console.info(this.state.today);
@@ -39,31 +25,24 @@ export class Clock extends React.PureComponent<Props, State> {
 
   componentDidUpdate(
     prevProps: Readonly<Props>,
-    prevState: Readonly<State>,
   ): void {
     if (prevProps.name !== this.props.name) {
       // eslint-disable-next-line no-console
       console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
     }
-
-    if (prevState.clockName !== this.state.clockName) {
-      // eslint-disable-next-line no-console
-      console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
-    }
   }
 
   componentWillUnmount(): void {
-    window.clearInterval(this.timerId1);
-    window.clearInterval(this.timerId2);
+    window.clearInterval(this.timerId);
   }
 
   render() {
-    const { clockName, today } = this.state;
+    const { today } = this.state;
 
     return (
       <div className="Clock">
         <strong className="Clock__name">
-          {clockName}
+          {this.props.name}
         </strong>
 
         {' time is '}
