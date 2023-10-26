d15714077da15bb765975ad8eb87e60fa3fc961c
Denys <denzzlle171@gmail.com>
2023-10-27 01:41:38 +0300

    solution2

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -44,10 +44,10 @@ export class Clock extends Component<ClockProps, ClockState> {
         <strong className="Clock__name">
           {this.props.name}
         </strong>
+        {' time is '}
         <span className="Clock__time">
-          {' time is '}
+          {this.state.time}
         </span>
-        {this.state.time}
       </div>
     );
   }
