9e76568327ced664aa982c22d46e1c3c45379387
Anton Bilous <toxanteloxran@gmail.com>
2023-10-27 15:21:43 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { Clock } from './Clock';
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
-
-  return `Clock-${value}`;
-}
-
 export const App: React.FC = () => {
-  const [clockName, setClockName] = useState('Clock-0');
-  const [today, setToday] = useState(new Date());
   const [isHidden, setIsHidden] = useState(true);
-
-  useEffect(() => {
-    const timerId = window.setInterval(() => {
-      setToday(new Date());
-
-      // eslint-disable-next-line no-console
-      console.info(new Date().toUTCString().slice(-12, -4));
-    }, 1000);
-
-    return () => {
-      window.clearInterval(timerId);
-    };
-  }, [today]);
-
-  useEffect(() => {
-    const timerId = window.setInterval(() => {
-      setClockName(getRandomName());
-
-      // eslint-disable-next-line no-console
-      console.info(getRandomName());
-    }, 3300);
-
-    return () => {
-      window.clearInterval(timerId);
-    };
-  }, [clockName]);
 
   document.addEventListener('contextmenu', (event: MouseEvent) => {
     event.preventDefault();
@@ -54,7 +20,7 @@ export const App: React.FC = () => {
       <h1>React clock</h1>
 
       {isHidden && (
-        <Clock today={today} clockName={clockName} />
+        <Clock />
       )}
     </div>
   );
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,9 +1,41 @@
-type Props = {
-  today: Date;
-  clockName: string;
-};
+import { useState, useEffect } from 'react';
 
-export const Clock: React.FC<Props> = ({ today, clockName }) => {
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
+
+export const Clock: React.FC = () => {
+  const [clockName, setClockName] = useState('Clock-0');
+  const [today, setToday] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      setToday(new Date());
+
+      // eslint-disable-next-line no-console
+      console.info(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+
+    return () => {
+      window.clearInterval(timerId);
+    };
+  }, [today]);
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      setClockName(getRandomName());
+
+      // eslint-disable-next-line no-console
+      console.info(getRandomName());
+    }, 3300);
+
+    return () => {
+      window.clearInterval(timerId);
+    };
+  }, [clockName]);
+
   return (
     <div className="Clock">
       <strong className="Clock__name">
