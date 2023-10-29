1190c20b0bd86764e9fe7606329148b4ac250253
Artem Trofymovych <navrotskymetra@outlook.com>
2023-10-29 14:29:46 +0100

    clock implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 function getRandomName(): string {
@@ -8,32 +8,74 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+  const [time, setTime] = useState(new Date());
+  const [hasClock, setHasClock] = useState(true);
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  document.addEventListener('contextmenu', (event: MouseEvent) => {
+    event.preventDefault();
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+    setHasClock(false);
+  });
+
+  document.addEventListener('click', () => {
+    setHasClock(true);
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newTime = new Date();
+
+      if (newTime !== time) {
+        if (hasClock) {
+          // eslint-disable-next-line no-console
+          console.info(time.toUTCString().slice(-12, -4));
+        }
+
+        setTime(newTime);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [time]);
+
+  const [clockName, setClockName] = useState('Clock-0');
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      const newName = getRandomName();
+
+      if (newName !== clockName) {
+        setClockName(getRandomName());
+        // eslint-disable-next-line no-console
+        console.debug(`Renamed from ${clockName} to ${newName}`);
+      }
+    }, 3300);
+
+    return () => {
+      window.clearInterval(timerId);
+    };
+  }, [clockName]);
 
   return (
     <div className="App">
       <h1>React clock</h1>
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
+      {hasClock
+      && (
+        <div className="Clock">
+          <strong className="Clock__name">
+            {clockName}
+          </strong>
 
-        {' time is '}
+          {' time is '}
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
+          <span className="Clock__time">
+            {time.toUTCString().slice(-12, -4)}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
