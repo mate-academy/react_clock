7bcdc8d4968cc75ccee50f0997ff87518c5e9de4
Bahtijar <no4kar@gmail.com>
2023-11-12 15:20:50 +0100

    stage-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -10,61 +11,29 @@ function getRandomName(): string {
 type Props = {};
 
 type State = {
-  today: Date,
   clockName: string,
   hasClock: boolean,
 };
 
 export class App extends React.PureComponent<Props, State> {
   state = {
-    today: new Date(),
     clockName: 'Clock-0',
     hasClock: true,
   };
 
   timerId = 0;
 
-  timerIdToday = 0;
-
   componentDidMount(): void {
-    // This code starts a timer
     this.timerId = window.setInterval(() => {
       this.setState({ clockName: getRandomName() });
     }, 3300);
-    this.timerIdToday = window.setInterval(() => {
-      this.setState({ today: new Date() });
-    }, 1000);
 
     document.addEventListener('click', this.handleClockShow);
     document.addEventListener('contextmenu', this.handleClockHide);
   }
 
-  componentDidUpdate(
-    prevProps: Readonly<Props>,
-    prevState: Readonly<State>,
-    // snapshot?: any,
-  ): void {
-    // eslint-disable-next-line no-console
-    console.info(this.state.today.toUTCString().slice(-12, -4));
-
-    if (prevState.clockName !== this.state.clockName) {
-      // eslint-disable-next-line no-console
-      console.debug(
-        `Renamed from ${prevState.clockName} to ${this.state.clockName}`
-      );
-    }
-
-    if (prevProps === undefined) {
-      // eslint-disable-next-line no-console
-      console.info();
-    }
-  }
-
   componentWillUnmount(): void {
-    // this code stops the timer
     window.clearInterval(this.timerId);
-    window.clearInterval(this.timerIdToday);
-
     document.removeEventListener('click', this.handleClockShow);
     document.removeEventListener('contextmenu', this.handleClockHide);
   }
@@ -85,19 +54,7 @@ export class App extends React.PureComponent<Props, State> {
         <h1>React clock</h1>
 
         {this.state.hasClock
-          && (
-            <div className="Clock">
-              <strong className="Clock__name">
-                {this.state.clockName}
-              </strong>
-
-              {' time is '}
-
-              <span className="Clock__time">
-                {this.state.today.toUTCString().slice(-12, -4)}
-              </span>
-            </div>
-          )}
+          && (<Clock name={this.state.clockName} />)}
       </div>
     );
   }
