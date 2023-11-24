fc45e8a9dcd1641b850918997a89a9835f5a7cec
Vladyslav Korobka <vladseipa@gmail.com>
2023-11-24 14:18:07 +0200

    task has been done

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-const initialState = {
-  clockName: 'Clock-0',
-  hasClock: true,
-};
-
 ReactDOM.render(
-  <App {...initialState} />,
+  <App clockName="Clock-0" hasClock />,
   document.getElementById('root'),
 );
