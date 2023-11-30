61c10c9fec1bffa12cfd4f691e7c72d436b9aa26
khviktoriia <khomenkoo.viktoriia@gmail.com>
2023-11-30 23:29:57 +0200

    Solution

---
+++ src/Clock.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+type Props = {
+  name: string,
+};
+
+export class Clock extends React.Component<Props> {
+  state = {
+    date: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ date: new Date() });
+      // eslint-disable-next-line no-console
+      console.info(this.state.date.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { name } = this.props;
+
+    if (prevProps.name !== name) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.name} to ${name}`);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {date.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
