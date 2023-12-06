4d64d1e16b1217db7d72f7372eadf338adf14d82
Artem Velychko <avelychko@icloud.com>
2023-12-06 18:05:49 -0500

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,25 @@ import React from 'react';
 import './App.scss';
 import { Clock } from './components/Clock';
 
+type Props = {};
+type State = {
+  clockName: string;
+  hasClock: boolean;
+};
+
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
 
   return `Clock-${value}`;
 }
 
-type Props = {};
-type State = {
-  today: Date;
-  clockName: string;
-  hasClock: boolean;
-};
-
 export class App extends React.Component<Props, State> {
   state = {
-    today: new Date(),
     clockName: 'Clock-0',
     hasClock: true,
   };
 
   componentDidMount() {
-    window.setInterval(this.timeId, 1000);
     window.setInterval(this.nameId, 3300);
 
     document.addEventListener('contextmenu', this.hideClock);
@@ -52,35 +49,19 @@ export class App extends React.Component<Props, State> {
     this.setState({ hasClock: true });
   };
 
-  timeId = () => {
-    if (this.state.hasClock) {
-      this.setState({ today: new Date() });
-      // eslint-disable-next-line no-console
-      console.info(this.state.today.toUTCString().slice(-12, -4));
-    }
-  };
-
   nameId = () => {
     this.setState({ clockName: getRandomName() });
   };
 
   render() {
-    const { today, clockName, hasClock } = this.state;
+    const { clockName, hasClock } = this.state;
 
     return (
       <div className="App">
         <h1>React clock</h1>
 
         {hasClock && (
-          <div className="Clock">
-            <Clock name={clockName} />
-
-            {' time is '}
-
-            <span className="Clock__time">
-              {today.toUTCString().slice(-12, -4)}
-            </span>
-          </div>
+          <Clock name={clockName} hasClock={hasClock} />
         )}
       </div>
     );
