d64f4b4548a63eeef5f4b48f7512aa18d9a97dc7
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2023-12-10 18:31:51 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,7 +8,7 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
+/* export const App: React.FC = () => {
   const today = new Date();
   let clockName = 'Clock-0';
 
@@ -34,6 +35,51 @@ export const App: React.FC = () => {
           {today.toUTCString().slice(-12, -4)}
         </span>
       </div>
+
+      <Clock />
     </div>
   );
+}; */
+
+type State = {
+  hasClock: boolean,
+  clockName: string,
 };
+
+export class App extends React.Component {
+  state = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
+
+  nameTimerId = window.setInterval(() => {
+    this.setState({ clockName: getRandomName() });
+  }, 3300);
+
+  componentDidUpdate(_: {}, prevState: Readonly<State>): void {
+    if (this.state.hasClock && this.state.clockName !== prevState.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
+    }
+  }
+
+  render() {
+    document.addEventListener('contextmenu', (event: MouseEvent) => {
+      event.preventDefault();
+
+      this.setState({ hasClock: false });
+    });
+
+    document.addEventListener('click', () => {
+      this.setState({ hasClock: true });
+    });
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock && <Clock name={this.state.clockName} />}
+      </div>
+    );
+  }
+}
