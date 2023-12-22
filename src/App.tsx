3eff14c3ef26feaf388d4ca05acac2a8820f3c40
Dominika Karmel <dkarmel@op.pl>
2023-12-22 11:35:55 +0100

    fix console.logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
-import Clock from './components/Clock';
+import { Clock } from './components/Clock';
 
-interface AppState {
-  hasClock: boolean;
+type State = {
+  clockName: string,
+  hasClock: boolean,
+};
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
 }
 
-export class App extends Component<{}, AppState> {
-  state: AppState = {
+export class App extends React.Component<{}, State> {
+  state = {
     hasClock: true,
+    clockName: 'Clock-0',
   };
 
-  handleContextMenu = (event: React.MouseEvent) => {
-    event.preventDefault();
-    this.setState({ hasClock: false });
-  };
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        clockName: getRandomName(),
+      });
+    }, 3300);
 
-  handleClick = () => {
-    this.setState({ hasClock: true });
-  };
+    document.addEventListener('contextmenu', (event: MouseEvent) => {
+      event.preventDefault();
+      this.setState({
+        hasClock: false,
+      });
+    });
+
+    document.addEventListener('click', () => {
+      this.setState({
+        hasClock: true,
+      });
+    });
+  }
 
   render() {
     return (
-      <div
-        className="App"
-        role="presentation"
-        onContextMenu={this.handleContextMenu}
-        onClick={this.handleClick}
-      >
+      <div className="App">
         <h1>React clock</h1>
-        {this.state.hasClock && <Clock onUpdateTime={() => {}} />}
+
+        {this.state.hasClock && <Clock clockName={this.state.clockName} />}
       </div>
     );
   }
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,44 +1,64 @@
-// Clock.tsx
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-interface ClockProps {
-  onUpdateTime: () => void;
-}
-
-const Clock: React.FC<ClockProps> = ({ onUpdateTime }) => {
-  const [clockName, setClockName] = useState<string>('Clock-0');
-  const [currentTime, setCurrentTime] = useState<string>(
-    new Date().toUTCString().slice(-12, -4),
-  );
-
-  useEffect(() => {
-    const timerId = window.setInterval(() => {
-      onUpdateTime();
-      setCurrentTime(new Date().toUTCString().slice(-12, -4));
-    }, 1000);
-
-    return () => {
-      window.clearInterval(timerId);
-    };
-  }, [onUpdateTime]);
-
-  useEffect(() => {
-    const nameUpdateTimerId = window.setInterval(() => {
-      setClockName(`Clock-${Date.now().toString().slice(-4)}`);
-    }, 3300);
-
-    return () => {
-      window.clearInterval(nameUpdateTimerId);
-    };
-  }, []);
-
-  return (
-    <div className="Clock" role="presentation">
-      <strong className="Clock__name">{clockName}</strong>
-      {' time is '}
-      <span className="Clock__time">{currentTime}</span>
-    </div>
-  );
+type Props = {
+  clockName: string;
 };
 
-export default Clock;
+type State = {
+  time: string;
+  clockId: number | undefined;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    time: new Date().toUTCString().slice(-12, -4),
+    clockId: undefined,
+  };
+
+  componentDidMount(): void {
+    const newClockId = window.setInterval(() => {
+      const date = new Date().toUTCString().slice(-12, -4);
+
+      this.setState({
+        time: date,
+      });
+
+      // eslint-disable-next-line no-console
+      console.info(date);
+    }, 1000);
+
+    this.setState({
+      clockId: newClockId,
+    });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const oldName = prevProps.clockName;
+    const newName = this.props.clockName;
+
+    if (newName !== oldName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${oldName} to ${newName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.state.clockId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {this.props.clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.time}
+        </span>
+      </div>
+    );
+  }
+}
