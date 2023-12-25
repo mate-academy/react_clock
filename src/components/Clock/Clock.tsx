8a1d3cd2214cc2664943d1ec937bf4b6fe8a50a8
Vladyslav Starodub <starodubvlad08@gmail.com>
2023-12-25 16:47:21 +0200

    add clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+interface Props {
+  name: string
+}
+
+export class Clock extends React.PureComponent<Props> {
+  state = {
+    today: new Date(),
+  };
+
+  timerId = 0;
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState((currentState) => ({
+        ...currentState,
+        today: new Date(),
+      }));
+
+      window.console.info(this.state.today);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.name !== this.props.name) {
+      window.console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.name}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
