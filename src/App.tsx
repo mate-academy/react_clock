58d1c15575055147f0a92146d604f243342f55ad
Hovera-Liubomyr <ljusuk@gmail.com>
2024-01-11 15:43:24 +0200

    react_clock-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import './App.scss';
-import { Clock } from './components/Clock';
+import { Clock } from './components/Clock/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
