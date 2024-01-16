8c1ed18a826ccc2ec998992d8b9d9f59574540ca
Oksana Maruniak <syper.ksenka@gmail.com>
2024-01-16 20:53:39 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -20,41 +20,25 @@ export class App extends React.PureComponent<{}, State> {
     this.timerId = window.setInterval(() => {
       this.setState({ clockName: getRandomName() });
     }, 3300);
-  }
-
-  componentDidUpdate(
-    _prevProps: Readonly<{}>,
-    prevState: Readonly<State>,
-  ): void {
-    if (prevState.clockName !== this.state.clockName) {
-      // eslint-disable-next-line no-console
-      console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
-    }
+    document.addEventListener('contextmenu', this.handleRightClick);
+    document.addEventListener('click', this.handleLeftClick);
   }
 
   componentWillUnmount(): void {
     window.clearInterval(this.timerId);
+    document.removeEventListener('contextmenu', this.handleRightClick);
+    document.removeEventListener('click', this.handleLeftClick);
   }
 
-  handleRightClick = (event: React.MouseEvent) => {
+  handleRightClick = (event: MouseEvent) => {
     event.preventDefault();
     this.setState({ hasClock: false });
-    window.clearInterval(this.timerId);
+    // window.clearInterval(this.timerId);
   };
 
-  handleLeftClick = (event: React.MouseEvent) => {
+  handleLeftClick = (event: MouseEvent) => {
     event.preventDefault();
     this.setState({ hasClock: true });
-  };
-
-  handleKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      this.setState({ hasClock: true });
-    }
-
-    if (event.key === ' ') {
-      this.setState({ hasClock: false });
-    }
   };
 
   render() {
@@ -63,26 +47,11 @@ export class App extends React.PureComponent<{}, State> {
     return (
       <div
         className="App"
-        onContextMenu={this.handleRightClick}
-        onClick={this.handleLeftClick}
-        onKeyDown={this.handleKeyDown}
-        role="button"
-        tabIndex={0}
       >
         <h1>React clock</h1>
 
-        {hasClock ? (
-          <div className="Clock">
-            <strong className="Clock__name">
-              {clockName}
-            </strong>
-
-            {' time is '}
-
-            <Clock />
-          </div>
-        ) : (
-          null
+        {hasClock && (
+          <Clock clockName={clockName} />
         )}
       </div>
     );
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -5,7 +5,11 @@ type State = {
   today: string,
 };
 
-export class Clock extends React.PureComponent<{}, State> {
+type Props = {
+  clockName: string,
+};
+
+export class Clock extends React.PureComponent<Props> {
   state: State = {
     today: getDate(),
   };
@@ -20,17 +24,36 @@ export class Clock extends React.PureComponent<{}, State> {
     }, 1000);
   }
 
+  componentDidUpdate(
+    prevProps: Props,
+    // prevState: Readonly<State>,
+  ): void {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevProps.clockName} to ${this.props.clockName}`);
+    }
+  }
+
   componentWillUnmount(): void {
     window.clearInterval(this.timerId);
   }
 
   render() {
     const { today } = this.state;
+    const { clockName } = this.props;
 
     return (
-      <span className="Clock__time">
-        {today}
-      </span>
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today}
+        </span>
+      </div>
     );
   }
 }
