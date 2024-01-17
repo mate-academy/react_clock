f457cddd2c3c0a5fab9882e1aa428197d1964a99
Yurii Hlushenko <yauglushenko@gmail.com>
2024-01-17 22:11:05 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
+import { Clock } from './components/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -8,32 +8,37 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+  const [hasClock, setHasClock] = useState(true);
+  const [isfirstMount, setIsfirstMount] = useState(true);
+  const [clockName, setClockName] = useState(
+    isfirstMount ? 'Clock-0' : getRandomName(),
+  );
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  document.addEventListener('contextmenu', (event: MouseEvent) => {
+    event.preventDefault();
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+    setHasClock(false);
+  });
+
+  document.addEventListener('click', () => {
+    setHasClock(true);
+  });
+
+  const saveMount = (m: boolean) => setIsfirstMount(m);
+  const saveClockName = (name: string) => setClockName(name);
 
   return (
     <div className="App">
       <h1>React clock</h1>
 
-      <div className="Clock">
-        <strong className="Clock__name">
-          {clockName}
-        </strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
+      {hasClock && (
+        <Clock
+          isfirstMount={isfirstMount}
+          saveMount={saveMount}
+          clockName={clockName}
+          saveClockName={saveClockName}
+        />
+      )}
     </div>
   );
 };
