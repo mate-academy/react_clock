9a6ea3cbe0bff6b781f2ebd1662222495cbea370
Kriss Kozak <misskrissretro@gmail.com>
2024-01-21 16:07:51 +0200

    clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
+
+type State = {
+  hasClock: boolean;
+  clockName: string;
+};
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,78 +13,36 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-type AppState = {
-  hasClock: boolean;
-  clockName: string;
-  clockMessage: string;
-};
-
-export class App extends Component<{}, AppState> {
-  private timerId: number | undefined;
-
-  state: Readonly<AppState> = {
+export class App extends React.PureComponent<{}, State> {
+  state: State = {
     hasClock: true,
     clockName: 'Clock-0',
-    clockMessage: '',
   };
 
-  componentDidMount() {
-    this.timerId = window.setInterval(() => {
-      const newClockName = getRandomName();
-      const newClockMessage = '';
+  clockId = 0;
 
-      this.setState({ clockName: newClockName, clockMessage: newClockMessage });
+  componentDidMount(): void {
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+      this.setState({ hasClock: false });
+    });
 
-      if (this.state.hasClock && new Date().getSeconds() % 3 === 0) {
-        // eslint-disable-next-line no-console
-        console.info(`The time is ${newClockName}`);
-      }
-    }, 1000);
+    document.addEventListener('click', () => {
+      this.setState({ hasClock: true });
+    });
+
+    this.clockId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
   }
-
-  componentWillUnmount() {
-    if (this.timerId) {
-      clearInterval(this.timerId);
-    }
-  }
-
-  handleContextMenu = (event: React.MouseEvent) => {
-    event.preventDefault();
-    this.setState({ hasClock: false });
-  };
-
-  handleClick = () => {
-    this.setState({ hasClock: true });
-  };
 
   render() {
-    const { hasClock, clockName, clockMessage } = this.state;
-
     return (
-      <div
-        className="App"
-        role="button"
-        tabIndex={0}
-        onContextMenu={this.handleContextMenu}
-        onClick={this.handleClick}
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            this.handleClick();
-          }
-        }}
-      >
+      <div className="App">
         <h1>React clock</h1>
-        {hasClock && (
-          <div className="Clock">
-            <strong className="Clock__name">{clockName}</strong>
-            {' time is '}
-            <span className="Clock__time">
-              {new Date().toUTCString().slice(-12, -4)}
-            </span>
-            {' - '}
-            <span className="Clock__message">{clockMessage}</span>
-          </div>
-        )}
+        {this.state.hasClock
+        && <Clock name={this.state.clockName} />}
+
       </div>
     );
   }
