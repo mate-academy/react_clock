c9b908ad6d8a1bf912a209aea93b96945bc07abd
Rostyslav Tkachuk <pro100kor.moon@gmail.com>
2024-01-22 22:47:52 +0200

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,52 @@
-import React from 'react';
+import { PureComponent } from 'react';
 import './App.scss';
-import { Clock } from './Clock/Clock';
+import { Clock } from './components/Clock/Clock';
 
-type Props = {};
+interface State {
+  hasClock: boolean;
+  clockName: string;
+}
 
-type State = {
-  hasClock: boolean
-};
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
 
-export class App extends React.Component<Props, State> {
-  state = {
-    hasClock: false,
+  return `Clock-${value}`;
+}
+
+export class App extends PureComponent {
+  state: State = {
+    hasClock: true,
+    clockName: 'Clock-0',
   };
 
+  timerId = 0;
+
   componentDidMount(): void {
-    document.addEventListener('mousedown', () => {
-      this.setState({ hasClock: true });
-    });
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
 
-    document.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      this.setState({ hasClock: false });
-    });
+    document.addEventListener('contextmenu', this.handleContextMenu);
+    document.addEventListener('click', this.handleClick);
   }
 
-  componentWillUnmount(): void {
-    document.removeEventListener('mousedown', () => {
-      this.setState({ hasClock: true });
-    });
+  handleContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
 
-    document.removeEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      this.setState({ hasClock: false });
-    });
-  }
+  handleClick = () => {
+    this.setState({ hasClock: true });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        {this.state.hasClock ? (
-          <Clock />
-        ) : ('')}
+        {this.state.hasClock && (
+          <Clock clockName={this.state.clockName} />
+        )}
       </div>
     );
   }
