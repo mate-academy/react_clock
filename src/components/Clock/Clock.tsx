33833bd0ea9d1d7be18898779c11922708a29da1
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-02-07 19:43:59 +0200

    clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends React.Component<Props> {
+  state = {
+    date: new Date(),
+  };
+
+  intervalId = 0;
+
+  componentDidMount(): void {
+    this.setState({ date: new Date() });
+
+    this.intervalId = window.setInterval(() => {
+      const newDate = new Date();
+
+      this.setState({ date: newDate });
+      // eslint-disable-next-line no-console
+      console.info(this.getTimeFromDate(newDate));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    const oldName = prevProps.name;
+    const currentName = this.props.name;
+
+    if (currentName !== oldName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${oldName} to ${currentName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.intervalId);
+  }
+
+  getTimeFromDate = (date: Date) => date.toUTCString().slice(-12, -4);
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.getTimeFromDate(date)}
+        </span>
+      </div>
+    );
+  }
+}
