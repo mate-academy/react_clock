0fc974dc02ed4426a4c22055cdd4901d6973a66a
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-02-11 21:05:41 +0100

    destr

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -15,8 +15,7 @@ export class Clock extends React.Component<Props> {
 
   todayId = 0;
 
-  // This code starts a timer
-  componentDidMount(): void {
+  componentDidMount() {
     this.todayId = window.setInterval(() => {
       this.setState({ today: new Date() });
       // eslint-disable-next-line no-console
@@ -24,21 +23,24 @@ export class Clock extends React.Component<Props> {
     }, 1000);
   }
 
-  componentWillUnmount(): void {
+  componentWillUnmount() {
     window.clearInterval(this.todayId);
   }
 
-  render(): React.ReactNode {
+  render() {
+    const { clockName } = this.props;
+    const { today } = this.state;
+
     return (
       <div className="Clock">
         <strong className="Clock__name">
-          {this.props.clockName}
+          {clockName}
         </strong>
 
         {' time is '}
 
         <span className="Clock__time">
-          {this.state.today.toUTCString().slice(-12, -4)}
+          {today.toUTCString().slice(-12, -4)}
         </span>
       </div>
     );
