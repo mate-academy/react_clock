60ae90b4ea0ec2596dca6d444590f2871bd8857e
Roman Sylman <woody.falcone.00@gmail.com>
2024-02-22 21:18:59 +0200

    develop

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from './App';
+import React from 'react';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
