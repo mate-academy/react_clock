3eaa9f58e6a1dfdc40c5176c3fd09e0d6aa6e05e
vova <hohiop09@gmail.com>
2024-02-23 11:05:43 +0200

    save

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -7,6 +7,20 @@ type Props = {
 };
 
 export class Clock extends PureComponent<Props> {
+  handleClock = window.setInterval(() => {
+    this.setState(curState => ({
+      ...curState,
+      // eslint-disable-next-line react/no-unused-state
+      today: new Date(),
+    }));
+    // eslint-disable-next-line no-console
+    console.log(this.props.today.toUTCString().slice(-12, -4));
+  }, 1000);
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.handleClock);
+  }
+
   render() {
     const { hasClock, clockName, today } = this.props;
 
