5575cb06803c59691587696b0bf019b02d33f89f
Roman Sylman <woody.falcone.00@gmail.com>
2024-02-23 17:09:06 +0200

    develop

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('app');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(<App />);
