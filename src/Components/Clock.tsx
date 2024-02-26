fc214bc8940b432a377c1babc28ff01a1414c87a
alona <alionazhv@gmail.com>
2024-02-26 11:43:12 +0100

    The solution to the assignment was implemented

---
+++ src/Components/Clock.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+export class Clock extends React.Component<Props> {
+  state = {
+    date: new Date(),
+  };
+
+  intervalValue = 0;
+
+  componentDidMount(): void {
+    this.intervalValue = window.setInterval(() => {
+      this.setState({ date: new Date() });
+
+      // eslint-disable-next-line no-console
+      console.info(this.getTimeFromDate(new Date()));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    const oldName = prevProps.name;
+    const currentName = this.props.name;
+
+    if (currentName !== oldName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${oldName} to ${currentName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.intervalValue);
+  }
+
+  getTimeFromDate = (date: Date) => {
+    return date.toUTCString().slice(-12, -4);
+  };
+
+  render() {
+    const { name } = this.props;
+    const { date } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{name}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.getTimeFromDate(date)}</span>
+      </div>
+    );
+  }
+}
