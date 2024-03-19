333a5ea0f0ed25067464eaa6acadeb89c894eeb2
Bohdan Ivankov <bodanich600@gmail.com>
2024-03-19 14:10:53 +0000

    first repo

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,87 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  clockName: string;
+  today: Date;
+  hasClock: boolean;
 };
+
+export class App extends React.Component<State> {
+  state = {
+    clockName: 'Clock-0',
+    today: new Date(),
+    hasClock: false,
+  };
+
+  handleRightClick = document.addEventListener(
+    'contextmenu',
+    (event: MouseEvent) => {
+      event.preventDefault();
+      this.setState({ hasClock: false });
+    },
+  );
+
+  handleLeftClick = document.addEventListener('click', (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: true, today: new Date() });
+  });
+
+  timerId = 0;
+
+  componentDidMount() {
+    // This code starts a timer
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+
+    window.setInterval(() => {
+      // const currentTime = new Date();
+      if (this.state.hasClock) {
+        // eslint-disable-next-line no-console
+        console.log(new Date().toUTCString().slice(-12, -4));
+      }
+
+      this.setState({ today: new Date() });
+    }, 1000);
+
+    this.setState({ hasClock: true });
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<{}>,
+    prevState: Readonly<State>,
+  ): void {
+    if (prevState.clockName !== this.state.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(
+        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    // this code stops the timer
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">{this.state.clockName}</strong>
+
+            {' time is '}
+
+            <span className="Clock__time">
+              {this.state.today.toUTCString().slice(-12, -4)}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
