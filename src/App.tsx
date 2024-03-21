e95f8fa75fe6a9f8a154910aaf8a2cee285a6614
Denis Kotov <dionisiy2904@gmail.com>
2024-03-21 17:34:27 +0200

    import React for index.tsx, add {} for app class

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ type State = {
   clockName: string;
 };
 
-export class App extends React.Component<State> {
+export class App extends React.Component<{}, State> {
   state: State = {
     hasClock: true,
     clockName: 'Clock-0',
