c6e733216d7a310aa05cc32f79473767d3953faa
volodymyr <volodiayurovych@gmail.com>
2024-03-21 19:10:09 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,14 @@ function getRandomName(): string {
 }
 
 type State = {
-  today: Date;
   clockName: string;
   hasClock: boolean;
 };
 
 export class App extends React.Component<{}, State> {
-  clockTimerId = 0;
-
   randomTimerId = 0;
 
   state: State = {
-    today: new Date(),
     clockName: 'Clock-0',
     hasClock: true,
   };
@@ -28,58 +24,35 @@ export class App extends React.Component<{}, State> {
   componentDidMount() {
     document.addEventListener('contextmenu', this.handleRightButton);
     document.addEventListener('click', this.handleLeftButton);
-    this.clockTimerId = window.setInterval(() => {
-      this.setState({ today: new Date() });
-    }, 1000);
 
     this.randomTimerId = window.setInterval(() => {
       this.setState({ clockName: getRandomName() });
     }, 3300);
   }
 
-  componentDidUpdate(_: string, prevState: State) {
-    if (this.state.today !== prevState.today) {
-      // eslint-disable-next-line no-console
-      console.log(
-        `${String(this.state.today.getHours()).padStart(2, '0')}:${String(this.state.today.getMinutes()).padStart(2, '0')}:${String(this.state.today.getSeconds()).padStart(2, '0')}`,
-      );
-    }
-
-    if (this.state.clockName !== prevState.clockName) {
-      // eslint-disable-next-line no-console
-      console.debug(
-        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
-      );
-    }
-  }
-
   componentWillUnmount() {
-    window.clearInterval(this.clockTimerId);
     window.clearInterval(this.randomTimerId);
   }
 
   handleRightButton = (event: MouseEvent) => {
     event.preventDefault();
     this.setState({ hasClock: false });
-    this.componentWillUnmount();
+    // this.componentWillUnmount();
   };
 
   handleLeftButton = () => {
     this.setState({ hasClock: true });
-    if (!this.state.hasClock) {
-      this.componentDidMount();
-    }
+
+    // if (!this.state.hasClock) {
+    //   this.componentDidMount();
+    // }
   };
 
   render() {
     return (
       <div className="App">
         <h1>React clock</h1>
-        <Clock
-          hasClock={this.state.hasClock}
-          clockName={this.state.clockName}
-          today={this.state.today}
-        />
+        {this.state.hasClock && <Clock clockName={this.state.clockName} />}
       </div>
     );
   }
--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
 
 type Props = {
-  today: Date;
   clockName: string;
-  hasClock: boolean;
 };
 
-export const Clock: React.FC<Props> = ({ today, clockName, hasClock }) => {
-  return (
-    <>
-      {hasClock && (
-        <div className="Clock">
-          <strong className="Clock__name">{clockName}</strong>
-
-          {' time is '}
-
-          <span className="Clock__time">
-            {today.toUTCString().slice(-12, -4)}
-          </span>
-        </div>
-      )}
-    </>
-  );
+type State = {
+  today: string;
 };
+
+export class Clock extends React.Component<Props, State> {
+  clockTimerId = 0;
+
+  state: State = {
+    today: new Date().toUTCString().slice(-12, -4),
+  };
+
+  componentDidMount() {
+    this.clockTimerId = window.setInterval(() => {
+      const today = new Date().toUTCString().slice(-12, -4);
+
+      // eslint-disable-next-line no-console
+      console.log(today);
+      this.setState({ today });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.clockName !== prevProps.clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(
+        `Renamed from ${prevProps.clockName} to ${this.props.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.clockTimerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.state.today}</span>
+      </div>
+    );
+  }
+}
