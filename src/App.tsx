08f9058a9b783c76c806555293ff0317f1e2f244
Nikita Donskoy <donskoy.nikita13@gmail.com>
2024-03-25 00:58:29 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -52,32 +52,3 @@ export class App extends React.Component {
     );
   }
 }
-
-// export const App: React.FC = () => {
-//   const today = new Date();
-//   let clockName = 'Clock-0';
-
-//   // This code starts a timer
-//   const timerId = window.setInterval(() => {
-//     clockName = getRandomName();
-//   }, 3300);
-
-//   // this code stops the timer
-//   window.clearInterval(timerId);
-
-//   return (
-//     <div className="App">
-//       <h1>React clock</h1>
-
-//       <div className="Clock">
-//         <strong className="Clock__name">{clockName}</strong>
-
-//         {' time is '}
-
-//         <span className="Clock__time">
-//           {today.toUTCString().slice(-12, -4)}
-//         </span>
-//       </div>
-//     </div>
-//   );
-// };
