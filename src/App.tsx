281d074930dff277d4489ec75b732bd3c612a001
Nataliia <koreniuknataliia@gmail.com>
2024-04-12 08:23:03 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import React from 'react';
+import { PureComponent } from 'react';
 import './App.scss';
 import { Clock } from './component/Clock';
+import React from 'react';
+import { Props } from './component/Clock';
 
-interface State {
-  hasClock: boolean;
-  clockName: string;
-}
-
-export class App extends React.Component<{}, State> {
-  state: State = {
+export class App extends PureComponent {
+  state = {
     hasClock: true,
     clockName: 'Clock-0',
   };
 
-  timerValue = 0;
+  handleClockName: undefined | number;
 
   componentDidMount(): void {
-    this.timerValue = window.setInterval(() => {
-      this.setState(() => ({
-        clockName: this.getRandomName(),
-      }));
-    }, 3300);
+    window.addEventListener('contextmenu', this.handleContextMenu);
+    window.addEventListener('click', this.handleClick);
 
-    document.addEventListener('contextmenu', this.hideClock);
-    document.addEventListener('click', this.showClock);
+    this.handleClockName = window.setInterval(() => {
+      this.setState({ clockName: this.getRandomName() });
+    }, 3300);
+  }
+
+  componentDidUpdate(_prevProps: Props, prevState: Props): void {
+    if (this.state.hasClock) {
+      if (prevState.clockName !== this.state.clockName) {
+        // eslint-disable-next-line no-console
+        console.debug(
+          `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
+        );
+      }
+    }
   }
 
   componentWillUnmount(): void {
-    window.clearInterval(this.timerValue);
+    document.removeEventListener('click', this.handleClick);
+    document.removeEventListener('contextmenu', this.handleContextMenu);
+    window.clearInterval(this.handleClockName);
   }
+
+  handleContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  };
+
+  handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+
+    if (event.button === 0) {
+      this.setState({ hasClock: true });
+    }
+  };
 
   getRandomName = (): string => {
     const value = Date.now().toString().slice(-4);
@@ -36,26 +57,12 @@ export class App extends React.Component<{}, State> {
     return `Clock-${value}`;
   };
 
-  hideClock = (event: MouseEvent) => {
-    event.preventDefault();
-
-    this.setState({ hasClock: false });
-  };
-
-  showClock = (event: MouseEvent) => {
-    event.preventDefault();
-
-    this.setState({ hasClock: true });
-  };
-
   render() {
-    const { hasClock, clockName } = this.state;
-
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        {hasClock && <Clock name={clockName} />}
+        {this.state.hasClock && <Clock {...this.state} />}
       </div>
     );
   }
--- src/component/Clock.tsx
+++ src/component/Clock.tsx
@@ -1,60 +1,46 @@
 import React from 'react';
+import { PureComponent } from 'react';
 
-type Props = {
-  name: string;
+export type Props = {
+  hasClock: boolean;
+  clockName: string;
 };
 
-export class Clock extends React.Component<Props> {
+export class Clock extends PureComponent<Props> {
   state = {
-    date: new Date(),
+    today: new Date(),
   };
 
-  intervalValue = 0;
+  handleClock = window.setInterval(() => {
+    const today = new Date();
 
-  componentDidMount(): void {
-    this.setState({ date: new Date() });
+    this.setState({ today: new Date() });
 
-    this.intervalValue = window.setInterval(() => {
-      const newDate = new Date();
-
-      this.setState({ date: newDate });
-
-      // eslint-disable-next-line no-console
-      console.log(this.getTimeFromDate(newDate));
-    }, 1000);
-  }
-
-  componentDidUpdate(prevProps: Props): void {
-    const oldName = prevProps.name;
-    const currentName = this.props.name;
-
-    if (currentName !== oldName) {
-      // eslint-disable-next-line no-console
-      console.debug(`Renamed from ${oldName} to ${currentName}`);
-    }
-  }
+    // eslint-disable-next-line no-console
+    console.log(today.toUTCString().slice(-12, -4));
+  }, 1000);
 
   componentWillUnmount(): void {
-    window.clearInterval(this.intervalValue);
+    window.clearInterval(this.handleClock);
   }
 
-  getTimeFromDate = (date: Date) => {
-    // this code retrun time in not my time zone(in Greenwich)
-    return date.toUTCString().slice(-12, -4);
-  };
-
   render() {
-    const { name } = this.props;
-    const { date } = this.state;
+    const { hasClock, clockName } = this.props;
 
     return (
-      <div className="Clock">
-        <strong className="Clock__name">{name}</strong>
+      <>
+        {hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">{clockName}</strong>
 
-        {' time is '}
+            {' time is '}
 
-        <span className="Clock__time">{this.getTimeFromDate(date)}</span>
-      </div>
+            <span className="Clock__time">
+              {this.state.today.toUTCString().slice(-12, -4)}
+            </span>
+          </div>
+        )}
+      </>
     );
   }
 }
