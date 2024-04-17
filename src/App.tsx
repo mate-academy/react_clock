11f2cafb1e30eb4b43ba65d8c7badda0bef48822
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-04-17 21:46:50 +0300

    set clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './component/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -22,33 +23,17 @@ class App extends React.PureComponent {
         this.setState({ clockName: getRandomName() });
       }, 3300),
     });
-
-    this.setState({
-      timer: setInterval(() => {
-        this.setState({ currentTime: new Date().toString().slice(16, -42) });
-      }, 1000),
-    });
   }
 
   componentWillUnmount() {
     clearInterval(this.state.timerId);
-    clearInterval(this.state.timer);
   }
 
   render() {
-    const { currentTime, clockName } = this.state;
-
-    // eslint-disable-next-line no-console
-    console.log(currentTime);
-
     return (
       <div className="App">
         <h1>React clock</h1>
-        <div className="Clock">
-          <strong className="Clock__name">{clockName}</strong>
-          {' time is '}
-          <span className="Clock__time">{currentTime}</span>
-        </div>
+        <Clock name={this.state.clockName} />
       </div>
     );
   }
