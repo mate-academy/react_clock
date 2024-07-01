8be54ece52b5aeaae9e7e6667ec3fbf3732eac02
Stanislav <stanislavandr@gmail.com>
2024-07-01 22:31:19 +0300

    solutiom

---
+++ src/Clock.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+interface ClockState {
+  currentTime: string;
+}
+
+interface ClockProps {
+  clockName: string;
+}
+
+export default class Clock extends Component<ClockProps> {
+  private timerId: number | undefined = undefined;
+
+  state: ClockState = {
+    currentTime: this.getCurrentTime(),
+  };
+
+  getCurrentTime(): string {
+    return new Date().toUTCString().slice(-12, -4);
+  }
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.tick();
+      // eslint-disable-next-line no-console
+      console.log(this.getCurrentTime());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      currentTime: this.getCurrentTime(),
+    });
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.clockName}</strong> time is{' '}
+        <span className="Clock__time">{this.state.currentTime}</span>
+      </div>
+    );
+  }
+}
