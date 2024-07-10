a928d1ee92326d6cb020ca691ea2a8f43df7a0a7
Yuliia Nudyk <yulyayu833@gmail.com>
2024-07-10 16:32:37 +0300

    task-solution-edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
 
   return `Clock-${value}`;
-}
-
-function getCurrentTime(): string {
-  return new Date().toUTCString().slice(-12, -4);
-}
-
-type ClockProps = {
-  name: string;
-};
-
-type ClockState = {
-  currentTime: string;
-};
-
-class Clock extends React.Component<ClockProps, ClockState> {
-  state = {
-    currentTime: getCurrentTime(),
-  };
-
-  changeCurrentTime = () => {
-    const newTime = getCurrentTime();
-
-    this.setState({ currentTime: newTime });
-
-    // eslint-disable-next-line no-console
-    console.log(newTime);
-  };
-
-  timerId = 0;
-
-  componentDidMount() {
-    this.timerId = window.setInterval(this.changeCurrentTime, 1000);
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.timerId);
-  }
-
-  componentDidUpdate(prevProps: Readonly<ClockProps>) {
-    if (prevProps.name !== this.props.name) {
-      // eslint-disable-next-line no-console
-      console.log(`Renamed from ${prevProps.name} to ${this.props.name}`);
-    }
-  }
-
-  render() {
-    return (
-      <div className="Clock">
-        <strong className="Clock__name">{this.props.name}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">{this.state.currentTime}</span>
-      </div>
-    );
-  }
 }
 
 type AppState = {
