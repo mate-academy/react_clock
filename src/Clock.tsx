b384572eb5b1ec23fa8c9f829a165005a6394c0f
Den <clixqqk@gmail.com>
2024-07-23 20:01:51 +0300

    dfsd

---
+++ src/Clock.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+  today: Date;
+  hasClock: boolean;
+}
+
+export class Clock extends React.Component<Props> {
+
+  render() {
+    const { name, today, hasClock } = this.props;
+
+    return (
+      hasClock && (
+        <div className="Clock">
+          <strong className="Clock__name">{name}</strong>
+          {' time is '}
+          <span className="Clock__time">
+            {today.toUTCString().slice(-12, -4)}
+          </span>
+        </div>
+      )
+    );
+  }
+}
