2c1424b4049a6e771347aa648ed373a21dd1f9ff
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2024-09-18 13:09:02 +0300

    final solution

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react';
-
 type Props = {
   clockName: string;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function usePrevious(value: any): any {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
 export const Clock: React.FC<Props> = ({ clockName }) => {
   const [today, setToday] = useState(new Date());
+
   const firstRender = useRef(true);
+  const prevClockName = usePrevious({ clockName });
 
   useEffect(() => {
     const timerIdToday = window.setInterval(() => {
       setToday(new Date());
-
       // eslint-disable-next-line no-console
       console.log(new Date().toUTCString().slice(-12, -4));
     }, 1000);
@@ -27,17 +38,19 @@ export const Clock: React.FC<Props> = ({ clockName }) => {
 
       return;
     }
+    // console.log(1);
 
-    // eslint-disable-next-line no-console
-    console.debug(`Renamed from ${clockName} to ${clockName}`);
+    if (prevClockName !== clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed from ${prevClockName?.clockName} to ${clockName}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [clockName]);
 
   return (
     <div className="Clock">
       <strong className="Clock__name">{clockName}</strong>
-
       {' time is '}
-
       <span className="Clock__time">{today.toUTCString().slice(-12, -4)}</span>
     </div>
   );
