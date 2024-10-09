f2b0e021c4b61fa3552591eb20484fb6b86d7f9a
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-10-09 17:58:29 +0300

    problem task

---
+++ src/Clock.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+type ClockProps = {
+  name: string;
+};
+
+type ClockState = {
+  time: string;
+  clockName: string;
+};
+
+export class Clock extends React.Component<ClockProps, ClockState> {
+  state: ClockState = {
+    time: new Date().toUTCString().slice(-12, -4),
+    clockName: this.props.name,
+  };
+
+  timerId: number | undefined;
+
+  clockNameId: number | undefined;
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      const currentTime = new Date().toUTCString().slice(-12, -4);
+
+      this.setState({ time: currentTime });
+
+      // eslint-disable-next-line no-console
+      console.log(currentTime);
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      window.clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.state.clockName}</strong>
+        {' time is '}
+        <span className="Clock__time">{this.state.time}</span>
+      </div>
+    );
+  }
+}
