d62581e7a548e6dac2567dddb2e82a9dae3c7104
Max Shapes <makc.bilous04@gmail.com>
2025-03-26 12:26:20 +0200

    made clock

---
+++ src/Clock.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+function getTime(): string {
+  const today = new Date();
+
+  return today.toUTCString().slice(-12, -4);
+}
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  clockName: string;
+  time: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state = {
+    clockName: 'Clock-0',
+    time: getTime(),
+  };
+
+  timeUpdateTimerId = 0;
+
+  // This code starts a timer
+  componentDidMount(): void {
+    this.timeUpdateTimerId = window.setInterval(() => {
+      this.setState({ time: getTime() });
+      // eslint-disable-next-line no-console
+      console.log(getTime());
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+      this.setState({ clockName: this.props.name });
+    }
+  }
+
+  // this code stops the timer
+  componentWillUnmount(): void {
+    window.clearInterval(this.timeUpdateTimerId);
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.name}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.state.time}</span>
+      </div>
+    );
+  }
+}
