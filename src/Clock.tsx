75b5231ae23f641173a1c9ae7dda8c3bea8f5c48
Grzegorz <grze.pra@gmail.com>
2025-04-28 13:20:46 +0200

    Not passing test with time every second

---
+++ src/Clock.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface Props {
+  clockName: string;
+}
+
+export class Clock extends React.Component<Props> {
+  private timerId: number = 0;
+
+  state = {
+    time: new Date().toUTCString().slice(-12, -4),
+  };
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({ time: new Date().toUTCString().slice(-12, -4) });
+      // eslint-disable-next-line no-console
+      console.log(this.state.time);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Renamed from ${prevProps.clockName} to ${this.props.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.state.time}</span>
+      </div>
+    );
+  }
+}
