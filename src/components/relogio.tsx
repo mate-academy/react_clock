b0adbc5ed228af9e24d82e8bedf43e2ea0827dd9
LuisCampelo2 <luisfilipecab@gmail.com>
2025-05-02 13:59:24 -0300

    terceiro commit

--- src/components/relogio.tsx
+++ src/components/relogio.tsx
@@ -28,6 +28,30 @@ export class Clock extends Component<Props, State> {
     }, 1000);
   }
 
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.name !== this.props.name) {
+      // Limpa o intervalo antigo
+      if (this.timerId !== undefined) {
+        clearInterval(this.timerId);
+      }
+
+      // Cria um novo intervalo
+      this.timerId = window.setInterval(() => {
+        // eslint-disable-next-line no-console
+        console.log(
+          this.state.today.toLocaleTimeString('pt-BR', {
+            timeZone: 'America/Sao_Paulo',
+          }),
+        );
+
+        this.setState({ today: new Date() });
+      }, 1000);
+
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
   componentWillUnmount() {
     if (this.timerId !== undefined) {
       window.clearInterval(this.timerId);
