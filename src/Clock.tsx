194c0f6e2a044f04460f3042fecaa84a7df741b5
dima.yeromin <dima.yeromin@gmail.com>
2025-05-15 23:01:57 +0200

    Refactor App component to use class-based structure, manage clock state, and handle context menu events

---
+++ src/Clock.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+interface ClockProps {
+  name: string;
+}
+
+export class Clock extends Component<ClockProps> {
+  private timerId: number | null = null;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.forceUpdate();
+      // eslint-disable-next-line no-console
+      console.log(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId) {
+      window.clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+  componentDidUpdate(prevProps: ClockProps) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  render() {
+    const { name } = this.props;
+    const today = new Date();
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{name}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+} 
