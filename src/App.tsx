2355ea21a67b86543e30ab065bc025dae602cd6d
LX-y-ka <alexandravirtualife@gmail.com>
2025-06-05 19:45:38 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -8,30 +9,41 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+  const [hasClock, setHasClock] = useState(true);
+  const [clockName, setClockName] = useState('Clock-0');
+  const timerId = useRef(0);
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  const handleMouseRight = (event: MouseEvent) => {
+    event.preventDefault(); // not to show the context menu
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+    setHasClock(false);
+  };
+
+  const handleMouseLeft = (event: MouseEvent) => {
+    event.preventDefault(); // not to show the context menu
+
+    setHasClock(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('contextmenu', handleMouseRight);
+    document.addEventListener('click', handleMouseLeft);
+    timerId.current = window.setInterval(() => {
+      setClockName(getRandomName());
+    }, 3300);
+
+    return () => {
+      document.removeEventListener('contextmenu', handleMouseRight);
+      document.addEventListener('click', handleMouseLeft);
+      window.clearInterval(timerId.current);
+    };
+  }, []);
 
   return (
     <div className="App">
       <h1>React clock</h1>
 
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
+      {hasClock ? <Clock clockName={clockName} /> : ''}
     </div>
   );
 };
