ad0b1565be09f0ef2ea1fd2623915eb1b8e1c576
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-08-24 09:41:15 -0300

    Development of the App application under the specifications described in the repository.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,105 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './components/Clock';
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
+type Props = {
 
-  return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+type State = {
+  time: Date | null;
+  clockName: string;
+  hasClock: boolean;
+}
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
+export class App extends React.Component<Props, State> {
+  /*
+    const today = new Date();
+    let clockName = 'Clock-0';
+
+    // this code stops the timer
+    window.clearInterval(timerId);
+  */
+
+  state = {
+    time: null,
+    clockName: 'Clock-0',
+    hasClock: true
+  }  
+    // This code starts a timer
+  timerId = window.setInterval(() => {
+    this.setState({
+      clockName: this.getRandomName()
+    })
   }, 3300);
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  timer = window.setInterval(() => {
+    this.setState({
+      time: new Date()
+    })
+    // eslint-disable-next-line no-console
+    console.log(this.state.time);
+  }, 1000)
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+  getRandomName(): string {
+    const value = Date.now().toString().slice(-4);
+    
+    return `Clock-${value}`;
+  }
 
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
+  hiddenClock = () => this.setState({ hasClock: false });
+  showClock = () => this.setState({hasClock: true})
 
-        {' time is '}
+  hasClockControl = window.addEventListener('contextmenu', (event: MouseEvent) => {
+    event.preventDefault(); // not to show the context menu
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+    this.hiddenClock();
+    window.clearInterval(this.timer);
+  });
+
+  showClockOnLeftClick = window.addEventListener('click', (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.showClock();
+    this.timer = window.setInterval(() => {
+      this.setState({
+        time: new Date()
+      })
+      // eslint-disable-next-line no-console
+      console.log(this.state.time);
+    }, 1000);
+    this.timer;
+  })
+
+  componentDidMount() {
+    this.setState({time: new Date()});
+    this.timerId;
+    this.timer;
+    this.hasClockControl;
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>): void {
+    if(prevState.clockName !== this.state.clockName) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevState.clockName} to ${this.state.clockName}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timer);
+  }
+
+  render() {
+    return (
+        <div className="App">
+        <h1>React clock</h1>
+  
+        {
+          this.state.hasClock
+            &&
+          <Clock name={this.state.clockName} time={this.state.time} />
+        }
       </div>
-    </div>
-  );
+    );
+  }
 };
