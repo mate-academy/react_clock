eb9057d936b5918f7225675a16a84308ddb885a5
Denys Herych <fojjof2@gmail.com>
2025-10-10 20:28:51 +0300

    solution

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -24,7 +24,7 @@ export class Clock extends React.Component<Props, State> {
     this.timerId = window.setInterval(() => {
       this.setState({ today: new Date() });
       // eslint-disable-next-line no-console
-      console.warn(this.state.today);
+      console.log(this.state.today.toUTCString().slice(-12, -4));
     }, 1000);
   };
 
