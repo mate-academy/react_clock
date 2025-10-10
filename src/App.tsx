350043492e2193d490a46c72b10b8e0157516e1b
Denys Herych <fojjof2@gmail.com>
2025-10-10 19:24:40 +0300

    need help

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,77 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  hasClock: boolean;
+  today: Date;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    hasClock: false,
+    today: new Date(),
+  };
+
+  clockName = 'Clock-0';
+
+  timerId?: number;
+
+  timerTimeId?: number;
+
+  handleContextmenu = (event: Event) => {
+    event.preventDefault();
+    window.clearInterval(this.timerId);
+    window.clearInterval(this.timerTimeId);
+    this.setState({ hasClock: false });
+  };
+
+  startTimerName = () => {
+    this.timerId = window.setInterval(() => {
+      this.clockName = getRandomName();
+      // eslint-disable-next-line no-console
+      console.log(this.clockName);
+    }, 3300);
+  };
+
+  startTimer = () => {
+    this.setState({ hasClock: true });
+
+    this.timerTimeId = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line no-console
+      console.log(this.state.today);
+    }, 1000);
+  };
+
+  handleClick = () => {
+    this.startTimer();
+    this.startTimerName();
+  };
+
+  componentDidMount(): void {
+    this.startTimer();
+    this.startTimerName();
+    document.addEventListener('contextmenu', this.handleContextmenu);
+    document.addEventListener('click', this.handleClick);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">{this.clockName}</strong>
+
+            {' time is '}
+
+            <span className="Clock__time">
+              {this.state.today.toUTCString().slice(-12, -4)}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
