5814fea9cff72db454c19bc75a93e4b98f4ea073
diego69-foda <diego.bkuhn@gmail.com>
2025-11-03 08:16:26 -0300

    refactor: convert App component to class-based and implement timer management

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,52 @@ import './App.scss';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
-
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 1000);
-
- componentDidUpdate (timerId) {    
-  console.log (timerId);
-}
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type State = {
+  clockName: string;
+  now: Date;
 };
+
+
+export class App extends React.Component<{}, State> {
+  private timerId: number | null = null;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = { clockName: 'Clock-0', now: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName(), now: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timerId !== null) {
+      window.clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { clockName, now } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        <div className="Clock">
+          <strong className="Clock__name">{clockName}</strong>
+
+          {' time is '}
+
+          <span className="Clock__time">
+            {now.toUTCString().slice(-12, -4)}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
