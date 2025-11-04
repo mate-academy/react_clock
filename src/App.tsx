3db2f079908116e677ae4223cd1dd40e77232183
danie010830 <danielgukillwar008@gmail.com>
2025-11-04 19:15:53 -0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,31 +8,77 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type AppState = {
+  hasClock: boolean;
+  clockName: string;
 };
+
+export class App extends React.Component<{}, AppState> {
+  private nameTimerId: number | null = null;
+
+  state: AppState = {
+    hasClock: true,
+    clockName: 'Clock-0',
+  };
+
+  constructor(props: {}) {
+    super(props);
+
+    this.onDocumentClick = this.onDocumentClick.bind(this);
+    this.onDocumentContextMenu = this.onDocumentContextMenu.bind(this);
+  }
+
+  componentDidMount() {
+    // update the clockName every 3300ms
+    this.nameTimerId = window.setInterval(() => {
+      const newName = getRandomName();
+
+      this.setState({ clockName: newName });
+    }, 3300);
+
+    // show clock on left click
+    document.addEventListener('click', this.onDocumentClick);
+
+    // hide clock and prevent context menu on right click
+    document.addEventListener('contextmenu', this.onDocumentContextMenu);
+  }
+
+  componentWillUnmount() {
+    if (this.nameTimerId != null) {
+      window.clearInterval(this.nameTimerId);
+      this.nameTimerId = null;
+    }
+
+    document.removeEventListener('click', this.onDocumentClick);
+    document.removeEventListener('contextmenu', this.onDocumentContextMenu);
+  }
+
+  onDocumentClick() {
+    // left click should show the clock
+    this.setState({ hasClock: true });
+  }
+
+  onDocumentContextMenu(event: MouseEvent) {
+    // right click should hide the clock and prevent default menu
+    event.preventDefault();
+    this.setState({ hasClock: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock && (
+          <div className="Clock">
+            <strong className="Clock__name">{this.state.clockName}</strong>
+
+            {' time is '}
+
+            <Clock name={this.state.clockName} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
