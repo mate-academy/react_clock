87e543025cea07401a24fc7b3baf45c155e542a4
Igor Tinyayev <tinyayev.ca@gmail.com>
2025-12-29 06:10:50 -0700

    react_clock

---
+++ src/Clock.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  time: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  private timerId: number | null = null;
+
+  state: State = {
+    time: new Date().toUTCString().slice(-12, -4),
+  };
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      const time = new Date().toUTCString().slice(-12, -4);
+
+      // eslint-disable-next-line no-console
+      console.log(time);
+
+      this.setState({ time });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    if (this.timerId !== null) {
+      window.clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{name}</strong>
+        {' time is '}
+        <span className="Clock__time">{time}</span>
+      </div>
+    );
+  }
+}
