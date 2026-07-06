d2bc2e1c9641728a8aa8ecfc32678e6616e5f149
Nadiia <koropchenkon@gmail.com>
2026-07-06 14:47:43 +0300

    add task solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  time: string;
+};
+
+export class Clock extends React.Component<Props, State> {
+  private timerId: number | null = null;
+
+  state: State = {
+    time: new Date().toUTCString().slice(-12, -4),
+  };
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      const currentTime = new Date().toUTCString().slice(-12, -4);
+
+      this.setState({ time: currentTime });
+      // eslint-disable-next-line no-console
+      console.log(currentTime);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{name}</strong>
+        {' time is '}
+        <span className="Clock__time">{time}</span>
+      </div>
+    );
+  }
+}
