327b6b8fa3d6bf8ef1c49776a1c2b34cc25708a3
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-07-06 18:56:42 +0300

    feat: add Clock

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -21,15 +21,14 @@ export class Clock extends React.PureComponent<Props, State> {
 
       this.setState({ today: now });
 
+      // eslint-disable-next-line no-console
       console.log(now.toUTCString().slice(-12, -4));
     }, 1000);
   }
 
-  componentDidUpdate(
-    prevProps: Readonly<Props>,
-    // prevState: Readonly<State>,
-  ): void {
+  componentDidUpdate(prevProps: Readonly<Props>): void {
     if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
       console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
     }
   }
