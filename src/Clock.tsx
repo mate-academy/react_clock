74a450ad678a1a92e7870e36575c96925d80fbc2
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-07-07 22:11:43 +0300

    Change 1

---
+++ src/Clock.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+type State = {
+  today: Date;
+};
+
+export class Clock extends React.Component<Props, State> {
+  state: State = {
+    today: new Date(),
+  };
+
+  timerId?: number;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      const now = new Date();
+
+      this.setState({
+        today: now,
+      });
+
+      console.log(now.toUTCString().slice(-12, -4));
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.name !== this.props.name) {
+      console.warn(
+        `Renamed from ${prevProps.name} to ${this.props.name}`,
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">
+          {this.props.name}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {this.state.today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
